4e703ab3001e55072a2485dc58f7182a339e91ff
Ilya Dmitrichenko <errordeveloper@gmail.com>
2018-06-18 16:41:33 -0700

    Fix release script

--- do-release.sh
+++ do-release.sh
@@ -1,18 +1,17 @@
 #!/bin/sh -x
 
 if [ -z "${CIRCLE_PULL_REQUEST}" ] && [ -n "${CIRCLE_TAG}" ] && [ "${CIRCLE_PROJECT_USERNAME}" = "weaveworks" ] ; then
-  export RELEASE_DESCRIPTION="${CIRCLE_TAG}"
+  export RELEASE_DESCRIPTION="${CIRCLE_TAG} (permalink)"
   goreleaser release --skip-validate --config=./.goreleaser.yml
 
   git tag -d "${CIRCLE_TAG}"
-  gig tag latest_release
-
+  git tag latest_release
 
   if github-release info --user weaveworks --repo eksctl --tag latest_release > /dev/null 2>&1 ; then
     github-release delete --user weaveworks --repo eksctl --tag latest_release
   fi
 
-  export RELEASE_DESCRIPTION="${RELEASE_DESCRIPTION} (permalink)"
+  export RELEASE_DESCRIPTION="${CIRCLE_TAG}"
   goreleaser release --config=./.goreleaser.yml
 else
   echo "Not a tag release, skip publish"
