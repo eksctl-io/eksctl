55893b38e4dda30d854adbaa2130a1791e5b870e
Ilya Dmitrichenko <errordeveloper@gmail.com>
2018-06-18 19:20:59 -0700

    Release Homebrew tap in the first pass

--- do-release.sh
+++ do-release.sh
@@ -2,12 +2,10 @@
 
 if [ -z "${CIRCLE_PULL_REQUEST}" ] && [ -n "${CIRCLE_TAG}" ] && [ "${CIRCLE_PROJECT_USERNAME}" = "weaveworks" ] ; then
   export RELEASE_DESCRIPTION="${CIRCLE_TAG} (permalink)"
-  goreleaser release --config=./.goreleaser.yml
+  cat ./.goreleaser.yml ./.goreleaser.brew.yml > .goreleaser.brew.combined.yml
+  goreleaser release --config=./.goreleaser.brew.combined.yml
 
   sleep 90 # GitHub API resolves the time to the nearest minute, so in order to control the sorting oder we need this
-
-  cat ./.goreleaser.yml ./.goreleaser.brew.yml > .goreleaser.brew.combined.yml
-  goreleaser release --rm-dist --config=./.goreleaser.brew.combined.yml
 
   git tag --delete "${CIRCLE_TAG}"
   git tag --force latest_release
