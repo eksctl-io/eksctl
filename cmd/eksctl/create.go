2ad3b63730f673699b2c66c15ba40d26f275f5f7
Archis Gore <archis@polyverse.io>
2018-06-28 09:18:11 -0700

    Unblocks channel to make it readable
    (cherry-picked from #85)

--- cmd/eksctl/create.go
+++ cmd/eksctl/create.go
@@ -126,7 +126,7 @@ func doCreateCluster(cfg *eks.ClusterConfig, name string) error {
 	{ // core action
 		taskErr := make(chan error)
 		// create each of the core cloudformation stacks
-		ctl.CreateCluster(taskErr)
+		go ctl.CreateCluster(taskErr)
 		// read any errors (it only gets non-nil errors)
 		for err := range taskErr {
 			return err
