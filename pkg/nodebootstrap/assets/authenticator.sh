380f18772d9456b679b740bb36f1028deeebc22d
Ilya Dmitrichenko <errordeveloper@gmail.com>
2018-08-10 14:35:59 +0100

    Static node bootsrap

--- pkg/nodebootstrap/assets/authenticator.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -eu
-
-# Simple wrapper for authenticator, so we can read AWS_EKS_CLUSTER_NAME from `metadata.env`
-# instead of having to update `kubeconfig.yaml` (as `kubectl config` doesn't support plugin fields)
-
-. /etc/eksctl/metadata.env
-
-aws-iam-authenticator token -i "${AWS_EKS_CLUSTER_NAME}"
