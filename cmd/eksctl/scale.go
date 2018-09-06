69368bde42f80fff091c5ea95db68386aa35abfe
Richard Case <richard.case@outlook.com>
2018-09-06 10:33:02 +0100

    Added nodegroup scaling
    This is currently work in progress and is dependent on a
    change to goformation so that ASG can be correctly unmarshalled.

---
+++ cmd/eksctl/scale.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/kubicorn/kubicorn/pkg/logger"
+	"github.com/spf13/cobra"
+	"github.com/weaveworks/eksctl/pkg/eks"
+	"github.com/weaveworks/eksctl/pkg/eks/api"
+)
+
+func scaleCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "scale",
+		Short: "Scale resources(s)",
+		Run: func(c *cobra.Command, _ []string) {
+			c.Help()
+		},
+	}
+
+	cmd.AddCommand(scaleNodeGroupCmd())
+
+	return cmd
+}
+
+func scaleNodeGroupCmd() *cobra.Command {
+	cfg := &api.ClusterConfig{}
+
+	cmd := &cobra.Command{
+		Use:   "nodegroup",
+		Short: "Scale a nodegroup",
+		Run: func(_ *cobra.Command, args []string) {
+			if err := doScaleNodeGroup(cfg); err != nil {
+				logger.Critical("%s\n", err.Error())
+				os.Exit(1)
+			}
+		},
+	}
+
+	fs := cmd.Flags()
+
+	fs.StringVarP(&cfg.ClusterName, "name", "n", "", "EKS cluster name")
+	fs.IntVarP(&cfg.Nodes, "nodes", "N", 0, "total number of nodes (scale to this number)")
+
+	fs.StringVarP(&cfg.Region, "region", "r", DEFAULT_EKS_REGION, "AWS region")
+	fs.StringVarP(&cfg.Profile, "profile", "p", "", "AWS creditials profile to use (overrides the AWS_PROFILE environment variable)")
+
+	return cmd
+}
+
+func doScaleNodeGroup(cfg *api.ClusterConfig) error {
+	ctl := eks.New(cfg)
+
+	if err := ctl.CheckAuth(); err != nil {
+		return err
+	}
+
+	if cfg.ClusterName == "" {
+		return fmt.Errorf("no cluster name supplied. Use the --name= flag")
+	}
+
+	if cfg.Nodes < 1 {
+		return fmt.Errorf("number of nodes must be greater than 0. Use the --nodes/-N flag")
+	}
+
+	stackManager := ctl.NewStackManager()
+	errs := stackManager.ScaleInitialNodeGroup()
+	if len(errs) > 0 {
+		logger.Info("%d error(s) occurred and nodegroup can't be scaled", len(errs))
+		for _, err := range errs {
+			logger.Critical("%s\n", err.Error())
+		}
+		return fmt.Errorf("failed to scale nodegroup for cluster %q", cfg.ClusterName)
+	}
+
+	return nil
+}
--- pkg/cfn/manager/nodegroup.go
+++ pkg/cfn/manager/nodegroup.go
@@ -3,8 +3,11 @@ package manager
 import (
 	"fmt"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/pkg/errors"
 	"github.com/weaveworks/eksctl/pkg/cfn/builder"
 
+	cfn "github.com/aws/aws-sdk-go/service/cloudformation"
 	"github.com/kubicorn/kubicorn/pkg/logger"
 )
 
@@ -28,6 +31,26 @@ func (c *StackCollection) CreateNodeGroup(seq int, errs chan error) error {
 	return c.CreateStack(name, stack, nil, errs)
 }
 
+func (c *StackCollection) ScaleNodeGroup(errs chan error) error {
+	clusterName := c.makeClusterStackName()
+	c.spec.ClusterStackName = clusterName
+	name := c.makeNodeGroupStackName(0)
+	logger.Info("scaling nodegroup stack %q in cluster", name, clusterName)
+	stack := builder.NewNodeGroupResourceSet(c.spec)
+
+	// Get current stack
+	template, err := c.getStackTemplate(name)
+	if err != nil {
+		return errors.Wrapf(err, "error getting stack template %s", name)
+	}
+
+	if err := stack.AddResourcesForScaling(template); err != nil {
+		return nil
+	}
+
+	return c.UpdateStack(name, stack, c.makeNodeGroupParams(0), errs)
+}
+
 func (c *StackCollection) DeleteNodeGroup() error {
 	_, err := c.DeleteStack(c.makeNodeGroupStackName(0))
 	return err
@@ -35,4 +58,18 @@ func (c *StackCollection) DeleteNodeGroup() error {
 
 func (c *StackCollection) WaitDeleteNodeGroup() error {
 	return c.WaitDeleteStack(c.makeNodeGroupStackName(0))
+}
+
+func (c *StackCollection) getStackTemplate(stackName string) (string, error) {
+	input := &cfn.GetTemplateInput{
+		StackName: aws.String(stackName),
+	}
+
+	output, err := c.cfn.GetTemplate(input)
+	if err != nil {
+		return "", err
+	}
+
+	logger.Debug("retrieved template: %s", *output.TemplateBody)
+	return *output.TemplateBody, nil
 }
