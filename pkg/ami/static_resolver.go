cc0098880400694f7e7eb37c7f840c5a85c66065
Richard Case <richard.case@outlook.com>
2018-09-20 14:14:46 +0100

    Generation of static AMI list
    The list of AMIs to use for the static resolver is now generated
    via go:generate. The list is queried from AWS and output to a code
    file.
    
    A separate makefile target called 'generate-ami' has been added that
    runs go generate. The reason for a separate target is the fact that
    AWS credentials are required for the code generation and this would
    break the current workflow for the 'generate' target.
    
    Another makefile target called 'ami-check' has been added. The
    intended purpose of this by an automated scheduled process that
    will run to check if new images are available.
    
    Issue #49

--- pkg/ami/static_resolver.go
+++ pkg/ami/static_resolver.go
@@ -4,29 +4,10 @@ import (
 	"fmt"
 
 	"github.com/kubicorn/kubicorn/pkg/logger"
-	"github.com/weaveworks/eksctl/pkg/eks/api"
 	"github.com/weaveworks/eksctl/pkg/utils"
 )
 
-// TODO: This file will be go generated in the future
-// https://github.com/weaveworks/eksctl/issues/49
-
-// StaticImages is a map of images in each region by
-// image OS family and by class
-var StaticImages = map[string]map[int]map[string]string{
-	ImageFamilyAmazonLinux2: {
-		ImageClassGeneral: {
-			api.EKS_REGION_US_WEST_2: "ami-0a54c984b9f908c81",
-			api.EKS_REGION_US_EAST_1: "ami-0440e4f6b9713faf6",
-			api.EKS_REGION_EU_WEST_1: "ami-0c7a4976cb6fafd3a",
-		},
-		ImageClassGPU: {
-			api.EKS_REGION_US_WEST_2: "ami-0731694d53ef9604b",
-			api.EKS_REGION_US_EAST_1: "ami-058bfb8c236caae89",
-			api.EKS_REGION_EU_WEST_1: "ami-0706dc8a5eed2eed9",
-		},
-	},
-}
+//go:generate go run generate/main.go
 
 // StaticDefaultResolver resolves the AMI to the defaults for the region
 type StaticDefaultResolver struct {
