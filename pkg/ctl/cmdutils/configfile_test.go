7b52d0ac154a6e4f21ddcefb6fd6ff1e3294da60
Christopher Hein <me@chrishein.com>
2019-04-02 23:32:50 +0000

    adding example for ebs and fsx
    Signed-off-by: Christopher Hein <me@chrishein.com>

--- pkg/ctl/cmdutils/configfile_test.go
+++ pkg/ctl/cmdutils/configfile_test.go
@@ -57,7 +57,7 @@ var _ = Describe("cmdutils configfile", func() {
 			examples, err := filepath.Glob(examplesDir + "*.yaml")
 			Expect(err).ToNot(HaveOccurred())
 
-			Expect(examples).To(HaveLen(5))
+			Expect(examples).To(HaveLen(6))
 			for _, example := range examples {
 				cfg := api.NewClusterConfig()
 
