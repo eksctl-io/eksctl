9e4306e6354e262d36967c934f13bedaeb442430
Martina Iglesias Fernandez <martina@weave.works>
2019-06-14 15:15:29 +0200

    Add support for extra kubelet configuration

--- pkg/nodebootstrap/userdata_test.go
+++ pkg/nodebootstrap/userdata_test.go
@@ -3,6 +3,9 @@ package nodebootstrap
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	api "github.com/weaveworks/eksctl/pkg/apis/eksctl.io/v1alpha5"
+	kubeletapi "k8s.io/kubelet/config/v1beta1"
+	"sigs.k8s.io/yaml"
 	"strconv"
 	"strings"
 )
@@ -18,6 +21,50 @@ var _ = Describe("User data", func() {
 				_, err := strconv.Atoi(parts[1])
 				Expect(err).ToNot(HaveOccurred())
 			}
+		})
+	})
+
+	Describe("creating kubelet config", func() {
+		var (
+			clusterConfig *api.ClusterConfig
+			ng            *api.NodeGroup
+		)
+		BeforeEach(func() {
+			clusterConfig = api.NewClusterConfig()
+			ng = &api.NodeGroup{}
+		})
+
+		It("the kubelet is serialized with the correct format", func() {
+			data, err := makeKubeletConfigYAML(clusterConfig, ng)
+			Expect(err).ToNot(HaveOccurred())
+
+			kubelet := &kubeletapi.KubeletConfiguration{}
+
+			errUnmarshal := yaml.UnmarshalStrict(data, kubelet)
+			Expect(errUnmarshal).ToNot(HaveOccurred())
+		})
+
+		It("the kubelet is serialized with the correct format", func() {
+
+			ng.KubeletExtraConfig = &api.NodeGroupKubeletConfig{
+				"kubeReserved": &map[string]string{
+					"cpu":               "300m",
+					"memory":            "300Mi",
+					"ephemeral-storage": "1Gi",
+				},
+			}
+			data, err := makeKubeletConfigYAML(clusterConfig, ng)
+			Expect(err).ToNot(HaveOccurred())
+
+			kubelet := &kubeletapi.KubeletConfiguration{}
+
+			errUnmarshal := yaml.UnmarshalStrict(data, kubelet)
+			Expect(errUnmarshal).ToNot(HaveOccurred())
+
+			Expect(kubelet.KubeReserved).ToNot(BeNil())
+			Expect(kubelet.KubeReserved["cpu"]).To(Equal("300m"))
+			Expect(kubelet.KubeReserved["memory"]).To(Equal("300Mi"))
+			Expect(kubelet.KubeReserved["ephemeral-storage"]).To(Equal("1Gi"))
 		})
 	})
 })
