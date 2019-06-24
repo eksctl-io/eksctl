8f4f1368249fe55e5855856b1fd6db968be5e355
Alfonso Acosta <fons@syntacticsugar.consulting>
2019-06-24 13:58:51 +0200

    Fix github.com/christopherhein/go-version dependency
    Full error was
    
    ```
    go: github.com/christopherhein/go-version@v0.0.0-20190621180219-0c7766a4ad7b: parsing go.mod: unexpected module path "go.hein.dev/go-version"
    ```

--- tools.go
+++ tools.go
@@ -5,9 +5,17 @@ package eksctl
 // Mock imports to enforce their installation by `go mod`.
 // See https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
 import (
+	// This go-version dependency only included to enforce aws-iam-authenticator to use a specific version of go-version.
+	// aws-iam-authenticator v0.4.0 uses `dep`, causing `go mod` to ignore its dependency files and just to pull
+	// github.com/christopherhein/go-version:master. This causes the build to break because go-version:master has recently
+	// switched to module name go.hein.dev/go-version, causing a module name mismatch.
+	// TODO(fons): get rid of this dependency once we bump the version of aws-iam-authenticator
+	//             which now uses go modules
+	_ "github.com/christopherhein/go-version"
 	_ "github.com/dave/jennifer/jen"
 	_ "github.com/goreleaser/goreleaser"
 	_ "github.com/jteeuwen/go-bindata/go-bindata"
+
 	_ "github.com/kubernetes-sigs/aws-iam-authenticator/cmd/aws-iam-authenticator"
 	_ "github.com/mattn/goveralls"
 	_ "github.com/vektra/mockery/cmd/mockery"
