1407777140033fbd2b3855ea8276d7c3091084fe
Alfonso Acosta <fons@syntacticsugar.consulting>
2019-06-26 02:19:16 +0200

    Correct k8s.io/code-generator dependecnies

--- tools.go
+++ tools.go
@@ -21,5 +21,9 @@ import (
 	_ "github.com/vektra/mockery/cmd/mockery"
 	_ "github.com/weaveworks/github-release"
 	_ "golang.org/x/tools/cmd/stringer"
-	_ "k8s.io/code-generator"
+	_ "k8s.io/code-generator/cmd/client-gen"
+	_ "k8s.io/code-generator/cmd/deepcopy-gen"
+	_ "k8s.io/code-generator/cmd/defaulter-gen"
+	_ "k8s.io/code-generator/cmd/informer-gen"
+	_ "k8s.io/code-generator/cmd/lister-gen"
 )
