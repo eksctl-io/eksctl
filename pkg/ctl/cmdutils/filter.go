0555c1f294d1a1f97f5277bd2005f35ff69d1614
Ilya Dmitrichenko <errordeveloper@gmail.com>
2019-08-05 16:14:44 +0100

    Generalised `cmdutils.Filter`
    - extract parts of `cmdutils.NodeGroupFilter` into `cmdutils.Filter` for future re-use
    - add `NameString` method to `api.NodeGroup`

---
+++ pkg/ctl/cmdutils/filter.go
@@ -0,0 +1,203 @@
+package cmdutils
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/gobwas/glob"
+	"github.com/kris-nova/logger"
+	"github.com/pkg/errors"
+
+	"k8s.io/apimachinery/pkg/util/sets"
+)
+
+// Filter holds filter configuration
+type Filter struct {
+	ExcludeAll bool // highest priority
+
+	// include filters take precedence
+	includeNames    sets.String
+	includeGlobs    []glob.Glob
+	rawIncludeGlobs []string
+
+	excludeNames    sets.String
+	excludeGlobs    []glob.Glob
+	rawExcludeGlobs []string
+}
+
+// AppendIncludeNames appends explicit names to the include filter
+func (f *Filter) AppendIncludeNames(names ...string) { f.includeNames.Insert(names...) }
+
+// AppendExcludeGlobs sets globs for exclusion rules
+func (f *Filter) AppendExcludeGlobs(globExprs ...string) error {
+	for _, expr := range globExprs {
+		compiledExpr, err := glob.Compile(expr)
+		if err != nil {
+			return errors.Wrapf(err, "parsing glob filter %q", expr)
+		}
+		f.excludeGlobs = append(f.excludeGlobs, compiledExpr)
+		f.rawExcludeGlobs = append(f.rawExcludeGlobs, expr)
+	}
+	return nil // exclude filter doesn't have to match anything, so we don't validate it
+}
+
+// AppendExcludeNames appends explicit names to the exclude filter
+func (f *Filter) AppendExcludeNames(names ...string) { f.excludeNames.Insert(names...) }
+
+func (*Filter) matchGlobs(name string, exprs []glob.Glob) bool {
+	for _, compiledExpr := range exprs {
+		if compiledExpr.Match(name) {
+			return true
+		}
+	}
+	return false
+}
+
+func (f *Filter) hasIncludeRules() bool {
+	return f.includeNames.Len()+len(f.includeGlobs) != 0
+}
+
+func (f *Filter) describeIncludeRules() string {
+	rules := append(f.includeNames.List(), f.rawIncludeGlobs...)
+	return fmt.Sprintf("%s", strings.Join(rules, ","))
+}
+
+func (f *Filter) hasExcludeRules() bool {
+	return f.excludeNames.Len()+len(f.excludeGlobs) != 0
+}
+
+func (f *Filter) describeExcludeRules() string {
+	rules := append(f.excludeNames.List(), f.rawExcludeGlobs...)
+	return fmt.Sprintf("%s", strings.Join(rules, ","))
+}
+
+// Match given nodegroup against the filter and returns
+// true or false if it has to be included or excluded
+func (f *Filter) Match(name string) bool {
+	if f.ExcludeAll {
+		return false // force exclude
+	}
+
+	hasIncludeRules := f.hasIncludeRules()
+	hasExcludeRules := f.hasExcludeRules()
+
+	if !hasIncludeRules && !hasExcludeRules {
+		return true // empty rules - include
+	}
+
+	mustInclude := false // use this override when rules overlap
+
+	if hasIncludeRules {
+		mustInclude = f.includeNames.Has(name)
+		if f.matchGlobs(name, f.includeGlobs) {
+			mustInclude = true
+		}
+		if !hasExcludeRules {
+			// empty exclusion rules - explicit inclusion mode
+			return mustInclude
+		}
+	}
+
+	if hasExcludeRules {
+		exclude := f.excludeNames.Has(name)
+		if f.matchGlobs(name, f.excludeGlobs) {
+			exclude = true
+		}
+		if exclude && !mustInclude {
+			// exclude, unless overridden by an inclusion rule
+			return false
+		}
+	}
+
+	return true // biased to include
+}
+
+// doMatchAll all names against the filter and return two sets of names - included and excluded
+func (f *Filter) doMatchAll(names []string) (sets.String, sets.String) {
+	included, excluded := sets.NewString(), sets.NewString()
+	if f.ExcludeAll {
+		for _, n := range names {
+			excluded.Insert(n)
+		}
+		return included, excluded
+	}
+	for _, n := range names {
+		if f.Match(n) {
+			included.Insert(n)
+		} else {
+			excluded.Insert(n)
+		}
+	}
+	return included, excluded
+}
+
+// doAppendIncludeGlobs sets globs for inclusion rules
+func (f *Filter) doAppendIncludeGlobs(names []string, resource string, globExprs ...string) error {
+	for _, expr := range globExprs {
+		compiledExpr, err := glob.Compile(expr)
+		if err != nil {
+			return errors.Wrapf(err, "parsing glob filter %q", expr)
+		}
+		f.includeGlobs = append(f.includeGlobs, compiledExpr)
+		f.rawIncludeGlobs = append(f.rawIncludeGlobs, expr)
+	}
+	return f.includeGlobsMatchAnything(names, resource)
+}
+
+func (f *Filter) doSetExcludeExistingFilter(names []string, resource string) error {
+	f.excludeNames.Insert(names...)
+	for _, n := range names {
+		isAlsoIncluded := f.includeNames.Has(n)
+		if f.matchGlobs(n, f.includeGlobs) {
+			isAlsoIncluded = true
+		}
+		if isAlsoIncluded {
+			return fmt.Errorf("existing %s %q should be excluded, but matches include filter: %s", resource, n, f.describeIncludeRules())
+		}
+	}
+	return nil
+}
+
+func (f *Filter) includeGlobsMatchAnything(names []string, resource string) error {
+	if len(f.includeGlobs) == 0 {
+		return nil
+	}
+	for _, n := range names {
+		if f.matchGlobs(n, f.includeGlobs) {
+			return nil
+		}
+	}
+	return fmt.Errorf("no %ss match include glob filter specification: %q", resource, strings.Join(f.rawIncludeGlobs, ","))
+}
+
+func (f *Filter) doLogInfo(resource string, names []string) {
+	logMsg := func(subset sets.String, status string) {
+		count := subset.Len()
+		list := strings.Join(subset.List(), ", ")
+		subjectFmt := "%d %ss (%s) were %s"
+		if count == 1 {
+			subjectFmt = "%d %s (%s) was %s"
+		}
+		logger.Info(subjectFmt, count, resource, list, status)
+	}
+
+	included, excluded := f.doMatchAll(names)
+	if f.hasIncludeRules() {
+		logger.Info("include rules: %s", f.describeIncludeRules())
+		if included.Len() == 0 {
+			logger.Info("no %ss were included by the filter", resource)
+		}
+	}
+	if included.Len() > 0 {
+		logMsg(included, "included")
+	}
+	if f.hasExcludeRules() {
+		logger.Info("exclude rules: %s", f.describeExcludeRules())
+		if excluded.Len() == 0 {
+			logger.Info("no %ss were excluded by the filter", resource)
+		}
+	}
+	if excluded.Len() > 0 {
+		logMsg(excluded, "excluded")
+	}
+}
--- pkg/ctl/cmdutils/nodegroup_filter.go
+++ pkg/ctl/cmdutils/nodegroup_filter.go
@@ -1,13 +1,7 @@
 package cmdutils
 
 import (
-	"fmt"
-	"strings"
-
-	"github.com/gobwas/glob"
 	"github.com/kris-nova/logger"
-	"github.com/pkg/errors"
-
 	"k8s.io/apimachinery/pkg/util/sets"
 
 	api "github.com/weaveworks/eksctl/pkg/apis/eksctl.io/v1alpha5"
@@ -16,24 +10,17 @@ import (
 
 // NodeGroupFilter holds filter configuration
 type NodeGroupFilter struct {
-	ExcludeAll bool // highest priority
-
-	// include filters take precedence
-	includeNames    sets.String
-	includeGlobs    []glob.Glob
-	rawIncludeGlobs []string
-
-	excludeNames    sets.String
-	excludeGlobs    []glob.Glob
-	rawExcludeGlobs []string
+	*Filter
 }
 
 // NewNodeGroupFilter create new NodeGroupFilter instance
 func NewNodeGroupFilter() *NodeGroupFilter {
 	return &NodeGroupFilter{
-		ExcludeAll:   false,
-		includeNames: sets.NewString(),
-		excludeNames: sets.NewString(),
+		Filter: &Filter{
+			ExcludeAll:   false,
+			includeNames: sets.NewString(),
+			excludeNames: sets.NewString(),
+		},
 	}
 }
 
@@ -47,48 +34,8 @@ func (f *NodeGroupFilter) AppendGlobs(includeGlobExprs, excludeGlobExprs []strin
 
 // AppendIncludeGlobs sets globs for inclusion rules
 func (f *NodeGroupFilter) AppendIncludeGlobs(nodeGroups []*api.NodeGroup, globExprs ...string) error {
-	for _, expr := range globExprs {
-		compiledExpr, err := glob.Compile(expr)
-		if err != nil {
-			return errors.Wrapf(err, "parsing glob filter %q", expr)
-		}
-		f.includeGlobs = append(f.includeGlobs, compiledExpr)
-		f.rawIncludeGlobs = append(f.rawIncludeGlobs, expr)
-	}
-	return f.includeGlobsMatchAnything(nodeGroups)
+	return f.doAppendIncludeGlobs(f.collectNames(nodeGroups), "nodegroup", globExprs...)
 }
-
-func (f *NodeGroupFilter) includeGlobsMatchAnything(nodeGroups []*api.NodeGroup) error {
-	if len(f.includeGlobs) == 0 {
-		return nil
-	}
-	for _, ng := range nodeGroups {
-		if f.matchGlobs(ng.Name, f.includeGlobs) {
-			return nil
-		}
-	}
-	return fmt.Errorf("no nodegroups match include glob filter specification: %q", strings.Join(f.rawIncludeGlobs, ","))
-}
-
-// AppendIncludeNames appends explicit names to the include filter
-func (f *NodeGroupFilter) AppendIncludeNames(names ...string) { f.includeNames.Insert(names...) }
-
-// AppendExcludeGlobs sets globs for exclusion rules
-func (f *NodeGroupFilter) AppendExcludeGlobs(globExprs ...string) error {
-	for _, expr := range globExprs {
-		compiledExpr, err := glob.Compile(expr)
-		if err != nil {
-			return errors.Wrapf(err, "parsing glob filter %q", expr)
-		}
-		f.excludeGlobs = append(f.excludeGlobs, compiledExpr)
-		f.rawExcludeGlobs = append(f.rawExcludeGlobs, expr)
-
-	}
-	return nil // exclude filter doesn't have to match anything, so we don't validate it
-}
-
-// AppendExcludeNames appends explicit names to the exclude filter
-func (f *NodeGroupFilter) AppendExcludeNames(names ...string) { f.excludeNames.Insert(names...) }
 
 // SetExcludeExistingFilter uses stackManager to list existing nodegroup stacks and configures
 // the filter accordingly
@@ -102,36 +49,19 @@ func (f *NodeGroupFilter) SetExcludeExistingFilter(stackManager *manager.StackCo
 		return err
 	}
 
-	f.excludeNames.Insert(existing...)
-
-	for _, name := range existing {
-		isAlsoIncluded := f.includeNames.Has(name)
-		if f.matchGlobs(name, f.includeGlobs) {
-			isAlsoIncluded = true
-		}
-		if isAlsoIncluded {
-			return fmt.Errorf("existing nodegroup %q should be excluded, but matches include filter: %s", name, f.describeIncludeRules())
-		}
-	}
-	return nil
+	return f.doSetExcludeExistingFilter(existing, "nodegroup")
 }
 
 // SetIncludeOrExcludeMissingFilter uses stackManager to list existing nodegroup stacks and configures
 // the filter to either explictily exluce or include nodegroups that are missing from given nodeGroups
 func (f *NodeGroupFilter) SetIncludeOrExcludeMissingFilter(stackManager *manager.StackCollection, includeOnlyMissing bool, nodeGroups *[]*api.NodeGroup) error {
-	stacks, err := stackManager.DescribeNodeGroupStacks()
+	existing, err := stackManager.ListNodeGroupStacks()
 	if err != nil {
 		return err
 	}
 
-	remote := sets.NewString()
+	remote := sets.NewString(existing...)
 	local := sets.NewString()
-
-	for _, s := range stacks {
-		if name := stackManager.GetNodeGroupName(s); name != "" {
-			remote.Insert(name)
-		}
-	}
 
 	for _, localNodeGroup := range *nodeGroups {
 		local.Insert(localNodeGroup.Name)
@@ -160,130 +90,20 @@ func (f *NodeGroupFilter) SetIncludeOrExcludeMissingFilter(stackManager *manager
 	return nil
 }
 
-func (*NodeGroupFilter) matchGlobs(name string, exprs []glob.Glob) bool {
-	for _, compiledExpr := range exprs {
-		if compiledExpr.Match(name) {
-			return true
-		}
-	}
-	return false
-}
-
-func (f *NodeGroupFilter) hasIncludeRules() bool {
-	return f.includeNames.Len()+len(f.includeGlobs) != 0
-}
-
-func (f *NodeGroupFilter) describeIncludeRules() string {
-	rules := append(f.includeNames.List(), f.rawIncludeGlobs...)
-	return fmt.Sprintf("%s", strings.Join(rules, ","))
-}
-
-func (f *NodeGroupFilter) hasExcludeRules() bool {
-	return f.excludeNames.Len()+len(f.excludeGlobs) != 0
-}
-
-func (f *NodeGroupFilter) describeExcludeRules() string {
-	rules := append(f.excludeNames.List(), f.rawExcludeGlobs...)
-	return fmt.Sprintf("%s", strings.Join(rules, ","))
-}
-
-// Match given nodegroup against the filter and returns
-// true or false if it has to be included or excluded
-func (f *NodeGroupFilter) Match(name string) bool {
-	if f.ExcludeAll {
-		return false // force exclude
-	}
-
-	hasIncludeRules := f.hasIncludeRules()
-	hasExcludeRules := f.hasExcludeRules()
-
-	if !hasIncludeRules && !hasExcludeRules {
-		return true // empty rules - include
-	}
-
-	mustInclude := false // use this override when rules overlap
-
-	if hasIncludeRules {
-		mustInclude = f.includeNames.Has(name)
-		if f.matchGlobs(name, f.includeGlobs) {
-			mustInclude = true
-		}
-		if !hasExcludeRules {
-			// empty exclusion rules - explicit inclusion mode
-			return mustInclude
-		}
-	}
-
-	if hasExcludeRules {
-		exclude := f.excludeNames.Has(name)
-		if f.matchGlobs(name, f.excludeGlobs) {
-			exclude = true
-		}
-		if exclude && !mustInclude {
-			// exclude, unless overridden by an inclusion rule
-			return false
-		}
-	}
-
-	return true // biased to include
-}
-
-// MatchAll nodegroups against the filter and return two sets of names - included and excluded
-func (f *NodeGroupFilter) MatchAll(nodeGroups []*api.NodeGroup) (sets.String, sets.String) {
-	included, excluded := sets.NewString(), sets.NewString()
-	if f.ExcludeAll {
-		for _, ng := range nodeGroups {
-			excluded.Insert(ng.Name)
-		}
-		return included, excluded
-	}
-	for _, ng := range nodeGroups {
-		if f.Match(ng.Name) {
-			included.Insert(ng.Name)
-		} else {
-			excluded.Insert(ng.Name)
-		}
-	}
-	return included, excluded
-}
-
 // LogInfo prints out a user-friendly message about how filter was applied
 func (f *NodeGroupFilter) LogInfo(nodeGroups []*api.NodeGroup) {
-	logMsg := func(ngSubset sets.String, status string) {
-		count := ngSubset.Len()
-		list := strings.Join(ngSubset.List(), ", ")
-		subject := "nodegroups (%s) were"
-		if count == 1 {
-			subject = "nodegroup (%s) was"
-		}
-		logger.Info("%d "+subject+" %s", count, list, status)
-	}
+	f.doLogInfo("nodegroup", f.collectNames(nodeGroups))
+}
 
-	included, excluded := f.MatchAll(nodeGroups)
-	if f.hasIncludeRules() {
-		logger.Info("include rules: %s", f.describeIncludeRules())
-		if included.Len() == 0 {
-			logger.Info("no nogroups were included by the filter")
-		}
-	}
-	if included.Len() > 0 {
-		logMsg(included, "included")
-	}
-	if f.hasExcludeRules() {
-		logger.Info("exclude rules: %s", f.describeExcludeRules())
-		if excluded.Len() == 0 {
-			logger.Info("no nogroups were excluded by the filter")
-		}
-	}
-	if excluded.Len() > 0 {
-		logMsg(excluded, "excluded")
-	}
+// MatchAll all names against the filter and return two sets of names - included and excluded
+func (f *NodeGroupFilter) MatchAll(nodeGroups []*api.NodeGroup) (sets.String, sets.String) {
+	return f.doMatchAll(f.collectNames(nodeGroups))
 }
 
 // ForEach iterates over each nodegroup that is included by the filter and calls iterFn
 func (f *NodeGroupFilter) ForEach(nodeGroups []*api.NodeGroup, iterFn func(i int, ng *api.NodeGroup) error) error {
 	for i, ng := range nodeGroups {
-		if f.Match(ng.Name) {
+		if f.Match(ng.NameString()) {
 			if err := iterFn(i, ng); err != nil {
 				return err
 			}
@@ -304,4 +124,12 @@ func (f *NodeGroupFilter) ValidateNodeGroupsAndSetDefaults(nodeGroups []*api.Nod
 		}
 		return nil
 	})
+}
+
+func (*NodeGroupFilter) collectNames(nodeGroups []*api.NodeGroup) []string {
+	names := []string{}
+	for _, ng := range nodeGroups {
+		names = append(names, ng.NameString())
+	}
+	return names
 }
