1ba00bcb3f8df2ebe4c8d4c8de304eee84f7d9f7
Jiaxin Shan <seedjeffwan@gmail.com>
2019-08-16 17:00:42 -0700

    Support gpu instance g3

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -7,7 +7,7 @@ import (
 // IsGPUInstanceType returns tru of the instance type is GPU
 // optimised.
 func IsGPUInstanceType(instanceType string) bool {
-	return strings.HasPrefix(instanceType, "p2") || strings.HasPrefix(instanceType, "p3")
+	return strings.HasPrefix(instanceType, "p2") || strings.HasPrefix(instanceType, "p3") || strings.HasPrefix(instanceType, "g3")
 }
 
 // HasGPUInstanceType returns true if it finds a gpu instance among the mixed instances
