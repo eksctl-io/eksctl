cad67a147b304f044662a1905f786c481604b8d7
Patrik Karlström <jp.karlstrom@gmail.com>
2019-09-02 11:18:46 +0200

    Add wrapper for the AWS sdk arn type

--- pkg/iam/mapping.go
+++ pkg/iam/mapping.go
@@ -2,10 +2,17 @@ package iam
 
 import "errors"
 
-// Identity represents an IAM identity.
+var (
+	ErrNeitherUserNorRole   = errors.New("arn is neither user nor role")
+	ErrNoKubernetesIdentity = errors.New("neither username nor groups is set for iam identity")
+)
+
+// Identity represents an IAM identity and its corresponding Kubernetes identity
 type Identity struct {
-	Username string   `json:"username"`
-	Groups   []string `json:"groups"`
+	UserARN  *ARN     `json:"userarn,omitempty"`
+	RoleARN  *ARN     `json:"rolearn,omitempty"`
+	Username *string  `json:"username,omitempty"`
+	Groups   []string `json:"groups,omitempty"`
 }
 
 // Valid ensures the identity is proper.
@@ -14,4 +21,30 @@ func (i Identity) Valid() error {
 		return errors.New("identity mapping needs at least 1 group")
 	}
 	return nil
+}
+
+func NewIdentity(arn ARN, username string, groups []string) (Identity, error) {
+	identity := Identity{}
+
+	if arn.User() {
+		identity.UserARN = &arn
+	} else if arn.Role() {
+		identity.RoleARN = &arn
+	} else {
+		return Identity{}, ErrNeitherUserNorRole
+	}
+
+	if username != "" {
+		identity.Username = &username
+	}
+
+	if len(groups) > 0 {
+		identity.Groups = groups
+	}
+
+	if identity.Username == nil && len(identity.Groups) == 0 {
+		return Identity{}, ErrNoKubernetesIdentity
+	}
+
+	return identity, nil
 }
