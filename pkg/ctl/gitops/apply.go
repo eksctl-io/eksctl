d1d2e3fb297bfd57858fd84deab5817184ce1370
Marc Carré <carre.marc@gmail.com>
2019-09-13 19:40:43 +0900

    Extract validation logic to options#validate method
    It makes sense for the `options` DTO to be able to validate itself.
    It also separate the "what" from the "how" of the validation logic, hence making `doApplyGitops` more readable.

--- pkg/ctl/gitops/apply.go
+++ pkg/ctl/gitops/apply.go
@@ -31,6 +31,19 @@ type options struct {
 	gitPrivateSSHKeyPath string
 }
 
+func (opts options) validate() error {
+	if opts.quickstartNameArg == "" {
+		return errors.New("please supply a valid gitops Quick Start URL or name in --quickstart-profile")
+	}
+	if err := opts.gitOptions.ValidateURL(); err != nil {
+		return errors.Wrap(err, "please supply a valid --git-url argument")
+	}
+	if opts.gitPrivateSSHKeyPath != "" && !file.Exists(opts.gitPrivateSSHKeyPath) {
+		return errors.New("please supply a valid --git-private-ssh-key-path argument")
+	}
+	return nil
+}
+
 func applyGitops(cmd *cmdutils.Cmd) {
 	cfg := api.NewClusterConfig()
 	cmd.ClusterConfig = cfg
@@ -71,15 +84,8 @@ func applyGitops(cmd *cmdutils.Cmd) {
 }
 
 func doApplyGitops(cmd *cmdutils.Cmd, opts options) error {
-	if opts.quickstartNameArg == "" {
-		return errors.New("please supply a valid gitops Quick Start URL or name in --quickstart-profile")
-	}
-
-	if err := opts.gitOptions.ValidateURL(); err != nil {
-		return errors.Wrap(err, "please supply a valid --git-url argument")
-	}
-	if opts.gitPrivateSSHKeyPath != "" && !file.Exists(opts.gitPrivateSSHKeyPath) {
-		return errors.New("please supply a valid --git-private-ssh-key-path argument")
+	if err := opts.validate(); err != nil {
+		return err
 	}
 
 	quickstartRepoURL, err := repoURLForQuickstart(opts.quickstartNameArg)
