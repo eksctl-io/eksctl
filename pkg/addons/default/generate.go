8369c6d7e5d1175296f455519df8d20fcd0d62b6
cpu1 <patwal.chetan@gmail.com>
2019-10-08 22:53:37 +0530

    Revert "Merge pull request #1401 from martina-if/fix-cni-generate-1.5.4"
    This reverts commit a8bf1f84bbf469cc1e95303c372f43911531a852, reversing
    changes made to 76ed7b5c5b20490f0c2303843ce891ffe1d18312.

--- pkg/addons/default/generate.go
+++ pkg/addons/default/generate.go
@@ -1,5 +1,5 @@
 package defaultaddons
 
-//go:generate curl --silent --location https://github.com/aws/amazon-vpc-cni-k8s/blob/release-1.5.4/config/v1.5/aws-k8s-cni.yaml?raw=1 --output assets/aws-node.yaml
+//go:generate curl --silent --location https://github.com/aws/amazon-vpc-cni-k8s/blob/dd631108e61a977809f9b1a1c40232637e734184/config/v1.5/aws-k8s-cni.yaml?raw=1 --output assets/aws-node.yaml
 
 //go:generate ${GOBIN}/go-bindata -pkg ${GOPACKAGE} -prefix assets -nometadata -o assets.go assets
