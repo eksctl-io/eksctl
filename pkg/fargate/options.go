5cf7408aafb7519cff398b47e744c0ed97961253
Marc Carré <carre.marc@gmail.com>
2019-11-13 14:30:33 +0900

    Fargate: add pkg/fargate.Options#Validate (+tests)

--- pkg/fargate/options.go
+++ pkg/fargate/options.go
@@ -1,6 +1,18 @@
 package fargate
 
+import (
+	"errors"
+)
+
 // Options groups the parameters required to interact with Fargate.
 type Options struct {
 	ProfileName string
+}
+
+// Validate validates this Options object's fields.
+func (o *Options) Validate() error {
+	if o.ProfileName == "" {
+		return errors.New("invalid Fargate profile: empty name")
+	}
+	return nil
 }
---
+++ pkg/fargate/options_test.go
@@ -0,0 +1,35 @@
+package fargate_test
+
+import (
+	"testing"
+
+	"github.com/bxcodec/faker"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/weaveworks/eksctl/pkg/fargate"
+	"github.com/weaveworks/eksctl/pkg/testutils"
+)
+
+func TestSuite(t *testing.T) {
+	testutils.RegisterAndRun(t)
+}
+
+var _ = Describe("fargate", func() {
+	Describe("Options", func() {
+		Describe("Validate", func() {
+			It("fails when profile name is empty", func() {
+				options := fargate.Options{}
+				err := options.Validate()
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(Equal("invalid Fargate profile: empty name"))
+			})
+			It("passes when profile name is not empty", func() {
+				options := fargate.Options{}
+				err := faker.FakeData(&options)
+				Expect(err).To(Not(HaveOccurred()))
+				err = options.Validate()
+				Expect(err).To(Not(HaveOccurred()))
+			})
+		})
+	})
+})
