8e0d289987df1c4a3069b98560176bf9f36cef7b
cpu1 <patwal.chetan@gmail.com>
2019-11-21 18:01:37 +0530

    Fix `only-missing` filter for nodegroups

--- pkg/ctl/delete/nodegroup.go
+++ pkg/ctl/delete/nodegroup.go
@@ -74,8 +74,7 @@ func doDeleteNodeGroup(cmd *cmdutils.Cmd, ng *api.NodeGroup, updateAuthConfigMap
 
 	if cmd.ClusterConfigFile != "" {
 		logger.Info("comparing %d nodegroups defined in the given config (%q) against remote state", len(cfg.NodeGroups), cmd.ClusterConfigFile)
-		allNodeGroups := cmdutils.ToKubeNodeGroups(cfg)
-		if err := ngFilter.SetIncludeOrExcludeMissingFilter(stackManager, onlyMissing, allNodeGroups); err != nil {
+		if err := ngFilter.SetIncludeOrExcludeMissingFilter(stackManager, onlyMissing, cfg); err != nil {
 			return err
 		}
 	} else {
