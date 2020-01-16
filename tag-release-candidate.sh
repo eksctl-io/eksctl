31223c2db19acbb326d43b26cc449fd89280a87f
Martina Iglesias Fernandez <martina@weave.works>
2020-01-16 15:54:43 +0100

    Implement rc scripts

--- tag-release-candidate.sh
+++ tag-release-candidate.sh
@@ -4,29 +4,6 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-if [ "$#" -ne 0 ] ; then
-  echo "Usage: ${0}. Got at least one extra argument"
-  exit 1
-fi
-
-candidate_for="${v/-rc.*}"
-release_branch="release-${candidate_for%.*}"  # e.g.: 0.2.0-rc.0 -> release-0.2
-
-if [ "${v}" = "${candidate_for}" ] ; then
-  echo "Must provide release candidate tag, use './tag-release.sh ${v}' instead"
-  exit 2
-fi
-
-if ! [[ "${release_branch}" =~ ^release-[0-9]+\.[0-9]+$ ]] ; then
-  echo "Invalid release branch: ${release_branch}"
-  exit 3
-fi
-
-#if [[ ! "$(git remote get-url origin)" =~ ^git@github.com:weaveworks/eksctl(\.git)?$ ]] ; then
-#  echo "Invalid origin: $(git remote get-url origin)"
-#  exit 3
-#fi
-
 function branch_exists() {
   git ls-remote --heads origin "${1}" | grep -q "${1}"
 }
@@ -35,35 +12,27 @@ function current_branch() {
   git rev-parse --abbrev-ref @
 }
 
-if ! branch_exists "${release_branch}" ; then
-  git checkout master
-  if [ ! "$(current_branch)" = master ] ; then
-    echo "Must be on master branch"
-    exit 4
-  fi
-  # Ensure local master is up-to-date by pulling its latest version from origin
-  # and fast-forwarding local master:
-  git fetch origin master
-  git merge --ff-only origin/master
-  # Create the release branch:
-  git push origin master:"${release_branch}"
+#if [[ ! "$(git remote get-url origin)" =~ ^git@github.com:weaveworks/eksctl(\.git)?$ ]] ; then
+#  echo "Invalid origin: $(git remote get-url origin)"
+#  exit 3
+#fi
 
-  # Prepare next development iteration in master
-  go run pkg/version/generate/release_generate.go development
-  git add ./pkg/version/release.go
-  git commit --message "Prepare for next development iteration"
-  git push origin master:master
+candidate_for=$(go run pkg/version/generate/release_generate.go print-version)
+
+release_branch="release-${candidate_for}"  # e.g.: 0.2.0-rc.0 -> release-0.2.0
+if ! [[ "${release_branch}" =~ ^release-[0-9]+\.[0-9]+\.[0-9]+$ ]] ; then
+  echo "Invalid release branch: ${release_branch}"
+  exit 3
 fi
 
-# Ensure local release branch is up-to-date by pulling its latest version from
-# origin and fast-forwarding the local branch:
-git fetch origin "${release_branch}"
-git checkout "${release_branch}"
 if [ ! "$(current_branch)" = "${release_branch}" ] ; then
   echo "Must be on ${release_branch} branch"
   exit 5
 fi
-git merge --ff-only origin/"${release_branch}"
+
+# Ensure local release branch is up-to-date by pulling its latest version from
+# origin and fast-forwarding the local branch:
+git pull --ff-only origin "${release_branch}" || echo "${release_branch} not found in origin. Will push new branch upstream"
 
 RELEASE_NOTES_FILE="docs/release_notes/${candidate_for}.md"
 
@@ -72,15 +41,14 @@ if [[ ! -f "${RELEASE_NOTES_FILE}" ]]; then
   exit 6
 fi
 
-export RELEASE_GIT_TAG="${v}"
 
 # Update eksctl version
-go run pkg/version/generate/release_generate.go release-candidate
-v=$(go run pkg/version/generate/release_generate.go print-version)
+full_version=$(go run pkg/version/generate/release_generate.go release-candidate)
+export RELEASE_GIT_TAG="${full_version}"
 git add ./pkg/version/release.go
 git add "${RELEASE_NOTES_FILE}"
 
-m="Tag ${v} release candidate"
+m="Tag ${full_version} release candidate"
 
 git commit --message "${m}"
 
@@ -88,6 +56,6 @@ git fetch --force --tags origin
 
 git push origin "${release_branch}"
 
-git tag --annotate --message "${m}" "${v}"
+git tag --annotate --message "${m}" "${full_version}"
 
 git push --force --tags origin
