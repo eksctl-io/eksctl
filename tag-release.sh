9f7ec753bda6a38abfd0e3b850c624e5f51ce91d
Martina Iglesias Fernandez <martina@weave.works>
2020-01-16 14:47:50 +0100

    Fixing tag-release.sh

--- tag-release.sh
+++ tag-release.sh
@@ -4,29 +4,6 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-if [ "$#" -ne 1 ] ; then
-  echo "Usage: ${0} <tag>"
-  exit 1
-fi
-
-v="${1}"
-release_branch="release-${v%.*}"  # e.g.: 0.2.0 -> release-0.2
-
-if [ ! "${v}" = "${v/-rc.*}" ] ; then
-  echo "Must provide release tag, use './tag-release-candidate.sh ${v}' instead"
-  exit 2
-fi
-
-if ! [[ "${release_branch}" =~ ^release-[0-9]+\.[0-9]+$ ]] ; then
-  echo "Invalid release branch: ${release_branch}"
-  exit 3
-fi
-
-#if [[ ! "$(git remote get-url origin)" =~ ^git@github.com:weaveworks/eksctl(\.git)?$ ]] ; then
-#  echo "Invalid origin: $(git remote get-url origin)"
-#  exit 3
-#fi
-
 function branch_exists() {
   git ls-remote --heads origin "${1}" | grep -q "${1}"
 }
@@ -35,38 +12,29 @@ function current_branch() {
   git rev-parse --abbrev-ref @
 }
 
-if ! branch_exists "${release_branch}" ; then
-  git checkout master
-  if [ ! "$(current_branch)" = master ] ; then
-    echo "Must be on master branch"
-    exit 7
-  fi
-  # Ensure local master is up-to-date by pulling its latest version from origin
-  # and fast-forwarding local master:
-  git fetch origin master
-  git merge --ff-only origin/master
-  # Create the release branch:
-  git push origin master:"${release_branch}"
+#if [[ ! "$(git remote get-url origin)" =~ ^git@github.com:weaveworks/eksctl(\.git)?$ ]] ; then
+#  echo "Invalid origin: $(git remote get-url origin)"
+#  exit 3
+#fi
 
-  # Prepare next development iteration in master
-  go run pkg/version/generate/release_generate.go development
-  git add ./pkg/version/release.go
-  git commit --message "Prepare for next development iteration"
-  git push origin master:master
+v=$(go run pkg/version/generate/release_generate.go print-version)
+
+release_branch="release-${v}"  # e.g.: 0.2.0 -> release-0.2.0
+if ! [[ "${release_branch}" =~ ^release-[0-9]+\.[0-9]+\.[0-9]+$ ]] ; then
+  echo "Invalid release branch: ${release_branch}"
+  exit 3
 fi
 
-# Ensure local release branch is up-to-date by pulling its latest version from
-# origin and fast-forwarding the local branch:
-git fetch origin "${release_branch}"
-git checkout "${release_branch}"
 if [ ! "$(current_branch)" = "${release_branch}" ] ; then
   echo "Must be on ${release_branch} branch"
   exit 5
 fi
-git merge --ff-only origin/"${release_branch}"
+
+# Ensure local release branch is up-to-date by pulling its latest version from
+# origin and fast-forwarding the local branch:
+git pull --ff-only origin "${release_branch}"
 
 RELEASE_NOTES_FILE="docs/release_notes/${v}.md"
-
 if [[ ! -f "${RELEASE_NOTES_FILE}" ]]; then
   echo "Must have release notes ${RELEASE_NOTES_FILE}"
   exit 6
@@ -76,29 +44,45 @@ export RELEASE_GIT_TAG="${v}"
 
 # Update eksctl version by removing the pre-release id
 go run pkg/version/generate/release_generate.go release
-code_version=$(go run pkg/version/generate/release_generate.go print-version)
-
-if [ "${v}" != "${code_version}" ]; then
-  echo "Version mismatch: eksctl version ${code_version}, branch version ${v}"
-  exit 7
-fi
-
 git add ./pkg/version/release.go
 git add "${RELEASE_NOTES_FILE}"
 
 m="Release ${v}"
 
 git commit --message "${m}"
-
-git fetch --force --tags origin
-
-git push origin "${release_branch}"
+echo git push origin "${release_branch}"
 
 # Create the release tag and push it to start release process
 git tag --annotate --message "${m}" --force "latest_release"
 git tag --annotate --message "${m}" "${v}"
-git push origin "${v}"
+echo git push origin "${v}"
 
 # Update the site by putting everything from master into the docs branch
-git push --force origin "${release_branch}":docs
+echo git push --force origin "${release_branch}":docs
 
+### TODO if master is not dev then next dev iteration
+#if ! branch_exists "${release_branch}" ; then
+#  git checkout master
+#  if [ ! "$(current_branch)" = master ] ; then
+#    echo "Must be on master branch"
+#    exit 7
+#  fi
+#  # Ensure local master is up-to-date by pulling its latest version from origin
+#  # and fast-forwarding local master:
+#  git fetch origin master
+#  git merge --ff-only origin/master
+#
+#  if [[ ! -f "${RELEASE_NOTES_FILE}" ]]; then
+#    echo "Must have release notes ${RELEASE_NOTES_FILE}"
+#    exit 6
+# fi
+#
+#  # Create the release branch:
+#  git push origin master:"${release_branch}"
+#
+#  # Prepare next development iteration in master
+#  go run pkg/version/generate/release_generate.go development
+#  git add ./pkg/version/release.go
+#  git commit --message "Prepare for next development iteration"
+#  git push origin master:master
+#fi
