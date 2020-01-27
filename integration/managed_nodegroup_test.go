56578f6608297032f4bc2e8a8ef5a8ae5d0b25df
Martina Iglesias Fernandez <martina@weave.works>
2020-01-27 16:38:16 +0100

    Fix IT failures

--- integration/managed_nodegroup_test.go
+++ integration/managed_nodegroup_test.go
@@ -25,7 +25,7 @@ var _ = Describe("(Integration) Create Managed Nodegroups", func() {
 		newNodeGroup     = "ng-1"
 	)
 
-	defaultTimeout := 10 * time.Minute
+	defaultTimeout := 15 * time.Minute
 
 	Describe("when creating a cluster with 1 managed nodegroup", func() {
 		// always generate a unique name
