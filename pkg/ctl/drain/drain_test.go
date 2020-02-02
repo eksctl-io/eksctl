18506ade6c773991594170728e45cf3517337263
sayboras <sayboras@yahoo.com>
2020-02-02 23:14:13 +1100

    Add unit test for drain commands

--- pkg/ctl/drain/drain_test.go
+++ pkg/ctl/drain/drain_test.go
@@ -11,21 +11,21 @@ import (
 var _ = Describe("drain", func() {
 	Describe("invalid-resource", func() {
 		It("with no flag", func() {
-			cmd := newMockCmd("invalid-resource")
+			cmd := newMockDefaultCmd("invalid-resource")
 			out, err := cmd.execute()
 			Expect(err).To(HaveOccurred())
 			Expect(err.Error()).To(Equal("unknown command \"invalid-resource\" for \"drain\""))
 			Expect(out).To(ContainSubstring("usage"))
 		})
 		It("with invalid-resource and some flag", func() {
-			cmd := newMockCmd("invalid-resource", "--invalid-flag", "foo")
+			cmd := newMockDefaultCmd("invalid-resource", "--invalid-flag", "foo")
 			out, err := cmd.execute()
 			Expect(err).To(HaveOccurred())
 			Expect(err.Error()).To(Equal("unknown command \"invalid-resource\" for \"drain\""))
 			Expect(out).To(ContainSubstring("usage"))
 		})
 		It("with invalid-resource and additional argument", func() {
-			cmd := newMockCmd("invalid-resource", "foo")
+			cmd := newMockDefaultCmd("invalid-resource", "foo")
 			out, err := cmd.execute()
 			Expect(err).To(HaveOccurred())
 			Expect(err.Error()).To(Equal("unknown command \"invalid-resource\" for \"drain\""))
@@ -34,9 +34,16 @@ var _ = Describe("drain", func() {
 	})
 })
 
-func newMockCmd(args ...string) *mockVerbCmd {
-	flagGrouping := cmdutils.NewGrouping()
-	cmd := Command(flagGrouping)
+func newMockDefaultCmd(args ...string) *mockVerbCmd {
+	cmd := Command(cmdutils.NewGrouping())
+	cmd.SetArgs(args)
+	return &mockVerbCmd{
+		parentCmd: cmd,
+	}
+}
+
+func newMockEmptyCmd(args ...string) *mockVerbCmd {
+	cmd := cmdutils.NewVerbCmd("get", "Get resource(s)", "")
 	cmd.SetArgs(args)
 	return &mockVerbCmd{
 		parentCmd: cmd,
---
+++ pkg/ctl/drain/nodegroup_test.go
@@ -0,0 +1,81 @@
+package drain
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/weaveworks/eksctl/pkg/apis/eksctl.io/v1alpha5"
+	"github.com/weaveworks/eksctl/pkg/ctl/cmdutils"
+)
+
+var _ = Describe("drain", func() {
+	Describe("nodegroup", func() {
+		It("with valid details", func() {
+			count := 0
+			cmd := newMockEmptyCmd("nodegroup", "--cluster", "clusterName", "--name", "ng")
+			cmdutils.AddResourceCmd(cmdutils.NewGrouping(), cmd.parentCmd, func(cmd *cmdutils.Cmd) {
+				drainNodeGroupWithRunFunc(cmd, func(cmd *cmdutils.Cmd, ng *v1alpha5.NodeGroup, undo, onlyMissing bool) error {
+					Expect(cmd.ClusterConfig.Metadata.Name).To(Equal("clusterName"))
+					Expect(ng.Name).To(Equal("ng"))
+					count++
+					return nil
+				})
+			})
+			_, err := cmd.execute()
+			Expect(err).To(Not(HaveOccurred()))
+			Expect(count).To(Equal(1))
+		})
+
+		It("with deprecated flag --only", func() {
+			count := 0
+			cmd := newMockEmptyCmd("nodegroup", "--cluster", "clusterName", "--name", "ng", "--only", "ng")
+			cmdutils.AddResourceCmd(cmdutils.NewGrouping(), cmd.parentCmd, func(cmd *cmdutils.Cmd) {
+				drainNodeGroupWithRunFunc(cmd, func(cmd *cmdutils.Cmd, ng *v1alpha5.NodeGroup, undo, onlyMissing bool) error {
+					Expect(cmd.ClusterConfig.Metadata.Name).To(Equal("clusterName"))
+					Expect(cmd.Include).To(ContainElement("ng"))
+					count++
+					return nil
+				})
+			})
+			out, err := cmd.execute()
+			Expect(err).To(Not(HaveOccurred()))
+			Expect(count).To(Equal(1))
+			Expect(out).To(ContainSubstring("Flag --only has been deprecated, use --include"))
+		})
+
+		It("with cluster name as flag", func() {
+			count := 0
+			cmd := newMockEmptyCmd("nodegroup", "--cluster", "clusterName")
+			cmdutils.AddResourceCmd(cmdutils.NewGrouping(), cmd.parentCmd, func(cmd *cmdutils.Cmd) {
+				drainNodeGroupWithRunFunc(cmd, func(cmd *cmdutils.Cmd, ng *v1alpha5.NodeGroup, undo, onlyMissing bool) error {
+					Expect(cmd.ClusterConfig.Metadata.Name).To(Equal("clusterName"))
+					count++
+					return nil
+				})
+			})
+			_, err := cmd.execute()
+			Expect(err).To(Not(HaveOccurred()))
+			Expect(count).To(Equal(1))
+		})
+
+		It("missing required flag --cluster", func() {
+			cmd := newMockDefaultCmd("nodegroup")
+			_, err := cmd.execute()
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(Equal("--cluster must be set"))
+		})
+
+		It("setting --name and argument at the same time", func() {
+			cmd := newMockDefaultCmd("nodegroup", "ng", "--cluster", "dummy", "--name", "dummy")
+			_, err := cmd.execute()
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(Equal("--cluster=dummy and argument ng cannot be used at the same time"))
+		})
+
+		It("invalid flag", func() {
+			cmd := newMockDefaultCmd("nodegroup", "--invalid", "dummy")
+			_, err := cmd.execute()
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(Equal("unknown flag: --invalid"))
+		})
+	})
+})
