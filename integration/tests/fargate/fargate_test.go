4ed41230cda78864f1140f7e12ff6312e8e6fa7e
Marc Carré <carre.marc@gmail.com>
2020-02-20 20:43:36 +0900

    Overriding ITs region should properly regenerate commands

--- integration/tests/fargate/fargate_test.go
+++ integration/tests/fargate/fargate_test.go
@@ -24,8 +24,8 @@ func init() {
 	// Call testing.Init() prior to tests.NewParams(), as otherwise -test.* will not be recognised. See also: https://golang.org/doc/go1.13#testing
 	testing.Init()
 	params = tests.NewParams("fargate")
-	// Fargate is not supported in us-west-2 yet
-	params.Region = "ap-northeast-1"
+	// Fargate is not supported in us-west-2 yet:
+	params.SetRegion("ap-northeast-1")
 }
 
 func TestSuite(t *testing.T) {
