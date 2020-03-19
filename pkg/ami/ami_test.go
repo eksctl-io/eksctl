86f71bdae523c58be3ca09d1b009a2fc0f737d41
cpu1 <patwal.chetan@gmail.com>
2020-03-19 19:20:00 +0530

    Add more tests for ami.Use

--- pkg/ami/ami_test.go
+++ pkg/ami/ami_test.go
@@ -1,13 +1,14 @@
-package ami
+package ami_test
 
 import (
-	"strconv"
+	"fmt"
 	"strings"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/stretchr/testify/mock"
+	"github.com/weaveworks/eksctl/pkg/ami"
 	api "github.com/weaveworks/eksctl/pkg/apis/eksctl.io/v1alpha5"
 	"github.com/weaveworks/eksctl/pkg/testutils/mockprovider"
 )
@@ -16,8 +17,10 @@ func TestUseAMI(t *testing.T) {
 	amiTests := []struct {
 		blockDeviceMappings []*ec2.BlockDeviceMapping
 		rootDeviceName      string
+		description         string
 	}{
 		{
+			description:    "Root device mapping not at index 0 (Windows AMIs in some regions)",
 			rootDeviceName: "/dev/sda1",
 			blockDeviceMappings: []*ec2.BlockDeviceMapping{
 				{
@@ -36,12 +39,44 @@ func TestUseAMI(t *testing.T) {
 				},
 			},
 		},
+		{
+			description:    "Only one device mapping (AL2 AMIs)",
+			rootDeviceName: "/dev/sda1",
+			blockDeviceMappings: []*ec2.BlockDeviceMapping{
+				{
+					DeviceName: aws.String("/dev/sda1"),
+					Ebs: &ec2.EbsBlockDevice{
+						Encrypted: aws.Bool(true),
+					},
+				},
+			},
+		},
+		{
+			description:    "Different root device name",
+			rootDeviceName: "/dev/xvda",
+			blockDeviceMappings: []*ec2.BlockDeviceMapping{
+				{
+					DeviceName:  aws.String("xvdca"),
+					VirtualName: aws.String("ephemeral0"),
+				},
+				{
+					DeviceName: aws.String("/dev/xvda"),
+					Ebs: &ec2.EbsBlockDevice{
+						Encrypted: aws.Bool(true),
+					},
+				},
+				{
+					DeviceName:  aws.String("xvdcb"),
+					VirtualName: aws.String("ephemeral1"),
+				},
+			},
+		},
 	}
 
 	for i, tt := range amiTests {
-		t.Run(strconv.Itoa(i), func(t *testing.T) {
+		t.Run(fmt.Sprintf("%d: %s", i, tt.description), func(t *testing.T) {
 			mockProvider := mockDescribeImages(tt.blockDeviceMappings, tt.rootDeviceName)
-			err := Use(mockProvider.MockEC2(), &api.NodeGroup{
+			err := ami.Use(mockProvider.MockEC2(), &api.NodeGroup{
 				AMI: "ami-0121d8347f8191f90",
 			})
 
