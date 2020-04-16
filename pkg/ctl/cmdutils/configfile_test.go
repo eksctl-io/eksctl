52fc5dc23c1461b695371b41da484f1191e197ef
cpu1 <patwal.chetan@gmail.com>
2020-04-16 21:31:32 +0530

    Fix tests, update schema

--- pkg/ctl/cmdutils/configfile_test.go
+++ pkg/ctl/cmdutils/configfile_test.go
@@ -224,8 +224,8 @@ var _ = Describe("cmdutils configfile", func() {
 				{"05-advanced-nodegroups.yaml", 3, false, false},
 				{"07-ssh-keys.yaml", 6, true, false},
 				{"07-ssh-keys.yaml", 6, false, false},
-				{"15-managed-nodes.yaml", 1, true, true},
-				{"15-managed-nodes.yaml", 1, false, true},
+				{"15-managed-nodes.yaml", 2, true, true},
+				{"15-managed-nodes.yaml", 2, false, true},
 				{"20-bottlerocket.yaml", 2, false, false},
 			}
 
