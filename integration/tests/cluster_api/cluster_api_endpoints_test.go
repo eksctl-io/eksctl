6e8ae20790b25da1756ab41f3de32ceb75653e31
Martina Iglesias Fernandez <martina@weave.works>
2020-05-06 12:18:07 +0200

    Remove old test for Cluster API endpoint only private access
    This case is not easy to test in our infra. The cluster creation in this
    case will just timeout unless it is done from inside the VPC (and we
    don't have a good way to do that in IT).

--- integration/tests/cluster_api/cluster_api_endpoints_test.go
+++ integration/tests/cluster_api/cluster_api_endpoints_test.go
@@ -162,13 +162,6 @@ var _ = Describe("(Integration) Create and Update Cluster with Endpoint Configs"
 			Type:    createCluster,
 			Fails:   false,
 		}),
-		Entry("Create cluster2, Private=true, Public=false, should not succeed", endpointAccessCase{
-			Name:    "cluster2",
-			Private: true,
-			Public:  false,
-			Type:    createCluster,
-			Fails:   true,
-		}),
 		Entry("Create cluster3, Private=true, Public=true, should succeed", endpointAccessCase{
 			Name:    "cluster3",
 			Private: true,
