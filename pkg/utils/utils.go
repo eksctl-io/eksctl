36566ea6e82428ba3e79712330ece42626e709d0
Gilbert <artemisgray@gmail.com>
2020-05-07 09:11:27 -0700

    add new gpu instance support for inf1 (#2057)

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -11,7 +11,7 @@ import (
 // IsGPUInstanceType returns tru of the instance type is GPU
 // optimised.
 func IsGPUInstanceType(instanceType string) bool {
-	return strings.HasPrefix(instanceType, "p2") || strings.HasPrefix(instanceType, "p3") || strings.HasPrefix(instanceType, "g3") || strings.HasPrefix(instanceType, "g4")
+	return strings.HasPrefix(instanceType, "p2") || strings.HasPrefix(instanceType, "p3") || strings.HasPrefix(instanceType, "g3") || strings.HasPrefix(instanceType, "g4") || strings.HasPrefix(instanceType, "inf1")
 }
 
 // HasGPUInstanceType returns true if it finds a gpu instance among the mixed instances
