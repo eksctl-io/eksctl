06a9c2d393174bb5f964a21b2a4f6b5a92cdefd6
Smruti Tripathy <smrutirtripathy@gmail.com>
2020-05-11 21:05:15 +0530

    Fixing as per review feedback

--- cmd/eksctl/version.go
+++ cmd/eksctl/version.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/spf13/cobra"
 
-	"github.com/weaveworks/eksctl/pkg/apis/eksctl.io/v1alpha5"
 	"github.com/weaveworks/eksctl/pkg/ctl/cmdutils"
 	"github.com/weaveworks/eksctl/pkg/version"
 )
@@ -15,11 +14,11 @@ var output string
 func versionCmd(_ *cmdutils.FlagGrouping) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "version",
-		Short: "Output the version of eksctl and EKS server supported versions",
+		Short: "Output the version of eksctl",
 		RunE: func(_ *cobra.Command, _ []string) error {
 			switch output {
 			case "":
-				fmt.Printf("eksctl client version: %s\nEKS server supported versions: %s\n", version.GetVersion(), v1alpha5.SupportedVersions())
+				fmt.Printf("%s\n", version.GetVersion())
 			case "json":
 				fmt.Printf("%s\n", version.String())
 			default:
