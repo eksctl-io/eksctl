480d13a2900b2add962b5df674cdc8851bf2e5ed
Tam Mach <sayboras@yahoo.com>
2020-05-15 13:10:47 +1000

    Fix flags issue in version

--- cmd/eksctl/version.go
+++ cmd/eksctl/version.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 
+	"github.com/spf13/pflag"
+
 	"github.com/spf13/cobra"
 
 	"github.com/weaveworks/eksctl/pkg/ctl/cmdutils"
@@ -11,22 +13,21 @@ import (
 
 var output string
 
-func versionCmd(_ *cmdutils.FlagGrouping) *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "version",
-		Short: "Output the version of eksctl",
-		RunE: func(_ *cobra.Command, _ []string) error {
-			switch output {
-			case "":
-				fmt.Printf("%s\n", version.GetVersion())
-			case "json":
-				fmt.Printf("%s\n", version.String())
-			default:
-				return fmt.Errorf("unknown output: %s", output)
-			}
-			return nil
-		},
+func versionCmd(cmd *cmdutils.Cmd) {
+	cmd.SetDescription("version", "Output the version of eksctl", "")
+	cmd.FlagSetGroup.InFlagSet("General", func(fs *pflag.FlagSet) {
+		fs.StringVarP(&output, "output", "o", "", "specifies the output format (valid option: json)")
+	})
+	cmd.CobraCommand.Args = cobra.NoArgs
+	cmd.CobraCommand.RunE = func(_ *cobra.Command, args []string) error {
+		switch output {
+		case "":
+			fmt.Printf("%s\n", version.GetVersion())
+		case "json":
+			fmt.Printf("%s\n", version.String())
+		default:
+			return fmt.Errorf("unknown output: %s", output)
+		}
+		return nil
 	}
-	cmd.Flags().StringVarP(&output, "output", "o", "", "specifies the output format (valid option: json)")
-	return cmd
 }
