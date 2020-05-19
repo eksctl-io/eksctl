46aa1f7e7c37c3e0be8f2f2e91b634a0661944bd
Michael Beaumont <mjboamail@gmail.com>
2020-05-19 18:27:52 +0200

    Improve schema docs for IPs

--- cmd/schema/generate.go
+++ cmd/schema/generate.go
@@ -5,22 +5,33 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"reflect"
 	"strings"
 
 	"github.com/alecthomas/jsonschema"
 	api "github.com/weaveworks/eksctl/pkg/apis/eksctl.io"
 	v1alpha5 "github.com/weaveworks/eksctl/pkg/apis/eksctl.io/v1alpha5"
+	"github.com/weaveworks/eksctl/pkg/utils/ipnet"
 )
 
-func main() {
+func typeMapper(t reflect.Type) *jsonschema.Type {
+	if t == reflect.TypeOf(&ipnet.IPNet{}) {
+		return &jsonschema.Type{Type: "string"}
+	}
+	return nil
+}
 
+func main() {
 	if len(os.Args) != 2 {
 		panic("expected one argument with the output file")
 	}
 	outputFile := os.Args[1]
 
 	var document strings.Builder
-	schema := jsonschema.Reflect(&v1alpha5.ClusterConfig{})
+	reflector := jsonschema.Reflector{
+		TypeMapper: typeMapper,
+	}
+	schema := reflector.Reflect(&v1alpha5.ClusterConfig{})
 	// We have to manually add examples here, because we can't tag `TypeMeta`
 	// from the k8s package
 	if kind, ok := schema.Definitions["ClusterConfig"].Properties.Get("kind"); ok {
