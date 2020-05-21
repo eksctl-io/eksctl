eksctl-io/eksctl

commit 41189ed2cb6726a8b9a307607ff5bd3b054f8165

Add unit test data for 1.14, 1.15 and 1.16 (#2179)
* Add test data for 1.14, 1.15 and 1.16

Bump library version to 1.16.8 (default EKS 1.16 version)

* Add comment about go.mod replace

Co-authored-by: Michael Beaumont <mjboamail@gmail.com>

pkg/kubernetes/client.go
@@ -16,7 +16,6 @@ import (
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/apimachinery/pkg/runtime/serializer"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/cli-runtime/pkg/resource"
 	"k8s.io/client-go/discovery"
@@ -110,7 +109,7 @@ func (c *RawClient) new() (*RawClient, error) {
 		c.config.APIPath = "/api"
 	}
 	if c.config.NegotiatedSerializer == nil {
-		c.config.NegotiatedSerializer = &serializer.DirectCodecFactory{CodecFactory: scheme.Codecs}
+		c.config.NegotiatedSerializer = scheme.Codecs
 	}
 
 	if err := restclient.SetKubernetesDefaults(c.config); err != nil {
@@ -346,8 +345,8 @@ func (r *RawResource) CreateOrReplace(plan bool) (string, error) {
 */
 
 // CreatePatchOrReplace attempts patching the resource before replacing it
-// TODO: it needs more testing and the issue sith strategicpatch has to be
-// undertood before we decide wheather to use it or not
+// TODO: it needs more testing and the issue with strategic patch has to be
+// understood before we decide whether to use it or not
 func (r *RawResource) CreatePatchOrReplace() error {
 	msg := func(verb string) { logger.Info("%s %q", verb, r) }
 

.docker/build_image_manifest
@@ -5,11 +5,11 @@
 "github.com/vektra/mockery v0.0.0-20181123154057-e78b021dcbb5"
 "github.com/weaveworks/github-release v0.6.3-0.20161024133933-73deea6af1e8"
 "golang.org/x/tools v0.0.0-20200301222351-066e0c02454c"
-"k8s.io/code-generator v0.15.11"
-"k8s.io/code-generator v0.15.11"
-"k8s.io/code-generator v0.15.11"
-"k8s.io/code-generator v0.15.11"
-"k8s.io/code-generator v0.15.11"
+"k8s.io/code-generator v0.16.8"
+"k8s.io/code-generator v0.16.8"
+"k8s.io/code-generator v0.16.8"
+"k8s.io/code-generator v0.16.8"
+"k8s.io/code-generator v0.16.8"
 100644 blob 6a6cdd52a76f51dc909850d047efd25bfb5a326e	Dockerfile
 100644 blob c82e75f3145cad2829f4906261adeb738bd33ce1	.requirements
 100755 blob 1ec4ce65165cf21e3767ee34d56fb635c51b4a5a	install-build-deps.sh

.docker/image_tag
@@ -1,1 +1,1 @@
-248dd42f7bd2492df53b29fd21238693ae3480d4
+8aadb537865e814adc51b48550c761a247a26829

go.mod
@@ -25,6 +25,7 @@ require (
 	github.com/justinbarrick/go-k8s-portforward v1.0.3
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51
 	github.com/kevinburke/go-bindata v3.15.0+incompatible
+	github.com/kr/fs v0.1.0 // indirect
 	github.com/kris-nova/logger v0.0.0-20181127235838-fd0d87064b06
 	github.com/kris-nova/lolgopher v0.0.0-20180921204813-313b3abb0d9b // indirect
 	github.com/kubicorn/kubicorn v0.0.0-20180829191017-06f6bce92acc
@@ -47,13 +48,13 @@ require (
 	github.com/whilp/git-urls v0.0.0-20160530060445-31bac0d230fa
 	golang.org/x/sys v0.0.0-20200428200454-593003d681fa // indirect
 	golang.org/x/tools v0.0.0-20200301222351-066e0c02454c
-	k8s.io/api v0.15.11
-	k8s.io/apiextensions-apiserver v0.15.11
-	k8s.io/apimachinery v0.15.11
-	k8s.io/cli-runtime v0.15.11
+	k8s.io/api v0.16.8
+	k8s.io/apiextensions-apiserver v0.16.8
+	k8s.io/apimachinery v0.16.8
+	k8s.io/cli-runtime v0.16.8
 	k8s.io/client-go v11.0.0+incompatible
-	k8s.io/cloud-provider v0.15.11
-	k8s.io/code-generator v0.15.11
+	k8s.io/cloud-provider v0.16.8
+	k8s.io/code-generator v0.16.8
 	k8s.io/kops v1.15.2
 	k8s.io/kubelet v0.0.0
 	k8s.io/legacy-cloud-providers v0.0.0
@@ -70,27 +71,30 @@ replace (
 	// Override version since auto-detected one fails with GOPROXY
 	github.com/census-instrumentation/opencensus-proto => github.com/census-instrumentation/opencensus-proto v0.2.0
 	github.com/googleapis/gnostic => github.com/googleapis/gnostic v0.3.1
+	// k8s.io/kops is still using old version of component-base
+	// which uses an older version of the following package
+	github.com/prometheus/client_golang => github.com/prometheus/client_golang v0.9.4
 	// Used to pin the k8s library versions regardless of what other dependencies enforce
-	k8s.io/api => k8s.io/api v0.15.11
-	k8s.io/apiextensions-apiserver => k8s.io/apiextensions-apiserver v0.15.11
-	k8s.io/apimachinery => k8s.io/apimachinery v0.15.11
-	k8s.io/apiserver => k8s.io/apiserver v0.15.11
-	k8s.io/cli-runtime => k8s.io/cli-runtime v0.15.11
-	k8s.io/client-go => k8s.io/client-go v0.15.11
-	k8s.io/cloud-provider => k8s.io/cloud-provider v0.15.11
-	k8s.io/cluster-bootstrap => k8s.io/cluster-bootstrap v0.15.11
-	k8s.io/code-generator => k8s.io/code-generator v0.15.11
-	k8s.io/component-base => k8s.io/component-base v0.15.11
-	k8s.io/cri-api => k8s.io/cri-api v0.15.11
-	k8s.io/csi-translation-lib => k8s.io/csi-translation-lib v0.15.11
-	k8s.io/kube-aggregator => k8s.io/kube-aggregator v0.15.11
-	k8s.io/kube-controller-manager => k8s.io/kube-controller-manager v0.15.11
-	k8s.io/kube-proxy => k8s.io/kube-proxy v0.15.11
-	k8s.io/kube-scheduler => k8s.io/kube-scheduler v0.15.11
-	k8s.io/kubectl => k8s.io/kubectl v0.15.11
-	k8s.io/kubelet => k8s.io/kubelet v0.15.11
-	k8s.io/kubernetes => k8s.io/kubernetes v1.15.11
-	k8s.io/legacy-cloud-providers => k8s.io/legacy-cloud-providers v0.15.11
-	k8s.io/metrics => k8s.io/metrics v0.15.11
-	k8s.io/sample-apiserver => k8s.io/sample-apiserver v0.15.11
+	k8s.io/api => k8s.io/api v0.16.8
+	k8s.io/apiextensions-apiserver => k8s.io/apiextensions-apiserver v0.16.8
+	k8s.io/apimachinery => k8s.io/apimachinery v0.16.8
+	k8s.io/apiserver => k8s.io/apiserver v0.16.8
+	k8s.io/cli-runtime => k8s.io/cli-runtime v0.16.8
+	k8s.io/client-go => k8s.io/client-go v0.16.8
+	k8s.io/cloud-provider => k8s.io/cloud-provider v0.16.8
+	k8s.io/cluster-bootstrap => k8s.io/cluster-bootstrap v0.16.8
+	k8s.io/code-generator => k8s.io/code-generator v0.16.8
+	k8s.io/component-base => k8s.io/component-base v0.16.8
+	k8s.io/cri-api => k8s.io/cri-api v0.16.8
+	k8s.io/csi-translation-lib => k8s.io/csi-translation-lib v0.16.8
+	k8s.io/kube-aggregator => k8s.io/kube-aggregator v0.16.8
+	k8s.io/kube-controller-manager => k8s.io/kube-controller-manager v0.16.8
+	k8s.io/kube-proxy => k8s.io/kube-proxy v0.16.8
+	k8s.io/kube-scheduler => k8s.io/kube-scheduler v0.16.8
+	k8s.io/kubectl => k8s.io/kubectl v0.16.8
+	k8s.io/kubelet => k8s.io/kubelet v0.16.8
+	k8s.io/kubernetes => k8s.io/kubernetes v1.16.8
+	k8s.io/legacy-cloud-providers => k8s.io/legacy-cloud-providers v0.16.8
+	k8s.io/metrics => k8s.io/metrics v0.16.8
+	k8s.io/sample-apiserver => k8s.io/sample-apiserver v0.16.8
 )

pkg/addons/default/assets.go
@@ -1,8 +1,6 @@
 // Code generated by go-bindata. DO NOT EDIT.
 // sources:
 // assets/aws-node.yaml (4.002kB)
-// assets/coredns-1.11.json (8.508kB)
-// assets/coredns-1.12.json (8.721kB)
 // assets/coredns-1.13.json (9.543kB)
 // assets/coredns-1.14.json (9.919kB)
 // assets/coredns-1.15.json (10.18kB)
@@ -92,46 +90,6 @@ func awsNodeYaml() (*asset, error) {
 
 	info := bindataFileInfo{name: "aws-node.yaml", size: 0, mode: os.FileMode(0), modTime: time.Unix(0, 0)}
 	a := &asset{bytes: bytes, info: info, digest: [32]uint8{0x22, 0x3e, 0x64, 0x24, 0x21, 0xb9, 0x92, 0x98, 0x4f, 0x96, 0xe3, 0xd3, 0xc5, 0x1a, 0x29, 0x6c, 0x89, 0x9a, 0x6a, 0xa4, 0x82, 0x2a, 0xc1, 0xe3, 0x1b, 0x46, 0xb6, 0xc2, 0x75, 0xa6, 0x3, 0x31}}
-	return a, nil
-}
-
-var _coredns111Json = []byte("\x1f\x8b\x08\x00\x00\x00\x00\x00\x00\xff\xe4\x19\xdb\x6e\xdb\xc8\xf5\xdd\x5f\x41\x10\xc8\x5b\x44\x59\x6b\x3b\x71\xf5\xe6\xb5\xdd\xac\x81\xd8\x15\x6c\x67\x5f\x9a\x60\x71\x34\x3c\x12\xa7\x1e\xce\x4c\xe7\xa2\x58\x1b\xf8\xdf\x8b\xe1\xfd\x32\xa4\x24\xd7\x69\x0b\x54\x0f\x09\x3d\xe7\xca\x73\x3f\xc3\x1f\x47\x41\x10\x82\xa4\xbf\xa3\xd2\x54\xf0\x70\x1e\x84\x9b\x59\xf8\xde\x9d\x52\x83\xa9\x0e\xe7\xc1\xdf\x8f\x82\x20\x08\x7e\x64\xff\x0e\x21\x67\x90\x27\xca\x63\x77\xf6\x80\x6a\x43\x09\xd6\x80\x14\x0d\xc4\x60\x20\x9c\x57\x6c\x1c\x23\xce\x85\x01\x43\x05\xd7\x2d\x40\x10\x84\x52\x89\x14\x4d\x82\x56\x47\x54\x4c\xa5\x50\xc6\xf1\xfd\xcb\xec\xec\xa4\x62\xea\xc1\xd3\x44\x81\x44\x87\x69\x94\xc5\xb0\x42\x7c\xa9\x69\x42\x06\x4b\x64\x3d\x79\xf8\xa4\x23\x48\xe1\x4f\xc1\xe1\xbb\x8e\x88\x48\xa7\x44\xa4\x52\x70\xe4\x99\xe0\x27\xbb\xc4\x49\xcc\x75\x5b\xf8\xd3\xb9\x9e\x80\x94\x23\x08\x76\x89\x8a\xa3\xc1\x4c\x3b\xc2\xac\x36\xa8\x26\xba\xb0\x4e\xa9\xe6\x08\x09\x87\x34\xc3\xbb\x14\x0a\xaf\xee\x1e\xfc\x6f\x54\x22\x79\x74\xc8\x60\x5a\x02\xa9\x11\xf4\x56\x1b\x4c\x4b\x4e\x15\x9f\x50\x4b\x24\x6d\xf7\x38\xab\xd7\xee\xcf\x7f\x4d\xa3\x35\x44\x77\xdf\xbc\xa0\x0e\xe7\xc1\xd9\x49\xf7\x5c\x09\x23\x88\x60\x8e\xec\xcb\xd5\xa2\x4b\x66\x40\xad\xd1\x2c\x4a\xe2\x06\xf0\xe5\xfd\x5e\x7a\x4c\x0c\x91\xaf\xd1\xe5\xf1\xf2\x10\x5d\xaa\xe7\x6f\x0d\x63\x6b\x64\x48\x8c\x50\xdd\xe0\xf2\xc5\x89\xd7\x93\x1a\xb5\x4b\xab\x8b\xd5\x8a\x72\x6a\xb6\x8e\xe0\x4e\xf0\x66\x84\x84\x66\x9b\x07\xf8\x65\x1e\x4c\x37\x8b\xca\x93\x47\x0d\x6e\x87\xe7\xea\x05\x21\xc2\x72\xb3\x2b\x65\x5f\x95\x3d\x44\x28\xdc\x2b\x79\x46\xc3\xbb\xcf\x65\x77\x74\x1f\x68\x93\xfe\xfb\xba\xc4\x5b\x51\x96\x09\x88\xe6\x67\x27\xc1\x8f\xaf\xdc\x01\x50\x29\xa1\x74\xfe\x9c\x20\x30\x93\xe4\xcf\x75\xfa\x06\x45\xba\x47\x4c\x10\x60\x01\xe5\x13\x88\x63\x15\x81\x92\x10\x50\xf9\x21\x7f\x28\xb8\x05\x81\x14\xb1\x0e\x28\xd7\x48\xac\xc2\xf2\xd0\x4a\x6d\x14\x42\x5a\xfe\xbd\x02\xc6\x4c\xa2\x84\x5d\x27\x7e\x7e\x39\xe2\x4b\xfe\x5f\x5d\x1a\x83\xb9\x2b\x9c\xd5\xe9\xf3\x36\x88\x82\x29\x1a\x32\x55\xa8\x05\xdb\x44\x44\xf0\x55\x0e\x25\x40\x12\x0c\x4e\x8e\xbf\xf2\x97\xaf\xbc\x5f\x23\xca\x98\xb9\x14\x7c\x45\xd7\xb7\x20\xff\xff\xc2\x05\xa4\xd4\x53\x5f\x1e\x5d\xa1\x64\x62\x9b\xe2\xee\x1c\xea\xb4\xbd\x7f\xbb\x39\xed\x6d\xaf\xb7\x6a\x34\xaf\x31\xed\x58\x9f\x51\x62\xad\x50\xeb\x2b\x84\x98\x51\x8e\x0f\x48\x04\x8f\x9d\x15\x7e\x99\x9d\x7e\x3c\x3d\x3f\xf9\x70\xfa\xb1\x21\x4a\xa1\x64\x94\x40\x06\x6f\x1d\x6f\xa8\x73\xd2\x6f\x54\x1b\xa1\xb6\x9f\x69\x4a\x9d\x75\x66\xc7\x7b\x14\xe8\x14\x0c\x49\x3e\xfb\x6c\xff\x3a\xeb\xef\x8a\xd7\x66\x03\x69\xd5\x7f\xa3\xc0\xe0\x7a\xdb\xd5\x4f\x09\xc6\x28\x5f\x7f\x91\x31\x18\xec\x6b\x98\xc2\xf3\x83\x55\x6b\x07\x99\xbd\xef\x81\xbe\x70\xd8\x00\x65\xb0\xcc\xaa\xd8\x6c\xa8\xa3\x56\x9d\xe5\xbe\x25\xcc\xab\xa7\xc1\x54\xb2\xbe\x2a\xfe\x78\xcf\x20\x44\x61\x16\xf1\x8f\x34\x45\x6d\x20\x75\x86\xe1\x96\xb1\x8e\xba\xde\xf8\x7f\xbd\x17\x76\xfb\xa1\xe9\x89\xae\x41\x7a\x91\x9a\x9d\x42\xdd\x9c\x7b\x4a\x72\x11\xe3\xc5\x30\x3c\x8b\xd2\x7f\x5a\xaa\x30\xbe\xb2\x8a\xf2\xf5\x03\x49\x30\xb6\xce\xda\x37\x6b\x2e\xaa\xe3\xeb\x67\x24\xd6\xe4\x05\xa7\xcf\xa3\x90\xf3\x50\xc4\xf2\x23\xaa\xb4\x3b\xa5\xd5\x3f\x1f\x79\x50\x85\xfc\xf5\xb3\x54\xf9\xc8\x31\xcc\x61\x8c\x4b\xc6\xe9\x09\xb3\x41\x65\x89\x06\xa2\x76\x51\x11\x7d\x7f\xb4\x28\x85\x44\x05\x79\x3a\x86\x37\x7c\x1c\x77\x03\xcc\xe2\xb8\x96\x19\x1e\xa3\xdc\x3e\x87\x23\x38\xdf\x06\x61\x2f\xc3\x0a\xbc\xd2\x00\xa0\x48\xf2\x9f\x37\x01\xa4\xf1\x87\xd3\x57\x9a\x60\x00\xe2\xa7\xf0\x61\xf7\x31\xbb\x58\xed\xbf\x3b\x46\x0f\x89\xe0\x06\x28\x47\xe5\x7b\x51\x4f\x42\x81\x5a\x0f\x99\x24\x9c\xb8\xc1\xc6\x6b\xd3\x30\x1b\x7f\x8a\xa2\x31\xad\x86\xbc\x1e\xe6\xb7\x3e\x71\x48\x53\xc8\x4a\x6d\xf8\x4e\x47\xf1\x93\x8a\x90\xa8\xe8\x9d\x8e\xde\xe9\x29\x3e\xe9\x92\xe7\x7c\x33\x8b\x66\xd1\x89\x47\x78\x4e\xbf\xb0\x8c\x2d\x04\xa3\x24\x0b\x9d\x9b\xd5\x9d\x30\x0b\x85\xba\x39\x3d\x34\x48\x18\xdd\x20\x47\xad\x17\x4a\x2c\xfb\xf5\xbf\x40\x5a\x01\x65\x56\xe1\x63\xa2\x50\x27\x82\xb9\xa1\xe4\xcc\xfb\xee\x89\x31\xf2\x13\x9a\x01\x3e\xae\x21\x83\x49\x9c\x5a\xd3\x7c\xb2\x1d\x88\xca\x72\xab\x3a\x3f\x3e\x3f\x1e\xc0\xd0\x24\xc1\x7c\x64\xf8\xed\xf1\x71\xe1\x0b\x49\x6f\xce\x85\xae\x80\x52\x60\x57\xc8\x60\x5b\x4f\x03\x1f\xbc\x62\x42\x89\x8a\x8a\xb8\x46\x9b\xf9\xd1\xb4\x25\x04\xb5\x6e\x9a\x67\xe6\x45\x34\x34\x45\x61\x4d\xcd\xf0\xac\x1f\xd2\x1e\x1f\x0d\x0f\x47\xb5\xa6\x9e\x85\xba\xfc\x0d\xb8\xa2\x4a\x87\x85\x7f\x85\xed\x89\xf7\x8b\x0e\x7c\x9b\xf7\xbe\xee\x78\x43\xd5\x3c\xcb\xb9\x57\x3d\xb7\x8c\xbf\xb1\x7a\x6e\x05\xda\xa1\x60\x8a\x46\x51\xb2\x97\xfd\x86\x14\xdc\xab\x82\xb8\xb5\xcb\x2a\x82\xbe\x41\x27\xc8\xf3\x3d\xa5\x66\x08\x9a\xcd\x59\xa9\x50\x59\xe5\x98\x7d\x3c\xbe\xa5\xfb\x27\x96\x1b\x3f\x50\x8f\xb1\x26\xd2\x66\x7c\x8f\x8f\xd3\x21\x3b\xd4\xd2\x07\x85\xef\x95\x31\xd9\xb2\x4b\xcd\xf6\x52\x70\x83\xcf\x43\xf5\x28\x04\xc6\xc4\xf7\x85\xa2\x1b\xca\x70\x8d\xd7\x9a\x00\x83\x62\x38\x5a\x01\xd3\xe8\x7d\x51\x02\x12\x96\x94\x51\x43\x07\xad\xec\x58\xc7\xf1\x48\x3b\x0d\xef\xae\x1f\xff\xf8\xf5\xe6\xee\xea\x8f\x87\xeb\xfb\xdf\x6f\x2e\xaf\xfd\x3d\xd5\xe3\xe1\x8c\x3a\x56\x42\x8e\x71\x07\xc6\x06\x18\x1e\xe0\x4e\x88\xff\xc6\xd9\xf6\x5e\x08\xf3\x57\xca\xb0\xd8\xb9\xe6\x81\x51\x16\xf7\xf2\x81\x41\x95\x52\x9e\xd9\xf3\x16\xb5\x76\x9d\xa9\x2c\xfe\x31\x6e\xa6\x0d\xf0\x84\x89\xb5\xaf\xa8\x79\x38\x54\x7d\xcd\xe9\xe4\xa3\xd9\x08\x66\x53\xbc\x15\x96\x1f\x5a\x0f\x53\x47\x53\xa9\xd8\x68\xe2\x43\xc1\x5a\x97\x65\xbe\xa2\xeb\x49\x2e\x79\x08\xb9\xb4\xe7\x90\x05\xbd\x09\x3e\x3a\xde\x74\x82\xc3\xd5\xc0\xda\x3c\x57\xb8\x02\xcb\xba\x1d\x3f\x94\x8a\x8a\x2c\x2f\x18\x68\x7d\x57\xe8\x9f\xbb\x76\xe2\x26\xff\x09\x51\xd4\x50\x02\xac\x4b\xa8\xdc\x6a\xa5\x4c\x2d\xe0\x82\x7d\x87\x6d\x6f\x35\xd5\xf9\xd6\x81\xaa\xe4\x1d\xe7\x7a\x4c\x2a\x40\x8f\xa2\x9f\xa9\xdd\xb9\x4d\xb7\xaf\x11\x87\x17\xe3\x36\xe2\xdd\x68\xd7\x6c\x06\xd7\x27\x05\x04\x17\x9d\x5e\x7f\x72\xdc\x25\x10\xcc\x0d\xd4\x03\x1b\x8d\x67\x7e\x2c\x86\xf7\xcb\xc2\xa4\x17\x71\x2c\xb8\xce\x62\xc0\x13\xb7\xcd\x69\xfd\xfa\x99\xba\x42\x7a\x90\xf7\xf3\xe8\xdb\x53\x33\x52\x5d\xb2\x0d\x14\xc6\xc2\x6d\xb7\x22\x76\x26\x3c\xfd\xc5\x3f\xf7\xb4\x3f\xe2\x74\x7f\x83\xfb\x61\x69\x97\x72\x34\x1e\xda\x4c\xaa\x61\x71\x78\x88\x0e\x86\x56\x05\xaf\xbe\xdd\x39\xea\xc0\xe1\xab\x99\xe5\xe3\xbe\x39\xf2\x41\xca\xa7\x7d\xae\x02\xd5\x12\x48\x04\xd6\x24\x42\xd1\x3f\xb3\x98\x8b\x9e\xce\xb3\xbd\xcf\x77\x3f\x58\x5c\xd8\xdf\x0b\xb6\xf3\xbb\xd8\x7f\xed\x16\x70\x29\x84\xd1\x46\x81\x94\x94\xaf\xcb\x57\x9c\x14\x71\xb6\xe3\xbe\x35\x2f\x50\xf3\x8e\xdb\xea\x7b\x3f\x65\x59\x27\xf2\x5b\x6f\x07\x92\x7e\x52\xc2\xca\x7e\xa8\x86\x4d\x3f\xb6\x62\xa6\x35\x48\x75\x88\x90\xc7\x52\x50\xd7\x5f\xfc\xf5\xc7\xf3\xbd\x2a\xee\x9d\x55\xb7\x9a\x7a\x58\x87\x0d\xaa\xa5\x47\x3e\xa3\xba\x57\xd8\xbf\x83\x21\x49\x8b\x93\xcf\xa2\x3f\xd5\x2c\xae\x7f\x1c\xfe\x32\x6b\x34\x03\x6a\x1f\x35\x4f\x7e\x52\xba\xfc\x4a\x79\xec\xe2\xf1\xf5\x5f\x93\x7d\xa2\xdb\x17\x35\xd6\x08\x5b\xde\xae\xbe\xf5\xc7\xe3\xff\xf9\xcc\x14\x0c\xef\x71\xd5\xb1\x66\x11\x78\xa3\x9e\x6b\x7e\x01\x18\xab\x73\x07\x28\xa3\xed\xf2\x1f\x48\xcc\x48\xa5\xd8\xf1\xe1\x32\xd8\xb5\x8c\xef\xfc\x56\xd1\x0b\xeb\xa3\x22\x4d\x2a\xd1\x9f\x5d\x72\x1f\xbd\x1c\xfd\x2b\x00\x00\xff\xff\xe1\xef\x14\x36\x3c\x21\x00\x00")
-
-func coredns111JsonBytes() ([]byte, error) {
-	return bindataRead(
-		_coredns111Json,
-		"coredns-1.11.json",
-	)
-}
-
-func coredns111Json() (*asset, error) {
-	bytes, err := coredns111JsonBytes()
-	if err != nil {
-		return nil, err
-	}
-
-	info := bindataFileInfo{name: "coredns-1.11.json", size: 0, mode: os.FileMode(0), modTime: time.Unix(0, 0)}
-	a := &asset{bytes: bytes, info: info, digest: [32]uint8{0x33, 0x5e, 0x21, 0xca, 0xae, 0x65, 0x51, 0x47, 0xb, 0x4b, 0x8, 0x18, 0x2, 0xde, 0x48, 0x1, 0x24, 0x6, 0x20, 0x12, 0x5b, 0xd3, 0xa0, 0x99, 0x47, 0x2b, 0x13, 0x8b, 0x0, 0x22, 0x9, 0x50}}
-	return a, nil
-}
-
-var _coredns112Json = []byte("\x1f\x8b\x08\x00\x00\x00\x00\x00\x00\xff\xe4\x19\x5d\x6f\x1b\xb9\xf1\xdd\xbf\x62\xb1\x40\xde\xa2\x95\x15\xc7\x89\xab\x37\x9f\xed\xe6\x0c\xc4\xae\x60\x3b\xf7\xd2\x04\x87\x11\x77\x24\xb1\xe6\x92\xec\x90\xab\x58\x17\xf8\xbf\x17\xdc\xef\x0f\xee\x4a\x76\x73\x6d\x81\xea\x21\x59\x93\xf3\xc5\xf9\x1e\xf2\xc7\x51\x10\x84\xa0\xf9\x6f\x48\x86\x2b\x19\xce\x83\x70\x3b\x0b\xdf\xba\x55\x6e\x31\x31\xe1\x3c\xf8\xfb\x51\x10\x04\xc1\x8f\xec\xdf\x21\xe0\x6c\xe7\x91\xcb\xd8\xad\xdd\x23\x6d\x39\xc3\x7a\x23\x41\x0b\x31\x58\x08\xe7\x15\x19\x47\x48\x4a\x65\xc1\x72\x25\x4d\x6b\x23\x08\x42\x4d\x2a\x41\xbb\xc1\xd4\x44\x5c\x4d\xb5\x22\xeb\xe8\xfe\x65\x76\x7a\x52\x11\xf5\xc0\x19\x46\xa0\xd1\x41\x5a\x4a\x31\xac\x00\x9f\x6b\x9c\x50\xc0\x12\x45\x8f\x1f\x3e\x9a\x08\x12\xf8\x43\x49\xf8\x6e\x22\xa6\x92\x29\x53\x89\x56\x12\x65\xc6\xf8\x31\x5d\xe2\x24\x96\xa6\xcd\xfc\xf1\xcc\x4c\x40\xeb\x11\x80\x74\x89\x24\xd1\x62\x26\x1d\x13\xa9\xb1\x48\x13\x53\x68\xa7\x14\x73\x04\x45\x42\x92\xc1\x5d\x28\xc2\xcb\xdb\x7b\xff\x89\x4a\x20\x8f\x0c\xd9\x9e\xd1\xc0\x6a\x00\xb3\x33\x16\x93\x92\x52\x45\x27\x34\x1a\x59\xdb\x3c\x4e\xeb\xb5\xf9\xf3\x5f\x53\x69\x0d\xd6\xdd\x93\x17\xd8\xe1\x3c\x38\x3d\xe9\xae\x93\xb2\x8a\x29\xe1\xd0\xbe\x5c\x2e\xba\x68\x16\x68\x8d\x76\x51\x22\x37\x36\x9f\xdf\x1e\x24\xc7\xc4\x32\xfd\x1a\x59\x1e\x2e\x5e\x22\x4b\xf5\xfd\xad\xa1\x6c\x83\x02\x99\x55\xd4\x75\x2e\x9f\x9f\x78\x2d\x69\xd0\xb8\xb0\x3a\x5f\xad\xb8\xe4\x76\xe7\x10\x6e\x95\x6c\x7a\x48\x68\x77\xb9\x83\x5f\xe4\xce\x74\xbd\xa8\x2c\x79\xd4\xa0\xf6\xf2\x58\x3d\x67\x4c\xa5\xd2\xee\x0b\xd9\x57\x45\x0f\x53\x84\x07\x05\xcf\xa8\x7b\xf7\xa9\xec\xf7\xee\x17\xea\xa4\x7f\x5e\x17\x78\x2b\x2e\x32\x06\xd1\xfc\xf4\x24\xf8\xf1\x55\xba\x0d\x24\x52\x64\xf2\xef\x0d\x82\xb0\x9b\xfc\xbb\x0e\xdf\xa0\x08\xf7\x48\x28\x06\x22\xe0\x72\x02\x71\x4c\x11\x90\x86\x80\xeb\x0f\xf9\x47\x41\x2d\x08\xb4\x8a\x4d\xc0\xa5\x41\x96\x12\x96\x8b\xa9\x36\x96\x10\x92\xf2\xef\x15\x08\x61\x37\xa4\xd2\xf5\xc6\x4f\x2f\x07\x7c\xce\xff\xab\x53\x63\x30\x77\x89\xb3\x5a\x7d\xda\x05\x51\x30\x45\xcb\xa6\x84\x46\x89\x6d\xc4\x94\x5c\xe5\xbb\x0c\xd8\x06\x83\x93\xe3\xfc\x2f\xa1\x94\xce\xbf\x08\x85\x82\xb8\x5c\x85\x78\x09\x02\x24\xc3\xaf\xf2\xf9\xab\xec\xa7\x92\xd2\xb5\x2e\x94\x5c\xf1\xf5\x0d\xe8\xff\x3f\xaf\x02\xad\xcd\xd4\x17\x6e\x97\xa8\x85\xda\x25\xb8\x3f\xd4\x3a\xd5\xf1\xdf\xae\x61\x07\xeb\xeb\x67\xd5\xa3\xd7\xa8\x76\xac\x1c\x91\x5a\x13\x1a\x73\x89\x10\x0b\x2e\xf1\x1e\x99\x92\xb1\xd3\xc2\xbb\xd9\xfb\x8f\xef\xcf\x4e\x3e\xbc\xff\xd8\x60\x45\xa8\x05\x67\x90\xed\xb7\x96\xb7\xdc\x19\xe9\x57\x6e\xac\xa2\xdd\x67\x9e\x70\xa7\x9d\xd9\xf1\x01\x79\x3c\x01\xcb\x36\x9f\x7d\xba\x7f\x9d\xf6\xf7\xf9\x6b\xb3\xce\xb4\xca\x84\x25\xb0\xb8\xde\x75\xe5\x23\x25\x04\x97\xeb\x2f\x3a\x06\x8b\x7d\x09\x13\x78\xba\x4f\x69\xed\x76\x66\x6f\x7b\x5b\x5f\x24\x6c\x81\x0b\x58\x66\xc9\x6e\x36\x54\x78\xab\x02\x74\xd7\x62\xe6\x95\xd3\x62\xa2\x45\x5f\x14\xbf\xbf\x67\x3b\x8c\x30\xf3\xf8\x07\x9e\xa0\xb1\x90\x38\xc5\xc8\x54\x88\x8e\xb8\x5e\xff\x7f\xbd\x15\xf6\xdb\xa1\x69\x89\xae\x42\x7a\x9e\x9a\xad\x42\x5d\xc3\x7b\x42\x4a\x15\xe3\xf9\xf0\x7e\xe6\xa5\xff\x4c\x39\x61\x7c\x99\x12\x97\xeb\x7b\xb6\xc1\x38\x75\xda\xbe\x5e\x4b\x55\x2d\x5f\x3d\x21\x4b\x6d\x9e\x70\xfa\x34\x0a\x3e\xf7\x85\x2f\x3f\x20\x25\xdd\x66\xae\xfe\xf9\xd0\x83\xca\xe5\xaf\x9e\x34\xe5\x9d\xc9\x30\x85\x31\x2a\x19\xa5\x47\xcc\xfa\x99\x25\x5a\x88\xda\x49\x45\xf5\xed\xd1\xc2\x54\x1a\x09\xf2\x70\x0c\xaf\xe5\x38\xec\x16\x44\x8a\xe3\x52\x66\x70\x82\xcb\xf4\x29\x1c\x81\xf9\x36\xb8\xf7\x3c\x2c\xc0\x2b\x15\x00\xc4\x36\xff\x79\x15\x40\x12\x7f\x78\xff\x4a\x15\x0c\xec\xf8\x31\x7c\xd0\x7d\xc8\x2e\x54\xfb\xef\x8e\xd2\x43\xa6\xa4\x05\x2e\x91\x7c\x07\xf5\x04\x14\xd0\x7a\x48\x25\xe1\xc4\xf5\x3f\x5e\x9d\x86\x59\x97\x54\x24\x8d\x69\xd5\x0b\xf6\x20\xbf\xf5\x91\x43\x9e\x40\x96\x6a\xc3\x37\x26\x8a\x1f\x29\x42\x46\xd1\x1b\x13\xbd\x31\x53\x7c\x34\x25\xcd\xf9\x76\x16\xbd\x8b\xde\x79\x98\xe7\xf8\x8b\x54\x88\x85\x12\x9c\x65\xae\x73\xbd\xba\x55\x76\x41\x68\x9a\xdd\x43\x03\x45\xf0\x2d\x4a\x34\x66\x41\x6a\xd9\xcf\xff\x05\xd0\x0a\xb8\x48\x09\x1f\x36\x84\x66\xa3\x84\x6b\x4a\x4e\xbd\x67\xdf\x58\xab\x3f\xa1\x1d\xa0\xe3\x0a\x32\xd8\x8d\x13\x6b\x9a\x37\xc0\x03\x5e\x59\x0e\x5f\x67\xc7\x67\xc7\x03\x10\x86\x6d\x30\x6f\x19\x7e\x7d\x78\x58\xf8\x5c\xd2\x1b\x73\xa1\x4b\xa0\x1c\xc4\x25\x0a\xd8\xd5\xdd\xc0\x07\x2f\x9b\x50\x23\x71\x15\xd7\x60\x33\x3f\x98\x49\x19\x43\x63\x9a\xea\x99\x79\x01\x2d\x4f\x50\xa5\xb6\x26\x78\xda\x77\x69\x8f\x8d\x86\x9b\xa3\x5a\x52\xcf\xdc\x5d\xfe\x06\x4c\x51\x85\xc3\xc2\x3f\xe9\xf6\xd8\xfb\x59\x07\xbe\x01\xfd\x50\x73\xfc\x44\xd1\x3c\x33\xbc\x57\x3c\x37\xb3\xff\x64\xf1\xdc\xa4\xb4\x47\xc0\x04\x2d\x71\x76\x90\xfe\x86\x04\x3c\x28\x83\xb8\xe9\x2c\x25\x86\xbe\x46\x27\xc8\xe3\x3d\xe1\x76\x68\x37\xeb\xb3\x12\x45\x59\xe6\x98\x7d\x3c\xbe\xe1\x87\x07\x96\x6b\x3f\xd0\x8c\x91\x66\x3a\xcd\xe8\x1e\x1f\x27\x43\x7a\xa8\xb9\x0f\x32\x3f\x28\x62\xb2\x99\x98\xdb\xdd\x85\x92\x16\x9f\x86\xf2\x51\x08\x42\xa8\xef\x0b\xe2\x5b\x2e\x70\x8d\x57\x86\x81\x80\xa2\x39\x5a\x81\x30\xe8\x3d\x28\x03\x0d\x4b\x2e\xb8\xe5\x83\x5a\x76\xa4\xe3\x78\xa4\x9c\x86\xb7\x57\x0f\xbf\xff\x72\x7d\x7b\xf9\xfb\xfd\xd5\xdd\x6f\xd7\x17\x57\xfe\x9a\xea\xb1\x70\x86\x1d\x93\xd2\x63\xd4\x41\x88\x01\x82\x07\x9b\x53\x93\x62\x37\xd9\xed\x4e\x36\x80\xae\x20\x15\xbe\xfa\x91\x19\x1e\xe2\xbf\x49\xb1\xbb\x53\xca\xfe\x95\x0b\x2c\xa6\xb3\x79\x60\x29\xc5\x83\xac\x65\x91\x12\x2e\x33\xcd\xdf\xa0\x31\xae\x86\x95\x65\x22\xc6\xed\xb4\xb1\x3d\x11\x6a\xed\x4b\x7f\x1e\x0a\x55\x05\x74\x32\xf9\x70\xb6\x4a\xa4\x09\x66\x67\x7c\x61\xe6\x4c\x1c\x4e\x25\x62\xa3\xdc\x0f\xb9\x75\x9d\xc0\xe5\x8a\xaf\x27\x39\xe7\x21\xe0\x52\x9f\x43\x1a\xf4\xa6\x82\xd1\x46\xa8\xe3\x46\x2e\x5b\xd6\xea\xf1\xdb\x36\xd4\xc4\x55\x16\x41\x02\x8c\xb9\x2d\xe4\xcf\x4d\x3b\x29\x2f\xa5\x19\x71\xcb\x19\x88\x2e\x2e\xb9\x39\x8c\x6c\xcd\xe3\x5c\x7c\x87\x5d\x6f\x8e\x35\xf9\x88\x82\x54\x92\x8f\x73\x51\x26\xd5\x46\x0f\xa3\x1f\xd6\xdd\x26\xcf\xb4\xaf\x26\x87\xa7\xe8\x36\xe0\xed\x68\x89\x6d\xfa\xd7\x27\x02\x86\x8b\x4e\x63\x70\x72\xdc\x45\x50\xc2\x75\xdf\x03\xe3\x8f\xa7\xd9\xc4\xd5\x0a\x99\xcd\x6f\x6f\x8b\xd9\xcd\xeb\xb5\xc5\x44\xe0\xc6\xb4\x09\x29\x81\x9d\xb1\x20\x01\x67\x99\x6e\xf0\xf7\x82\xce\x23\x40\x41\xf8\xa2\xb0\xe9\x79\x1c\x2b\x69\x32\x3f\xf4\x48\xd1\x9c\x2d\xae\x9e\xb8\x4b\xfb\x2f\xf2\xc0\x3c\x02\x0e\x54\x0d\xab\xae\x04\x07\xd2\x78\xe1\x37\x37\x2a\x76\x36\x7c\xff\xce\xdf\xa5\xb5\x5f\xa6\xba\xbf\xc1\x69\xb6\xd4\x4b\xd9\xc8\x0f\xcd\x51\x55\x6b\x3b\xdc\xf2\x07\x43\x83\x8d\x57\xde\x6e\xd7\xf7\xc2\x56\xb1\x99\x69\xc6\x6d\x73\xe4\xdb\x29\xbf\x0e\xb9\xb8\xa4\x25\xb0\x08\x52\xbb\x51\xc4\xff\xc8\x9c\x3e\x7a\x3c\xcb\xdc\xd1\x77\x9b\x59\xbc\x42\xdc\x29\xb1\xf7\xb1\xef\xbf\x76\x67\xb9\x54\xca\x1a\x4b\xa0\x35\x97\xeb\xf2\x88\x93\xc2\xcf\xf6\xdc\x0e\xe7\x49\x72\xde\x31\x5b\x7d\x4b\x49\xa9\xe8\x78\x7e\xeb\x74\xa0\xf9\x27\x52\xa9\xee\xbb\x6a\xd8\xb4\x63\xcb\x67\x5a\x6d\x5f\x07\x09\x65\xac\x15\x77\x35\xce\x9f\x00\x3d\x8f\x70\x71\x6f\xad\xba\x83\x35\xc3\x32\x6c\x91\x96\x1e\xfe\x82\x9b\x5e\x71\xf9\x0e\x96\x6d\x5a\x94\x7c\x1a\xfd\x53\xd5\xe2\x12\xe8\xcb\x0f\xb3\x46\x3b\x20\xf6\x51\x73\xe5\x4f\x0a\x97\x5f\xb8\x8c\x9d\x3f\xbe\xfe\x89\xdc\xc7\xba\x7d\xad\x94\x5a\x95\x96\x77\xc1\x3f\xfb\x45\xfc\x7f\x3e\x32\x95\xc0\x3b\x5c\x75\xb4\x59\x38\xde\xa8\xe5\x9a\xef\x15\x63\x79\xee\x05\xc2\x98\x74\xf9\x0f\x64\x76\x24\x53\xec\x79\x8d\x0d\xf6\x5d\x1d\xec\x7d\x59\xe9\xb9\xf5\x51\x11\x26\x15\xeb\xcf\x2e\xb8\x8f\x9e\x8f\xfe\x15\x00\x00\xff\xff\xa1\x14\xd5\xb1\x11\x22\x00\x00")
-
-func coredns112JsonBytes() ([]byte, error) {
-	return bindataRead(
-		_coredns112Json,
-		"coredns-1.12.json",
-	)
-}
-
-func coredns112Json() (*asset, error) {
-	bytes, err := coredns112JsonBytes()
-	if err != nil {
-		return nil, err
-	}
-
-	info := bindataFileInfo{name: "coredns-1.12.json", size: 0, mode: os.FileMode(0), modTime: time.Unix(0, 0)}
-	a := &asset{bytes: bytes, info: info, digest: [32]uint8{0x2e, 0x1f, 0x23, 0x78, 0x88, 0x81, 0xfa, 0xdb, 0x61, 0x37, 0xfb, 0xac, 0x91, 0x95, 0xa, 0xf8, 0xea, 0x7b, 0x12, 0xec, 0x7, 0x9, 0x94, 0x31, 0xbd, 0x8a, 0x9b, 0x8f, 0x58, 0xf3, 0xb7, 0xe1}}
 	return a, nil
 }
 
@@ -307,8 +265,6 @@ func AssetNames() []string {
 // _bindata is a table, holding each asset generator, mapped to its name.
 var _bindata = map[string]func() (*asset, error){
 	"aws-node.yaml":     awsNodeYaml,
-	"coredns-1.11.json": coredns111Json,
-	"coredns-1.12.json": coredns112Json,
 	"coredns-1.13.json": coredns113Json,
 	"coredns-1.14.json": coredns114Json,
 	"coredns-1.15.json": coredns115Json,
@@ -357,8 +313,6 @@ type bintree struct {
 
 var _bintree = &bintree{nil, map[string]*bintree{
 	"aws-node.yaml":     &bintree{awsNodeYaml, map[string]*bintree{}},
-	"coredns-1.11.json": &bintree{coredns111Json, map[string]*bintree{}},
-	"coredns-1.12.json": &bintree{coredns112Json, map[string]*bintree{}},
 	"coredns-1.13.json": &bintree{coredns113Json, map[string]*bintree{}},
 	"coredns-1.14.json": &bintree{coredns114Json, map[string]*bintree{}},
 	"coredns-1.15.json": &bintree{coredns115Json, map[string]*bintree{}},

pkg/addons/default/aws_node_test.go
@@ -18,8 +18,8 @@ var _ = Describe("default addons - aws-node", func() {
 			ct        *testutils.CollectionTracker
 		)
 
-		It("can load sample for 1.12 and create objests that don't exist", func() {
-			sampleAddons := testutils.LoadSamples("testdata/sample-1.12.json")
+		It("can load sample for 1.14 and create objects that don't exist", func() {
+			sampleAddons := testutils.LoadSamples("testdata/sample-1.14.json")
 
 			rawClient = testutils.NewFakeRawClient()
 
@@ -46,15 +46,15 @@ var _ = Describe("default addons - aws-node", func() {
 			Expect(err).ToNot(HaveOccurred())
 			Expect(awsNode.Spec.Template.Spec.Containers).To(HaveLen(1))
 			Expect(awsNode.Spec.Template.Spec.Containers[0].Image).To(
-				Equal("602401143452.dkr.ecr.eu-west-1.amazonaws.com/amazon-k8s-cni:v1.4.1"),
+				Equal("602401143452.dkr.ecr.eu-west-1.amazonaws.com/amazon-k8s-cni:v1.5.7"),
 			)
 
 		})
 
-		It("can update 1.12 sample to latest", func() {
+		It("can update 1.14 sample to latest", func() {
 			rawClient.AssumeObjectsMissing = false
 
-			_, err := UpdateAWSNode(rawClient, "eu-west-2", false)
+			_, err := UpdateAWSNode(rawClient, "eu-west-1", false)
 			Expect(err).ToNot(HaveOccurred())
 			Expect(rawClient.Collection.UpdatedItems()).To(HaveLen(4))
 			Expect(rawClient.Collection.CreatedItems()).To(HaveLen(10))
@@ -65,13 +65,13 @@ var _ = Describe("default addons - aws-node", func() {
 			Expect(err).ToNot(HaveOccurred())
 			Expect(awsNode.Spec.Template.Spec.Containers).To(HaveLen(1))
 			Expect(awsNode.Spec.Template.Spec.Containers[0].Image).To(
-				Equal("602401143452.dkr.ecr.eu-west-2.amazonaws.com/amazon-k8s-cni:v1.6.1"),
+				Equal("602401143452.dkr.ecr.eu-west-1.amazonaws.com/amazon-k8s-cni:v1.6.1"),
 			)
 
 			rawClient.ClearUpdated()
 		})
 
-		It("can update 1.12 sample for different region", func() {
+		It("can update 1.14 sample for different region", func() {
 			rawClient.ClientSetUseUpdatedObjects = false // must be set for subsequent UpdateAWSNode
 
 			_, err := UpdateAWSNode(rawClient, "us-east-1", false)
@@ -87,7 +87,7 @@ var _ = Describe("default addons - aws-node", func() {
 			)
 		})
 
-		It("can update 1.12 sample for china region", func() {
+		It("can update 1.14 sample for china region", func() {
 			rawClient.ClientSetUseUpdatedObjects = false // must be set for subsequent UpdateAWSNode
 
 			_, err := UpdateAWSNode(rawClient, "cn-northwest-1", false)

commit b0807cfd3a2395e371343e495573663ebcddc0ba

Fix kubernetes client serializer
Broken in #2179
See also https://github.com/kubernetes/kubernetes/pull/76098

pkg/kubernetes/client.go
@@ -109,7 +109,7 @@ func (c *RawClient) new() (*RawClient, error) {
 		c.config.APIPath = "/api"
 	}
 	if c.config.NegotiatedSerializer == nil {
-		c.config.NegotiatedSerializer = scheme.Codecs
+		c.config.NegotiatedSerializer = scheme.Codecs.WithoutConversion()
 	}
 
 	if err := restclient.SetKubernetesDefaults(c.config); err != nil {