4dc21b9c23b39bc1b9f6b294c2222b399ec5b884
Michael Beaumont <mjboamail@gmail.com>
2020-06-04 18:49:03 +0200

    Add schema generation integration tests

--- pkg/schema/definition/comment_test.go
+++ pkg/schema/definition/comment_test.go
@@ -1,24 +1,29 @@
 package definition
 
 import (
-	"testing"
-
-	"github.com/stretchr/testify/assert"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
 
-func TestHandleJSONSchemaComment(t *testing.T) {
-	def := &Definition{}
-	comment := `Comment about struct
-	+jsonschema noderive
-	+jsonschema { "type": "string" }`
-	noderive, remaining, err := HandleJSONSchemaComment(comment, def)
-	assert.Nil(t, err)
-	assert.True(t, noderive)
-	assert.Equal(t, "string", def.Type)
-	assert.Equal(t, "Comment about struct", remaining)
-}
+var _ = Describe("HandleJSONSchemaComment", func() {
+	It("works", func() {
+		def := &Definition{}
+		comment := `Comment about struct
+		+jsonschema noderive
+		+jsonschema { "type": "string" }`
+		noderive, remaining, err := HandleJSONSchemaComment(comment, def)
+		Expect(err).ToNot(HaveOccurred())
+		Expect(noderive).To(BeTrue())
+		Expect(remaining).To(Equal("Comment about struct"))
+		Expect(def.Type).To(Equal("string"))
+	})
+})
 
-func TestGetTypeName(t *testing.T) {
-	assert.Equal(t, "Thing", getTypeName("some/pkg.Thing"))
-	assert.Equal(t, "Thing", getTypeName("Thing"))
-}
+var _ = Describe("GetTypeName", func() {
+	It("handles imported types", func() {
+		Expect(getTypeName("some/pkg.Thing")).To(Equal("Thing"))
+	})
+	It("handles in scope types", func() {
+		Expect(getTypeName("Thing")).To(Equal("Thing"))
+	})
+})
