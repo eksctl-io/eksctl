aad36e83ef0803a79f55a3519c9222ebbbd7512c
Mike Beaumont <mjboamail@gmail.com>
2020-07-24 13:09:32 +0200

    Cleanup build and release scripts (#2466)
    * Remove unused scripts
    
    * Move all release/CI scripts into build/scripts folder
    
    * Remove unused tools/brew

--- hack/run-integration-tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -o errexit
-set -o pipefail
-set -o nounset
-
-skaffold build --profile=local
-make integration-test-container-pre-built EKSCTL_IMAGE=weaveworks/eksctl:local TEST_V=1
