f33a727c25688e77070fa86f7c97c672d9f9cc62
cpu1 <patwal.chetan@gmail.com>
2020-08-13 16:24:57 +0530

    Use const for image_id in SSM parameter construction

--- pkg/ami/ssm_resolver.go
+++ pkg/ami/ssm_resolver.go
@@ -56,17 +56,19 @@ func MakeSSMParameterName(version, instanceType, imageFamily string) (string, er
 		}
 	}
 
+	const fieldName = "image_id"
+
 	switch imageFamily {
 	case api.NodeImageFamilyAmazonLinux2:
-		return fmt.Sprintf("/aws/service/eks/optimized-ami/%s/%s/recommended/image_id", version, imageType(imageFamily, instanceType)), nil
+		return fmt.Sprintf("/aws/service/eks/optimized-ami/%s/%s/recommended/%s", version, imageType(imageFamily, instanceType), fieldName), nil
 	case api.NodeImageFamilyWindowsServer2019CoreContainer:
-		return fmt.Sprintf("/aws/service/ami-windows-latest/Windows_Server-2019-English-Core-EKS_Optimized-%s/image_id", version), nil
+		return fmt.Sprintf("/aws/service/ami-windows-latest/Windows_Server-2019-English-Core-EKS_Optimized-%s/%s", version, fieldName), nil
 	case api.NodeImageFamilyWindowsServer2019FullContainer:
-		return fmt.Sprintf("/aws/service/ami-windows-latest/Windows_Server-2019-English-Full-EKS_Optimized-%s/image_id", version), nil
+		return fmt.Sprintf("/aws/service/ami-windows-latest/Windows_Server-2019-English-Full-EKS_Optimized-%s/%s", version, fieldName), nil
 	case api.NodeImageFamilyWindowsServer1909CoreContainer:
-		return fmt.Sprintf("/aws/service/ami-windows-latest/Windows_Server-1909-English-Core-EKS_Optimized-%s/image_id", version), nil
+		return fmt.Sprintf("/aws/service/ami-windows-latest/Windows_Server-1909-English-Core-EKS_Optimized-%s/%s", version, fieldName), nil
 	case api.NodeImageFamilyBottlerocket:
-		return fmt.Sprintf("/aws/service/bottlerocket/aws-k8s-%s/%s/latest/image_id", version, instanceEC2ArchName(instanceType)), nil
+		return fmt.Sprintf("/aws/service/bottlerocket/aws-k8s-%s/%s/latest/%s", version, instanceEC2ArchName(instanceType), fieldName), nil
 	case api.NodeImageFamilyUbuntu1804:
 		return "", &UnsupportedQueryError{msg: fmt.Sprintf("SSM Parameter lookups for %s AMIs is not supported yet", imageFamily)}
 	default:
