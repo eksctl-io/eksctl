a486478a321a549d2ef509a1bb96ecfd6ffbecff
Michael Beaumont <mjboamail@gmail.com>
2020-08-20 10:27:40 +0200

    Handle URL parsing internally
    go-git-providers doesn't handle git@github.com

--- pkg/gitops/deploykey/deploykey.go
+++ pkg/gitops/deploykey/deploykey.go
@@ -3,27 +3,39 @@ package deploykey
 import (
 	"context"
 	"os"
+	"strings"
 
 	"github.com/fluxcd/go-git-providers/github"
 	"github.com/fluxcd/go-git-providers/gitprovider"
+	"github.com/pkg/errors"
+	giturls "github.com/whilp/git-urls"
 )
 
 const githubTokenVariable = "GITHUB_TOKEN"
 
 // GetDeployKeyClient provides a DeployKeyClient for the repo at the given URL
 func GetDeployKeyClient(ctx context.Context, url string) (gitprovider.DeployKeyClient, error) {
-	ref, err := gitprovider.ParseUserRepositoryURL(url)
+	repo, err := giturls.Parse(url)
 	if err != nil {
 		return nil, err
 	}
-	switch ref.Domain {
-	case "github.com":
+	if strings.Contains(repo.Host, "github.com") {
 		githubToken := os.Getenv(githubTokenVariable)
 		gh, err := github.NewClient(github.WithOAuth2Token(githubToken))
 		if err != nil {
 			return nil, err
 		}
-		rep, err := gh.UserRepositories().Get(ctx, *ref)
+		ownerRepo := strings.Split(repo.Path, "/")
+		if len(ownerRepo) != 2 {
+			return nil, errors.Errorf("couldn't handle URL as github.com URL: %s", url)
+		}
+		rep, err := gh.UserRepositories().Get(ctx, gitprovider.UserRepositoryRef{
+			UserRef: gitprovider.UserRef{
+				Domain:    "github.com",
+				UserLogin: ownerRepo[0],
+			},
+			RepositoryName: ownerRepo[1],
+		})
 		if err != nil {
 			return nil, err
 		}
