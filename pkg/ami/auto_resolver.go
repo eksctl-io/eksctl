011b2e8ddc141bfb4ace03543b1cf8cdff64bd94
Martina Iglesias Fernandez <martina@weave.works>
2020-09-03 09:08:59 +0200

    Add support for Windows 2004 Server AMI

--- pkg/ami/auto_resolver.go
+++ pkg/ami/auto_resolver.go
@@ -38,6 +38,9 @@ func MakeImageSearchPatterns(version string) map[string]map[int]string {
 		api.NodeImageFamilyWindowsServer1909CoreContainer: {
 			ImageClassGeneral: fmt.Sprintf("Windows_Server-1909-English-Core-EKS_Optimized-%v-*", version),
 		},
+		api.NodeImageFamilyWindowsServer2004CoreContainer: {
+			ImageClassGeneral: fmt.Sprintf("Windows_Server-2004-English-Core-EKS_Optimized-%v-*", version),
+		},
 	}
 }
 
--- pkg/ami/ssm_resolver.go
+++ pkg/ami/ssm_resolver.go
@@ -66,6 +66,8 @@ func MakeSSMParameterName(version, instanceType, imageFamily string) (string, er
 		return fmt.Sprintf("/aws/service/ami-windows-latest/Windows_Server-2019-English-Full-EKS_Optimized-%s/%s", version, fieldName), nil
 	case api.NodeImageFamilyWindowsServer1909CoreContainer:
 		return fmt.Sprintf("/aws/service/ami-windows-latest/Windows_Server-1909-English-Core-EKS_Optimized-%s/%s", version, fieldName), nil
+	case api.NodeImageFamilyWindowsServer2004CoreContainer:
+		return fmt.Sprintf("/aws/service/ami-windows-latest/Windows_Server-2004-English-Core-EKS_Optimized-%s/%s", version, fieldName), nil
 	case api.NodeImageFamilyBottlerocket:
 		return fmt.Sprintf("/aws/service/bottlerocket/aws-k8s-%s/%s/latest/%s", version, instanceEC2ArchName(instanceType), fieldName), nil
 	case api.NodeImageFamilyUbuntu1804:
