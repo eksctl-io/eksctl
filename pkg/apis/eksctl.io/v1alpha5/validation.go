00c96084567df9680d3a9aa742ec3b6663943951
Michael Beaumont <mjboamail@gmail.com>
2020-09-15 15:16:02 +0200

    Validate disable IMDS options with managed ng and launch template

--- pkg/apis/eksctl.io/v1alpha5/validation.go
+++ pkg/apis/eksctl.io/v1alpha5/validation.go
@@ -485,10 +485,11 @@ func ValidateManagedNodeGroup(ng *ManagedNodeGroup, index int) error {
 
 		if ng.InstanceType != "" || ng.AMI != "" || IsEnabled(ng.SSH.Allow) || len(ng.SSH.SourceSecurityGroupIDs) > 0 ||
 			ng.VolumeSize != nil || len(ng.PreBootstrapCommands) > 0 || ng.OverrideBootstrapCommand != nil ||
-			len(ng.SecurityGroups.AttachIDs) > 0 || ng.InstanceName != "" || ng.InstancePrefix != "" || ng.MaxPodsPerNode != 0 {
+			len(ng.SecurityGroups.AttachIDs) > 0 || ng.InstanceName != "" || ng.InstancePrefix != "" || ng.MaxPodsPerNode != 0 ||
+			IsEnabled(ng.DisableIMDSv1) || IsEnabled(ng.DisablePodIMDS) {
 
 			return errors.New("cannot set instanceType, ami, ssh.allow, ssh.sourceSecurityGroupIds, securityGroups, " +
-				"volumeSize, instanceName, instancePrefix, maxPodsPerNode, disableIMDSv1, preBootstrapCommands or overrideBootstrapCommand in managedNodeGroup when a launch template is supplied")
+				"volumeSize, instanceName, instancePrefix, maxPodsPerNode, disableIMDSv1, disablePodIMDS, preBootstrapCommands or overrideBootstrapCommand in managedNodeGroup when a launch template is supplied")
 		}
 
 	case ng.AMI != "":
