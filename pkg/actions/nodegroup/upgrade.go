a151d6ef171594f28688af59697f1ef5026edb3c
Jake <jakelarsj@gmail.com>
2020-12-17 14:16:54 +0000

    wait until upgrade complete

--- pkg/actions/nodegroup/upgrade.go
+++ pkg/actions/nodegroup/upgrade.go
@@ -1,9 +1,14 @@
 package nodegroup
 
 import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws/request"
 	"github.com/aws/aws-sdk-go/service/eks"
+	"github.com/kris-nova/logger"
 	"github.com/weaveworks/eksctl/pkg/cfn/manager"
 	"github.com/weaveworks/eksctl/pkg/managed"
+	"github.com/weaveworks/eksctl/pkg/utils/waiters"
 )
 
 func (m *Manager) Upgrade(nodeGroupName, version, launchTemplateVersion string, forceUpgrade bool) error {
@@ -23,12 +28,50 @@ func (m *Manager) Upgrade(nodeGroupName, version, launchTemplateVersion string, 
 		})
 	}
 
-	_, err = m.ctl.Provider.EKS().UpdateNodegroupVersion(&eks.UpdateNodegroupVersionInput{
+	return m.upgradeAndWait(nodeGroupName, version, launchTemplateVersion, forceUpgrade)
+}
+
+func (m *Manager) upgradeAndWait(nodeGroupName, version, launchTemplateVersion string, forceUpgrade bool) error {
+	upgradeResponse, err := m.ctl.Provider.EKS().UpdateNodegroupVersion(&eks.UpdateNodegroupVersionInput{
 		ClusterName:   &m.cfg.Metadata.Name,
 		Force:         &forceUpgrade,
 		NodegroupName: &nodeGroupName,
 		Version:       &version,
 	})
 
-	return err
+	if err != nil {
+		return err
+	}
+
+	if upgradeResponse != nil {
+		logger.Debug("upgrade response for %q: %s", nodeGroupName, upgradeResponse.String())
+	}
+
+	logger.Info("upgrade of nodegroup %q in progress", nodeGroupName)
+
+	newRequest := func() *request.Request {
+		input := &eks.DescribeNodegroupInput{
+			ClusterName:   &m.cfg.Metadata.Name,
+			NodegroupName: &nodeGroupName,
+		}
+		req, _ := m.ctl.Provider.EKS().DescribeNodegroupRequest(input)
+		return req
+	}
+
+	msg := fmt.Sprintf("waiting for upgrade of nodegroup %q to complete", nodeGroupName)
+
+	acceptors := waiters.MakeAcceptors(
+		"Nodegroup.Status",
+		eks.NodegroupStatusActive,
+		[]string{
+			eks.NodegroupStatusDegraded,
+		},
+	)
+
+	err = waiters.Wait(nodeGroupName, msg, acceptors, newRequest, m.ctl.Provider.WaitTimeout(), nil)
+	if err != nil {
+		return err
+	}
+	logger.Info("upgrade of nodegroup %q complete", nodeGroupName)
+	return nil
 }
