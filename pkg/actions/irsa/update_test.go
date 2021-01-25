a96c4ddf4c582a41e0947b8d56bc8eea0126e5e2
Jake <jakelarsj@gmail.com>
2021-01-25 14:42:57 +0000

    iam -> irsa package name

--- pkg/actions/irsa/update_test.go
+++ pkg/actions/irsa/update_test.go
@@ -1,4 +1,4 @@
-package iam_test
+package irsa_test
 
 import (
 	"github.com/aws/aws-sdk-go/aws"
@@ -8,8 +8,8 @@ import (
 	"github.com/weaveworks/eksctl/pkg/cfn/manager"
 	"github.com/weaveworks/eksctl/pkg/eks"
 
-	"github.com/weaveworks/eksctl/pkg/actions/iam"
-	"github.com/weaveworks/eksctl/pkg/actions/iam/fakes"
+	"github.com/weaveworks/eksctl/pkg/actions/irsa"
+	"github.com/weaveworks/eksctl/pkg/actions/irsa/fakes"
 	api "github.com/weaveworks/eksctl/pkg/apis/eksctl.io/v1alpha5"
 	iamoidc "github.com/weaveworks/eksctl/pkg/iam/oidc"
 	"github.com/weaveworks/eksctl/pkg/testutils/mockprovider"
@@ -18,7 +18,7 @@ import (
 var _ = Describe("Update", func() {
 
 	var (
-		iamManager       *iam.Manager
+		irsaManager      *irsa.Manager
 		oidc             *iamoidc.OpenIDConnectManager
 		fakeStackManager *fakes.FakeStackManager
 		mockProvider     *mockprovider.MockProvider
@@ -43,7 +43,7 @@ var _ = Describe("Update", func() {
 		oidc, err = iamoidc.NewOpenIDConnectManager(nil, "456123987123", "https://oidc.eks.us-west-2.amazonaws.com/id/A39A2842863C47208955D753DE205E6E", "aws")
 		Expect(err).ToNot(HaveOccurred())
 		oidc.ProviderARN = "arn:aws:iam::456123987123:oidc-provider/oidc.eks.us-west-2.amazonaws.com/id/A39A2842863C47208955D753DE205E6E"
-		iamManager = iam.New("my-cluster", &eks.ClusterProvider{Provider: mockProvider}, fakeStackManager, oidc, nil)
+		irsaManager = irsa.New("my-cluster", &eks.ClusterProvider{Provider: mockProvider}, fakeStackManager, oidc, nil)
 	})
 
 	When("the IAMServiceAccount exists", func() {
@@ -54,7 +54,7 @@ var _ = Describe("Update", func() {
 				},
 			}, nil)
 
-			err := iamManager.UpdateIAMServiceAccounts(serviceAccount, false)
+			err := irsaManager.UpdateIAMServiceAccounts(serviceAccount, false)
 			Expect(err).NotTo(HaveOccurred())
 
 			Expect(fakeStackManager.ListStacksMatchingCallCount()).To(Equal(1))
@@ -78,7 +78,7 @@ var _ = Describe("Update", func() {
 					},
 				}, nil)
 
-				err := iamManager.UpdateIAMServiceAccounts(serviceAccount, true)
+				err := irsaManager.UpdateIAMServiceAccounts(serviceAccount, true)
 				Expect(err).NotTo(HaveOccurred())
 
 				Expect(fakeStackManager.ListStacksMatchingCallCount()).To(Equal(1))
