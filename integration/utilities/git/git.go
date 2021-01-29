dc5aacf1cf9fd5c185b54860d77778f42da92435
Mike <2266568+michaelbeaumont@users.noreply.github.com>
2021-01-29 12:32:20 +0100

    Add Github workflow for integration tests (#3174)
    * Add Github workflow for integration tests (#3156)
    
    * Fix circleci integration test invocation (#3164)

--- integration/utilities/git/git.go
+++ integration/utilities/git/git.go
@@ -3,10 +3,7 @@
 package git
 
 import (
-	"fmt"
-	"io/ioutil"
-	"os"
-	"os/exec"
+	"github.com/weaveworks/eksctl/pkg/git"
 )
 
 const (
@@ -21,63 +18,44 @@ const (
 )
 
 // CreateBranch creates the provided branch.
-func CreateBranch(branch, privateSSHKeyPath string) (string, error) {
-	cloneDir, err := ioutil.TempDir(os.TempDir(), "eksctl-install-flux-test-clone-")
+func CreateBranch(branch string) (string, error) {
+	cli := git.NewGitClient(git.ClientParams{})
+	cloneDir, err := cli.CloneRepoInTmpDir(
+		"eksctl-install-flux-test-branch-",
+		git.CloneOptions{
+			URL:       Repository,
+			Branch:    branch,
+			Bootstrap: true,
+		},
+	)
 	if err != nil {
-		return "", fmt.Errorf("failed to create temporary directory: %s", err)
-	}
-	if err := gitWith(gitParams{Args: []string{"clone", "-b", "master", Repository, cloneDir}, Dir: cloneDir, Env: gitSSHCommand(privateSSHKeyPath)}); err != nil {
 		return "", err
 	}
-	if err := gitWith(gitParams{Args: []string{"checkout", "-b", branch}, Dir: cloneDir, Env: gitSSHCommand(privateSSHKeyPath)}); err != nil {
-		return "", err
-	}
-	if err := gitWith(gitParams{Args: []string{"push", "origin", branch}, Dir: cloneDir, Env: gitSSHCommand(privateSSHKeyPath)}); err != nil {
+	if err := cli.Push(); err != nil {
 		return "", err
 	}
 	return cloneDir, nil
 }
 
-// DeleteBranch deletes the local clone used for testing Git repository, and delete the branch from "origin" as well.
-func DeleteBranch(branch, cloneDir, privateSSHKeyPath string) error {
-	defer os.RemoveAll(cloneDir)
-	return gitWith(gitParams{Args: []string{"push", "origin", "--delete", branch}, Dir: cloneDir, Env: gitSSHCommand(privateSSHKeyPath)})
+// CleanupBranchAndRepo deletes the local clone used for testing Git repository, and delete the branch from "origin" as well.
+func CleanupBranchAndRepo(branch, cloneDir string) error {
+	cli := git.NewGitClient(git.ClientParams{})
+	cli.WithDir(cloneDir)
+	if err := cli.DeleteRemoteBranch(branch); err != nil {
+		return err
+	}
+	return cli.DeleteLocalRepo()
 }
 
 // GetBranch clones the testing Git repository and checks out the provided branch.
-func GetBranch(branch, privateSSHKeyPath string) (string, error) {
-	cloneDir, err := ioutil.TempDir(os.TempDir(), "eksctl-install-flux-test-branch-")
-	if err != nil {
-		return "", fmt.Errorf("failed to create temporary directory: %s", err)
-	}
-	if err := gitWith(gitParams{Args: []string{"clone", "-b", branch, Repository, cloneDir}, Dir: cloneDir, Env: gitSSHCommand(privateSSHKeyPath)}); err != nil {
-		return "", err
-	}
-	return cloneDir, nil
-}
-
-type gitParams struct {
-	Args []string
-	Env  []string
-	Dir  string
-}
-
-func gitWith(params gitParams) error {
-	gitCmd := exec.Command("git", params.Args...)
-	if params.Env != nil {
-		gitCmd.Env = params.Env
-	}
-	gitCmd.Stdout = os.Stdout
-	gitCmd.Stderr = os.Stderr
-	if params.Dir != "" {
-		gitCmd.Dir = params.Dir
-	}
-	return gitCmd.Run()
-}
-
-func gitSSHCommand(privateSSHKeyPath string) []string {
-	return []string{
-		fmt.Sprintf("PATH=%s", os.Getenv("PATH")),
-		fmt.Sprintf("GIT_SSH_COMMAND=ssh -i %s", privateSSHKeyPath),
-	}
+func GetBranch(branch string) (string, error) {
+	cli := git.NewGitClient(git.ClientParams{})
+	return cli.CloneRepoInTmpDir(
+		"eksctl-install-flux-test-branch-",
+		git.CloneOptions{
+			URL:       Repository,
+			Branch:    branch,
+			Bootstrap: true,
+		},
+	)
 }
