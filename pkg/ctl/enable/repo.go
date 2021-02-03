813aeed72b08f166fd59fb4f1eaed0a5b273c886
Claudia Beresford <claudiaberesford@gmail.com>
2021-02-03 17:26:23 +0000

    Move repo code into actions
    Refactoring to bring in line with the plan to move everything into a
    library.

--- pkg/ctl/enable/repo.go
+++ pkg/ctl/enable/repo.go
@@ -1,16 +1,15 @@
 package enable
 
 import (
-	"context"
 	"time"
 
 	"github.com/kris-nova/logger"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
+	"github.com/weaveworks/eksctl/pkg/actions/repo"
 	api "github.com/weaveworks/eksctl/pkg/apis/eksctl.io/v1alpha5"
 	"github.com/weaveworks/eksctl/pkg/ctl/cmdutils"
 	"github.com/weaveworks/eksctl/pkg/gitops"
-	"github.com/weaveworks/eksctl/pkg/gitops/flux"
 )
 
 type options struct {
@@ -66,35 +65,21 @@ func configureRepositoryCmd(cmd *cmdutils.Cmd) *options {
 	return &opts
 }
 
-// doEnableRepository enables GitOps on the configured repository.
 func doEnableRepository(cmd *cmdutils.Cmd) error {
 	k8sClientSet, k8sRestConfig, err := cmdutils.KubernetesClientAndConfigFrom(cmd)
 	if err != nil {
 		return err
 	}
 
-	installer, err := flux.NewInstaller(k8sRestConfig, k8sClientSet, cmd.ClusterConfig, cmd.ProviderConfig.WaitTimeout)
+	installer, err := repo.New(k8sRestConfig, k8sClientSet, cmd.ClusterConfig, cmd.ProviderConfig.WaitTimeout)
 	if err != nil {
 		return err
 	}
 
-	fluxIsInstalled, err := installer.IsFluxInstalled()
-	if err != nil {
-		// Continue with installation
-		logger.Warning(err.Error())
-	} else if fluxIsInstalled {
-		logger.Warning("found existing flux deployment in namespace %q. Skipping installation",
-			cmd.ClusterConfig.Git.Operator.Namespace)
-		return nil
-	}
-
-	userInstructions, err := installer.Run(context.Background())
-	if err != nil {
+	if err := installer.Run(); err != nil {
 		logger.Critical("unable to set up gitops repo: %s", err.Error())
 		return err
 	}
-
-	logger.Info(userInstructions)
 
 	return nil
 }
