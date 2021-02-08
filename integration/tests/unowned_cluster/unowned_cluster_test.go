b520326183b933b6326b9d146a01711cd6e23824
Michael Beaumont <mjboamail@gmail.com>
2021-02-08 11:10:21 +0100

    Restructure unowned_cluster integration tests to improve output

--- integration/tests/unowned_cluster/unowned_cluster_test.go
+++ integration/tests/unowned_cluster/unowned_cluster_test.go
@@ -40,241 +40,255 @@ func TestE2E(t *testing.T) {
 }
 
 var _ = Describe("(Integration) [non-eksctl cluster & nodegroup support]", func() {
-	Context("Get, upgrade & delete cluster/nodegroups", func() {
-		var (
-			clusterName, stackName, ng1, ng2 string
-			ctl                              api.ClusterProvider
-		)
+	var (
+		clusterName, stackName, ng1, ng2 string
+		ctl                              api.ClusterProvider
+	)
 
-		BeforeEach(func() {
-			ng1 = "ng-1"
-			ng2 = "ng-2"
-			// "unowned_clusters" lead to names longer than allowed for CF stacks
-			clusterName = params.NewClusterName("uc")
-			stackName = fmt.Sprintf("eksctl-%s", clusterName)
-			cfg := &api.ClusterConfig{
-				Metadata: &api.ClusterMeta{
-					Name:   params.ClusterName,
-					Region: params.Region,
-				},
-			}
-			ctl = eks.New(&api.ProviderConfig{Region: params.Region}, cfg).Provider
-			createClusterWithNodegroups(clusterName, stackName, ng1, ng2, ctl)
-		})
+	BeforeEach(func() {
+		ng1 = "ng-1"
+		ng2 = "ng-2"
+		// "unowned_clusters" lead to names longer than allowed for CF stacks
+		clusterName = params.NewClusterName("uc")
+		stackName = fmt.Sprintf("eksctl-%s", clusterName)
+		cfg := &api.ClusterConfig{
+			Metadata: &api.ClusterMeta{
+				Name:   params.ClusterName,
+				Region: params.Region,
+			},
+		}
+		ctl = eks.New(&api.ProviderConfig{Region: params.Region}, cfg).Provider
+		createClusterWithNodegroups(clusterName, stackName, ng1, ng2, ctl)
+	})
 
-		AfterEach(func() {
-			deleteStack(stackName, ctl)
-		})
+	AfterEach(func() {
+		deleteStack(stackName, ctl)
+	})
 
-		It("should work", func() {
-			By("Getting clusters")
-			cmd := params.EksctlGetCmd.
-				WithArgs(
-					"clusters",
-					"--verbose", "2",
-				)
-			Expect(cmd).To(RunSuccessfullyWithOutputStringLines(
-				ContainElement(ContainSubstring(clusterName)),
-			))
+	It("supports getting non-eksctl resources", func() {
+		By("getting clusters")
+		cmd := params.EksctlGetCmd.
+			WithArgs(
+				"clusters",
+				"--verbose", "2",
+			)
+		Expect(cmd).To(RunSuccessfullyWithOutputStringLines(
+			ContainElement(ContainSubstring(clusterName)),
+		))
 
-			By("Getting nodegroups")
-			cmd = params.EksctlGetCmd.
-				WithArgs(
-					"nodegroups",
-					"--cluster", clusterName,
-					"--verbose", "2",
-				)
-			Expect(cmd).To(RunSuccessfullyWithOutputStringLines(
-				ContainElement(ContainSubstring("ng-1")),
-			))
-			Expect(cmd).To(RunSuccessfullyWithOutputStringLines(
-				ContainElement(ContainSubstring("ng-2")),
-			))
+		By("getting nodegroups")
+		cmd = params.EksctlGetCmd.
+			WithArgs(
+				"nodegroups",
+				"--cluster", clusterName,
+				"--verbose", "2",
+			)
+		Expect(cmd).To(RunSuccessfullyWithOutputStringLines(
+			ContainElement(ContainSubstring("ng-1")),
+		))
+		Expect(cmd).To(RunSuccessfullyWithOutputStringLines(
+			ContainElement(ContainSubstring("ng-2")),
+		))
+	})
 
-			By("Setting labels on a nodegroup")
-			cmd = params.EksctlSetLabelsCmd.
-				WithArgs(
-					"--cluster", clusterName,
-					"--nodegroup", ng1,
-					"--labels", "key=value",
-					"--verbose", "2",
-				)
-			Expect(cmd).To(RunSuccessfully())
+	It("supports labels", func() {
+		By("setting labels on a nodegroup")
+		cmd := params.EksctlSetLabelsCmd.
+			WithArgs(
+				"--cluster", clusterName,
+				"--nodegroup", ng1,
+				"--labels", "key=value",
+				"--verbose", "2",
+			)
+		Expect(cmd).To(RunSuccessfully())
 
-			By("Getting labels for a nodegroup")
-			cmd = params.EksctlGetCmd.
-				WithArgs(
-					"labels",
-					"--cluster", clusterName,
-					"--nodegroup", ng1,
-					"--verbose", "2",
-				)
-				// It takes a second or 2 for the above set to take effect
-			Eventually(func() *gbytes.Buffer { return cmd.Run().Out }, time.Second*10).Should(gbytes.Say("key=value"))
+		By("getting labels for a nodegroup")
+		cmd = params.EksctlGetCmd.
+			WithArgs(
+				"labels",
+				"--cluster", clusterName,
+				"--nodegroup", ng1,
+				"--verbose", "2",
+			)
+			// It takes a second or 2 for the above set to take effect
+		Eventually(func() *gbytes.Buffer { return cmd.Run().Out }, time.Second*10).Should(gbytes.Say("key=value"))
 
-			By("Unsetting labels on a nodegroup")
-			cmd = params.EksctlUnsetLabelsCmd.
-				WithArgs(
-					"--cluster", clusterName,
-					"--nodegroup", ng1,
-					"--labels", "key",
-					"--verbose", "2",
-				)
-			Expect(cmd).To(RunSuccessfully())
+		By("unsetting labels on a nodegroup")
+		cmd = params.EksctlUnsetLabelsCmd.
+			WithArgs(
+				"--cluster", clusterName,
+				"--nodegroup", ng1,
+				"--labels", "key",
+				"--verbose", "2",
+			)
+		Expect(cmd).To(RunSuccessfully())
+	})
 
-			By("Enabling OIDC")
-			cmd = params.EksctlUtilsCmd.
-				WithArgs(
-					"associate-iam-oidc-provider",
-					"--name", clusterName,
-					"--approve",
-					"--verbose", "2",
-				)
-			Expect(cmd).To(RunSuccessfully())
+	It("supports IRSA", func() {
+		By("enabling OIDC")
+		cmd := params.EksctlUtilsCmd.
+			WithArgs(
+				"associate-iam-oidc-provider",
+				"--name", clusterName,
+				"--approve",
+				"--verbose", "2",
+			)
+		Expect(cmd).To(RunSuccessfully())
 
-			By("Creating an IAMServiceAccount")
-			cmd = params.EksctlCreateCmd.
-				WithArgs(
-					"iamserviceaccount",
-					"--cluster", clusterName,
-					"--name", "test-sa",
-					"--namespace", "default",
-					"--attach-policy-arn",
-					"arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
-					"--approve",
-					"--verbose", "2",
-				)
-			Expect(cmd).To(RunSuccessfully())
+		By("creating an IAMServiceAccount")
+		cmd = params.EksctlCreateCmd.
+			WithArgs(
+				"iamserviceaccount",
+				"--cluster", clusterName,
+				"--name", "test-sa",
+				"--namespace", "default",
+				"--attach-policy-arn",
+				"arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
+				"--approve",
+				"--verbose", "2",
+			)
+		Expect(cmd).To(RunSuccessfully())
 
-			By("Getting IAMServiceAccounts")
-			cmd = params.EksctlGetCmd.
-				WithArgs(
-					"iamserviceaccounts",
-					"--cluster", clusterName,
-					"--verbose", "2",
-				)
-			Expect(cmd).To(RunSuccessfullyWithOutputStringLines(
-				ContainElement(ContainSubstring("test-sa")),
-			))
+		By("getting IAMServiceAccounts")
+		cmd = params.EksctlGetCmd.
+			WithArgs(
+				"iamserviceaccounts",
+				"--cluster", clusterName,
+				"--verbose", "2",
+			)
+		Expect(cmd).To(RunSuccessfullyWithOutputStringLines(
+			ContainElement(ContainSubstring("test-sa")),
+		))
+	})
 
-			By("Upgrading the cluster")
-			cmd = params.EksctlUpgradeCmd.
-				WithArgs(
-					"cluster",
-					"--name", clusterName,
-					"--version", "1.18",
-					"--approve",
-					"--verbose", "2",
-				)
-			Expect(cmd).To(RunSuccessfully())
+	It("supports cluster upgrades", func() {
+		By("upgrading the cluster")
+		cmd := params.EksctlUpgradeCmd.
+			WithArgs(
+				"cluster",
+				"--name", clusterName,
+				"--version", "1.18",
+				"--approve",
+				"--verbose", "2",
+			)
+		Expect(cmd).To(RunSuccessfully())
+	})
 
-			By("Creating an addon")
-			cmd = params.EksctlCreateCmd.
-				WithArgs(
-					"addon",
-					"--cluster", clusterName,
-					"--name", "vpc-cni",
-					"--verbose", "2",
-				)
-			Expect(cmd).To(RunSuccessfully())
+	It("supports addons", func() {
+		By("creating an addon")
+		cmd := params.EksctlCreateCmd.
+			WithArgs(
+				"addon",
+				"--cluster", clusterName,
+				"--name", "vpc-cni",
+				"--verbose", "2",
+			)
+		Expect(cmd).To(RunSuccessfully())
 
-			By("Getting an addon")
-			cmd = params.EksctlGetCmd.
-				WithArgs(
-					"addons",
-					"--cluster", clusterName,
-					"--verbose", "2",
-				)
-			Expect(cmd).To(RunSuccessfullyWithOutputStringLines(
-				ContainElement(ContainSubstring("vpc-cni")),
-			))
+		By("getting an addon")
+		cmd = params.EksctlGetCmd.
+			WithArgs(
+				"addons",
+				"--cluster", clusterName,
+				"--verbose", "2",
+			)
+		Expect(cmd).To(RunSuccessfullyWithOutputStringLines(
+			ContainElement(ContainSubstring("vpc-cni")),
+		))
+	})
 
-			By("Creating a fargate profile")
-			cmd = params.EksctlCreateCmd.
-				WithArgs(
-					"fargateprofile",
-					"--cluster", clusterName,
-					"--name", "fp-test",
-					"--namespace", "default",
-				)
-			Expect(cmd).To(RunSuccessfullyWithOutputStringLines(
-				ContainElement(SatisfyAll(ContainSubstring("created"), ContainSubstring("fp-test"))),
-			))
+	It("supports fargate", func() {
+		By("creating a fargate profile")
+		cmd := params.EksctlCreateCmd.
+			WithArgs(
+				"fargateprofile",
+				"--cluster", clusterName,
+				"--name", "fp-test",
+				"--namespace", "default",
+			)
+		Expect(cmd).To(RunSuccessfullyWithOutputStringLines(
+			ContainElement(SatisfyAll(ContainSubstring("created"), ContainSubstring("fp-test"))),
+		))
 
-			By("Getting a fargate profile")
-			cmd = params.EksctlGetCmd.
-				WithArgs(
-					"fargateprofile",
-					"--cluster", clusterName,
-					"--verbose", "2",
-				)
-			Expect(cmd).To(RunSuccessfullyWithOutputStringLines(
-				ContainElement(ContainSubstring("fp-test")),
-			))
+		By("getting a fargate profile")
+		cmd = params.EksctlGetCmd.
+			WithArgs(
+				"fargateprofile",
+				"--cluster", clusterName,
+				"--verbose", "2",
+			)
+		Expect(cmd).To(RunSuccessfullyWithOutputStringLines(
+			ContainElement(ContainSubstring("fp-test")),
+		))
 
-			By("Deleting a fargate profile")
-			cmd = params.EksctlDeleteCmd.
-				WithArgs(
-					"fargateprofile",
-					"--cluster", clusterName,
-					"--name", "fp-test",
-					"--wait",
-				)
-			Expect(cmd).To(RunSuccessfullyWithOutputStringLines(
-				ContainElement(SatisfyAll(ContainSubstring("deleted"), ContainSubstring("fp-test"))),
-			))
+		By("deleting a fargate profile")
+		cmd = params.EksctlDeleteCmd.
+			WithArgs(
+				"fargateprofile",
+				"--cluster", clusterName,
+				"--name", "fp-test",
+				"--wait",
+			)
+		Expect(cmd).To(RunSuccessfullyWithOutputStringLines(
+			ContainElement(SatisfyAll(ContainSubstring("deleted"), ContainSubstring("fp-test"))),
+		))
+	})
 
-			By("Upgrading one of the nodegroups")
-			cmd = params.EksctlUpgradeCmd.
-				WithArgs(
-					"nodegroup",
-					"--name", ng1,
-					"--cluster", clusterName,
-					"--kubernetes-version", "1.18",
-					"--wait",
-					"--verbose", "2",
-				)
-			Expect(cmd).To(RunSuccessfully())
+	It("supports nodegroup upgrades", func() {
+		cmd := params.EksctlUpgradeCmd.
+			WithArgs(
+				"nodegroup",
+				"--name", ng1,
+				"--cluster", clusterName,
+				"--kubernetes-version", "1.18",
+				"--wait",
+				"--verbose", "2",
+			)
+		Expect(cmd).To(RunSuccessfully())
+	})
 
-			By("Scaling a nodegroup")
-			cmd = params.EksctlScaleNodeGroupCmd.
-				WithArgs(
-					"--name", ng1,
-					"--nodes", "2",
-					"--nodes-max", "3",
-					"--cluster", clusterName,
-					"--verbose", "2",
-				)
-			Expect(cmd).To(RunSuccessfully())
+	It("supports draining and scaling nodegroups", func() {
+		By("scaling a nodegroup")
+		cmd := params.EksctlScaleNodeGroupCmd.
+			WithArgs(
+				"--name", ng1,
+				"--nodes", "2",
+				"--nodes-max", "3",
+				"--cluster", clusterName,
+				"--verbose", "2",
+			)
+		Expect(cmd).To(RunSuccessfully())
 
-			By("Draining a nodegroup")
-			cmd = params.EksctlDrainNodeGroupCmd.
-				WithArgs(
-					"--cluster", clusterName,
-					"--name", ng2,
-					"--verbose", "2",
-				)
-			Expect(cmd).To(RunSuccessfully())
+		By("draining a nodegroup")
+		cmd = params.EksctlDrainNodeGroupCmd.
+			WithArgs(
+				"--cluster", clusterName,
+				"--name", ng2,
+				"--verbose", "2",
+			)
+		Expect(cmd).To(RunSuccessfully())
+	})
 
-			By("Deleting a nodegroup")
-			cmd = params.EksctlDeleteCmd.
-				WithArgs(
-					"nodegroup",
-					"--name", ng2,
-					"--cluster", clusterName,
-					"--verbose", "2",
-				)
-			Expect(cmd).To(RunSuccessfully())
+	It("supports deleting nodegroups", func() {
+		cmd := params.EksctlDeleteCmd.
+			WithArgs(
+				"nodegroup",
+				"--name", ng2,
+				"--cluster", clusterName,
+				"--verbose", "2",
+			)
+		Expect(cmd).To(RunSuccessfully())
+	})
 
-			By("Deleting the cluster")
-			cmd = params.EksctlDeleteCmd.
-				WithArgs(
-					"cluster",
-					"--name", clusterName,
-					"--verbose", "2",
-				)
-			Expect(cmd).To(RunSuccessfully())
-		})
+	It("supports deleting clusters", func() {
+		By("deleting the cluster")
+		cmd := params.EksctlDeleteCmd.
+			WithArgs(
+				"cluster",
+				"--name", clusterName,
+				"--verbose", "2",
+			)
+		Expect(cmd).To(RunSuccessfully())
 	})
 })
 
