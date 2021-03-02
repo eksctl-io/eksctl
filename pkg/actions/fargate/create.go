77c74c3dd77dab9c5b2eaf204e902d207ed53aad
Claudia Beresford <claudiaberesford@gmail.com>
2021-03-02 13:52:32 +0000

    Stop creating new stackmanagers everywhere

--- pkg/actions/fargate/create.go
+++ pkg/actions/fargate/create.go
@@ -36,14 +36,14 @@ func (m *Manager) Create() error {
 			return errors.Wrap(err, "couldn't ensure unowned cluster is ready for fargate")
 		}
 	} else {
-		if err := ctl.LoadClusterIntoSpec(cfg); err != nil {
+		if err := ctl.LoadClusterIntoSpec(cfg, stackManager); err != nil {
 			return errors.Wrap(err, "couldn't load cluster into spec")
 		}
 	}
 
 	if !api.IsSetAndNonEmptyString(cfg.IAM.FargatePodExecutionRoleARN) {
 		// Read back the default Fargate pod execution role ARN from CloudFormation:
-		if err := ctl.NewStackManager(cfg).RefreshFargatePodExecutionRoleARN(); err != nil {
+		if err := stackManager.RefreshFargatePodExecutionRoleARN(); err != nil {
 			return errors.Wrap(err, "couldn't refresh role arn")
 		}
 	}
