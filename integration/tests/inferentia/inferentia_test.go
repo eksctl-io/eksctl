62115cad1e10007860f9ad948a6aea55456bc212
Jake Klein <jake@weave.works>
2021-03-12 11:31:41 +0000

    pend GPU integration tests (#3428)

--- integration/tests/inferentia/inferentia_test.go
+++ integration/tests/inferentia/inferentia_test.go
@@ -18,7 +18,6 @@ import (
 	"github.com/weaveworks/eksctl/integration/tests"
 	api "github.com/weaveworks/eksctl/pkg/apis/eksctl.io/v1alpha5"
 	"github.com/weaveworks/eksctl/pkg/eks"
-	"github.com/weaveworks/eksctl/pkg/testutils"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -39,7 +38,7 @@ func init() {
 }
 
 func TestInferentia(t *testing.T) {
-	testutils.RegisterAndRun(t)
+	//testutils.RegisterAndRun(t)
 }
 
 var _ = Describe("(Integration) Inferentia nodes", func() {
