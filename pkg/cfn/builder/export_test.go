c74e92c374189c07cfd5f8f49f5b74905b572fa5
Claudia <claudiaberesford@gmail.com>
2021-04-12 15:06:02 +0100

    Revert bootstrap changes (#3557)
    * Revert 9c34f52b
    
    * Update release notes

--- pkg/cfn/builder/export_test.go
+++
@@ -1,7 +0,0 @@
-package builder
-
-import "github.com/weaveworks/eksctl/pkg/nodebootstrap"
-
-func (n *NodeGroupResourceSet) SetBootstrapper(bootstrapper nodebootstrap.Bootstrapper) {
-	n.bootstrapper = bootstrapper
-}
