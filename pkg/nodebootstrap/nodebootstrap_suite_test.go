550ac0feeb1d28b0fcc558b54b5bdee9e45cef7d
Claudia Beresford <claudiaberesford@gmail.com>
2021-04-12 11:38:57 +0100

    Revert 9c34f52b

--- pkg/nodebootstrap/nodebootstrap_suite_test.go
+++ pkg/nodebootstrap/nodebootstrap_suite_test.go
@@ -1,21 +1,11 @@
-package nodebootstrap_test
+package nodebootstrap
 
 import (
 	"testing"
 
-	. "github.com/onsi/gomega"
-
-	"github.com/weaveworks/eksctl/pkg/cloudconfig"
 	"github.com/weaveworks/eksctl/pkg/testutils"
 )
 
 func TestNodebootstrap(t *testing.T) {
 	testutils.RegisterAndRun(t)
-}
-
-func decode(userData string) *cloudconfig.CloudConfig {
-	cloudCfg, err := cloudconfig.DecodeCloudConfig(userData)
-	Expect(err).NotTo(HaveOccurred())
-
-	return cloudCfg
 }
