5f6686d7b5879f584959865517ea550bc4781adf
Jake Klein <jake@weave.works>
2021-04-20 10:07:18 +0100

    Removed deprecated labels for 1.19+ clusters (#3567)

--- pkg/addons/default/kube_proxy.go
+++ pkg/addons/default/kube_proxy.go
@@ -13,14 +13,19 @@ import (
 	"github.com/weaveworks/eksctl/pkg/addons"
 	"github.com/weaveworks/eksctl/pkg/printers"
 
+	api "github.com/weaveworks/eksctl/pkg/apis/eksctl.io/v1alpha5"
 	apierrs "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/weaveworks/eksctl/pkg/utils"
 	"k8s.io/client-go/kubernetes"
 )
 
 const (
 	// KubeProxy is the name of the kube-proxy addon
-	KubeProxy = "kube-proxy"
+	KubeProxy     = "kube-proxy"
+	ArchBetaLabel = "beta.kubernetes.io/arch"
+	ArchLabel     = "kubernetes.io/arch"
 )
 
 func IsKubeProxyUpToDate(clientSet kubernetes.Interface, controlPlaneVersion string) (bool, error) {
@@ -58,7 +63,16 @@ func UpdateKubeProxy(clientSet kubernetes.Interface, controlPlaneVersion string,
 		return false, errors.Wrapf(err, "getting %q", KubeProxy)
 	}
 
-	hasArm64NodeSelector := daemeonSetHasArm64NodeSelector(d)
+	archLabel := ArchLabel
+	isMinVersion, err := utils.IsMinVersion(api.Version1_18, controlPlaneVersion)
+	if err != nil {
+		return false, err
+	}
+	if !isMinVersion {
+		archLabel = ArchBetaLabel
+	}
+
+	hasArm64NodeSelector := daemeonSetHasArm64NodeSelector(d, archLabel)
 	if !hasArm64NodeSelector {
 		logger.Info("missing arm64 nodeSelector value")
 	}
@@ -99,7 +113,7 @@ func UpdateKubeProxy(clientSet kubernetes.Interface, controlPlaneVersion string,
 	}
 
 	if !hasArm64NodeSelector {
-		addArm64NodeSelector(d)
+		addArm64NodeSelector(d, archLabel)
 	}
 
 	if _, err := clientSet.AppsV1().DaemonSets(metav1.NamespaceSystem).Update(context.TODO(), d, metav1.UpdateOptions{}); err != nil {
@@ -110,13 +124,13 @@ func UpdateKubeProxy(clientSet kubernetes.Interface, controlPlaneVersion string,
 	return false, nil
 }
 
-func daemeonSetHasArm64NodeSelector(daemonSet *v1.DaemonSet) bool {
+func daemeonSetHasArm64NodeSelector(daemonSet *v1.DaemonSet, archLabel string) bool {
 	if daemonSet.Spec.Template.Spec.Affinity != nil &&
 		daemonSet.Spec.Template.Spec.Affinity.NodeAffinity != nil &&
 		daemonSet.Spec.Template.Spec.Affinity.NodeAffinity.RequiredDuringSchedulingIgnoredDuringExecution != nil {
 		for _, nodeSelectorTerms := range daemonSet.Spec.Template.Spec.Affinity.NodeAffinity.RequiredDuringSchedulingIgnoredDuringExecution.NodeSelectorTerms {
 			for _, nodeSelector := range nodeSelectorTerms.MatchExpressions {
-				if nodeSelector.Key == "beta.kubernetes.io/arch" {
+				if nodeSelector.Key == archLabel {
 					for _, value := range nodeSelector.Values {
 						if value == "arm64" {
 							return true
@@ -129,11 +143,11 @@ func daemeonSetHasArm64NodeSelector(daemonSet *v1.DaemonSet) bool {
 	return false
 }
 
-func addArm64NodeSelector(daemonSet *v1.DaemonSet) {
+func addArm64NodeSelector(daemonSet *v1.DaemonSet, archLabel string) {
 	for nodeSelectorTermsIndex, nodeSelectorTerms := range daemonSet.Spec.Template.Spec.Affinity.NodeAffinity.
 		RequiredDuringSchedulingIgnoredDuringExecution.NodeSelectorTerms {
 		for nodeSelectorIndex, nodeSelector := range nodeSelectorTerms.MatchExpressions {
-			if nodeSelector.Key == "beta.kubernetes.io/arch" {
+			if nodeSelector.Key == archLabel {
 				daemonSet.Spec.Template.Spec.Affinity.NodeAffinity.RequiredDuringSchedulingIgnoredDuringExecution.
 					NodeSelectorTerms[nodeSelectorTermsIndex].MatchExpressions[nodeSelectorIndex].Values = append(nodeSelector.Values, "arm64")
 			}
