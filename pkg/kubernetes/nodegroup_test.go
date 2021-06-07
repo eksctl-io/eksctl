6eab4df79b5a17177fcb95e69dd1293a34851273
nikimanoledaki <18622989+nikimanoledaki@users.noreply.github.com>
2021-06-07 17:00:49 +0200

    Add unit tests for getting ng k8s version

--- pkg/kubernetes/nodegroup_test.go
+++ pkg/kubernetes/nodegroup_test.go
@@ -6,6 +6,7 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
+	"github.com/pkg/errors"
 	api "github.com/weaveworks/eksctl/pkg/apis/eksctl.io/v1alpha5"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -55,6 +56,26 @@ var _ = Describe("NodeGroup", func() {
 						},
 						Status: v1.NodeStatus{
 							NodeInfo: v1.NodeSystemInfo{
+								KubeletVersion: "1.19.6",
+							},
+						},
+					}, metav1.CreateOptions{})
+					Expect(err).NotTo(HaveOccurred())
+				},
+				expectedVersion: "1.19.6",
+			}),
+
+			Entry("[happy path] returns correct version with version trimming", ngEntry{
+				mockCalls: func(c *fake.Clientset) {
+					_, err := c.CoreV1().Nodes().Create(context.TODO(), &v1.Node{
+						ObjectMeta: metav1.ObjectMeta{
+							Name: "test-node",
+							Labels: map[string]string{
+								api.NodeGroupNameLabel: ngName,
+							},
+						},
+						Status: v1.NodeStatus{
+							NodeInfo: v1.NodeSystemInfo{
 								KubeletVersion: "v1.19.6-eks-49a6c0",
 							},
 						},
@@ -62,6 +83,18 @@ var _ = Describe("NodeGroup", func() {
 					Expect(err).NotTo(HaveOccurred())
 				},
 				expectedVersion: "1.19.6",
+			}),
+
+			Entry("fails to list nodes", ngEntry{
+				mockCalls: func(c *fake.Clientset) {
+					_, err := c.CoreV1().Nodes().Create(context.TODO(), &v1.Node{
+						ObjectMeta: metav1.ObjectMeta{
+							Name: "test-node",
+						},
+					}, metav1.CreateOptions{})
+					Expect(err).NotTo(HaveOccurred())
+				},
+				expError: errors.New("no nodes were found"),
 			}),
 		)
 	})
