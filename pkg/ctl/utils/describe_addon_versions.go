57210bbbb577a91ee3bc3dc0cc49eb1bac627b76
Jake Klein <jake@weave.works>
2021-09-08 17:27:55 +0100

    don't issue describe cluster call in eksctl utils describe-addon-versions (#4196)

--- pkg/ctl/utils/describe_addon_versions.go
+++ pkg/ctl/utils/describe_addon_versions.go
@@ -44,7 +44,7 @@ func describeAddonVersionsCmd(cmd *cmdutils.Cmd) {
 }
 
 func describeAddonVersions(cmd *cmdutils.Cmd, addonName, k8sVersion, clusterName string) error {
-	clusterProvider, err := cmd.NewProviderForExistingCluster()
+	clusterProvider, err := cmd.NewCtl()
 	if err != nil {
 		return err
 	}
