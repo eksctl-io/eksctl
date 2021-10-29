2c7ca5afe9a9d28500c462cbd6e232a1a7e397da
Yaash Jain <yaash.jain@alumni.ubc.ca>
2021-10-29 15:34:06 -0700

    Dump logs to disk on failure integration test
    A `eksctl get --name <does_not_exist>` command is run, with assertions.
    These assertions check if the code exits with status 1, and if a new
    failure log file is generated. The code also performs necessary cleanup.

---
+++ integration/tests/dump_logs/dump_logs_test.go
@@ -0,0 +1,60 @@
+//go:build integration
+// +build integration
+
+package dump_logs
+
+import (
+	"os"
+	"testing"
+
+	"github.com/pkg/errors"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/weaveworks/eksctl/integration/tests"
+
+	api "github.com/weaveworks/eksctl/pkg/apis/eksctl.io/v1alpha5"
+	"github.com/weaveworks/eksctl/pkg/testutils"
+)
+
+var params *tests.Params
+
+func init() {
+	testing.Init()
+	if err := api.Register(); err != nil {
+		panic(errors.Wrap(err, "unexpected error registering API scheme"))
+	}
+	params = tests.NewParams("dump_logs")
+}
+
+func TestDumpLogs(t *testing.T) {
+	testutils.RegisterAndRun(t)
+}
+
+var _ = Describe("(Integration) Dump logs on failure", func() {
+
+	os.RemoveAll("logs/")
+
+	cmd := params.EksctlGetCmd.WithArgs(
+		"cluster",
+		"--name",
+		"definitely_not_a_cluster",
+	)
+
+	session := cmd.Run()
+
+	It("exits with exit code 1", func() {
+		Expect(session.ExitCode()).To(Equal(1))
+	})
+
+	It("logs/ directory is created", func() {
+		Expect("logs/").Should(BeADirectory())
+	})
+
+	It("logs/ directory should have one file", func() {
+		d, err := os.ReadDir("logs/")
+		Expect(err).ShouldNot(HaveOccurred())
+		Expect(len(d)).To(Equal(1))
+		os.RemoveAll("logs/")
+	})
+})
