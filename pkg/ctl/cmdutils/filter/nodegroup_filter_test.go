f3682e2fbee2b9e7d070bd5a6374abf3603fcb52
Jake <jake@weave.works>
2021-11-01 15:50:41 +0000

    make 1.21 default

--- pkg/ctl/cmdutils/filter/nodegroup_filter_test.go
+++ pkg/ctl/cmdutils/filter/nodegroup_filter_test.go
@@ -339,7 +339,7 @@ const expected = `
 		"metadata": {
 		  "name": "test-3x3-ngs",
 		  "region": "eu-central-1",
-		  "version": "1.20"
+		  "version": "1.21"
 		},
 		"iam": {
 		  "withOIDC": false
