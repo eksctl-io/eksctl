ad597a8330e1b9b1e34a7c11b6fce470734bd701
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2021-11-24 13:44:31 +0100

    Removed unused code

--- pkg/apis/eksctl.io/v1alpha5/types.go
+++ pkg/apis/eksctl.io/v1alpha5/types.go
@@ -240,9 +240,6 @@ const (
 	// NodeGroupNameLabel defines the label of the nodegroup name
 	NodeGroupNameLabel = "alpha.eksctl.io/nodegroup-name"
 
-	// KarpenterNameTag defines the tag of the karpenter stack name
-	KarpenterNameTag = "alpha.eksctl.io/karpenter-name"
-
 	EKSNodeGroupNameLabel = "eks.amazonaws.com/nodegroup"
 
 	// SpotAllocationStrategyLowestPrice defines the ASG spot allocation strategy of lowest-price
