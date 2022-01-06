fc4e32fd243da171842a82ab9f6c2eb71ed23186
Chetan Patwal <cPu1@users.noreply.github.com>
2022-01-06 19:47:14 +0530

    Fix placement of ipFamily field in dry-run test (#4579)

--- integration/tests/dry_run/dry_run_test.go
+++ integration/tests/dry_run/dry_run_test.go
@@ -56,6 +56,8 @@ kind: ClusterConfig
 metadata:
   name: %[1]s
   region: us-west-2
+kubernetesNetworkConfig:
+  ipFamily: IPv4
 nodeGroups:
 - amiFamily: AmazonLinux2
   containerRuntime: dockerd
@@ -137,7 +139,6 @@ managedNodeGroups:
 privateCluster:
   enabled: false
 vpc:
-  ipFamily: IPv4
   autoAllocateIPv6: false
   cidr: 192.168.0.0/16
   clusterEndpoints:
