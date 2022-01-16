2d2b09df639b08ef9566e5a8759a87671ba7358c
Amit Ben Ami <benami.amit1@gmail.com>
2022-01-17 01:22:14 +0200

    Adding the `newNodeGroupManager` as exported for easier mocking

--- pkg/actions/cluster/export_test.go
+++ pkg/actions/cluster/export_test.go
@@ -1,6 +1,11 @@
 package cluster
 
-import "github.com/weaveworks/eksctl/pkg/kubernetes"
+import (
+	"github.com/weaveworks/eksctl/pkg/actions/nodegroup"
+	api "github.com/weaveworks/eksctl/pkg/apis/eksctl.io/v1alpha5"
+	"github.com/weaveworks/eksctl/pkg/eks"
+	"github.com/weaveworks/eksctl/pkg/kubernetes"
+)
 
 func (c *UnownedCluster) SetNewClientSet(newClientSet func() (kubernetes.Interface, error)) {
 	c.newClientSet = newClientSet
@@ -8,4 +13,12 @@ func (c *UnownedCluster) SetNewClientSet(newClientSet func() (kubernetes.Interfa
 
 func (c *OwnedCluster) SetNewClientSet(newClientSet func() (kubernetes.Interface, error)) {
 	c.newClientSet = newClientSet
+}
+
+func (c *UnownedCluster) SetNewNodeGroupManager(newNodeGroupManager func(cfg *api.ClusterConfig, ctl *eks.ClusterProvider, clientSet kubernetes.Interface) *nodegroup.Manager) {
+	c.newNodeGroupManager = newNodeGroupManager
+}
+
+func (c *OwnedCluster) SetNewNodeGroupManager(newNodeGroupManager func(cfg *api.ClusterConfig, ctl *eks.ClusterProvider, clientSet kubernetes.Interface) *nodegroup.Manager) {
+	c.newNodeGroupManager = newNodeGroupManager
 }
