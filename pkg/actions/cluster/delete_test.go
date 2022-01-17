f01e0fa8f4d71a75faa2e1afa29ed7e2f652f841
Amit Ben Ami <benami.amit1@gmail.com>
2022-01-17 16:14:01 +0200

    Adding the first test of the drain
    Signed-off-by: Amit Ben Ami <benami.amit1@gmail.com>

---
+++ pkg/actions/cluster/delete_test.go
@@ -0,0 +1,63 @@
+package cluster
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	api "github.com/weaveworks/eksctl/pkg/apis/eksctl.io/v1alpha5"
+	"github.com/weaveworks/eksctl/pkg/cfn/manager"
+	"github.com/weaveworks/eksctl/pkg/cfn/manager/fakes"
+	"github.com/weaveworks/eksctl/pkg/eks"
+	"github.com/weaveworks/eksctl/pkg/kubernetes"
+	"k8s.io/client-go/kubernetes/fake"
+	"time"
+
+	"github.com/weaveworks/eksctl/pkg/testutils/mockprovider"
+)
+
+var _ = Describe("Delete", func() {
+	var (
+		clusterName      string
+		p                *mockprovider.MockProvider
+		cfg              *api.ClusterConfig
+		fakeStackManager *fakes.FakeStackManager
+		fakeClientSet    *fake.Clientset
+		ctl              *eks.ClusterProvider
+	)
+
+	BeforeEach(func() {
+		clusterName = "my-cluster"
+		p = mockprovider.NewMockProvider()
+		cfg = api.NewClusterConfig()
+		cfg.Metadata.Name = clusterName
+		fakeStackManager = new(fakes.FakeStackManager)
+		fakeClientSet = fake.NewSimpleClientset()
+		ctl = &eks.ClusterProvider{Provider: p, Status: &eks.ProviderStatus{}}
+	})
+
+	Context("draining node groups", func() {
+		It("drain the node groups without disabling the eviction", func() {
+			c := NewOwnedCluster(cfg, ctl, nil, fakeStackManager)
+			c.SetNewClientSet(func() (kubernetes.Interface, error) {
+				return fakeClientSet, nil
+			})
+
+			nodeGroupStacks := []manager.NodeGroupStack{{NodeGroupName: "ng-1"}}
+			disableEviction := false
+
+			nodeGroupDrainerCalled := 0
+			nodeGroupDrainer := func(nodeGroups []eks.KubeNodeGroup, plan bool, maxGracePeriod time.Duration, disableEviction bool) error {
+				nodeGroupDrainerCalled++
+				return nil
+			}
+			vpcCniDeleterCalled := 0
+			vpcCniDeleter := func(clusterName string, ctl *eks.ClusterProvider, clientSet kubernetes.Interface) {
+				vpcCniDeleterCalled++
+			}
+
+			err := drainAllNodeGroups(c.cfg, c.ctl, fakeClientSet, nodeGroupStacks, &disableEviction, nodeGroupDrainer, vpcCniDeleter)
+			Expect(err).To(BeNil())
+			Expect(nodeGroupDrainerCalled).To(Equal(1))
+			Expect(vpcCniDeleterCalled).To(Equal(1))
+		})
+	})
+})
