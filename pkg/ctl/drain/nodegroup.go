3909f101e7a16aeb3ecac5e37830941b29e5d53b
Ray Terrill <rayterrill@users.noreply.github.com>
2022-01-17 13:53:39 -0800

    Update pkg/ctl/drain/nodegroup.go
    Co-authored-by: Chetan Patwal <cPu1@users.noreply.github.com>

--- pkg/ctl/drain/nodegroup.go
+++ pkg/ctl/drain/nodegroup.go
@@ -58,7 +58,7 @@ func drainNodeGroupWithRunFunc(cmd *cmdutils.Cmd, runFunc func(cmd *cmdutils.Cmd
 	cmdutils.AddCommonFlagsForAWS(cmd.FlagSetGroup, &cmd.ProviderConfig, true)
 }
 
-func doDrainNodeGroup(cmd *cmdutils.Cmd, ng *api.NodeGroup, undo, onlyMissing bool, maxGracePeriod time.Duration, nodeDrainWaitPeriod time.Duration, disableEviction bool) error {
+func doDrainNodeGroup(cmd *cmdutils.Cmd, ng *api.NodeGroup, undo, onlyMissing bool, maxGracePeriod, nodeDrainWaitPeriod time.Duration, disableEviction bool) error {
 	ngFilter := filter.NewNodeGroupFilter()
 
 	if err := cmdutils.NewDeleteNodeGroupLoader(cmd, ng, ngFilter).Load(); err != nil {
