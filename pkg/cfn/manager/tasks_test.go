3e1f6665f1771760f6e8eff90bec21d38244c57c
Nicolas Degory <nicolas.degory@gmail.com>
2022-03-21 20:36:05 -0700

    add a test for IAM Service Account stack creation

--- pkg/cfn/manager/tasks_test.go
+++ pkg/cfn/manager/tasks_test.go
@@ -3,12 +3,19 @@ package manager
 import (
 	"fmt"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/request"
+	"github.com/aws/aws-sdk-go/awstesting"
+	"github.com/aws/aws-sdk-go/service/cloudformation"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/pkg/errors"
 	"github.com/stretchr/testify/mock"
+	"k8s.io/client-go/kubernetes/fake"
 
 	api "github.com/weaveworks/eksctl/pkg/apis/eksctl.io/v1alpha5"
+	iamoidc "github.com/weaveworks/eksctl/pkg/iam/oidc"
+	"github.com/weaveworks/eksctl/pkg/kubernetes"
 	"github.com/weaveworks/eksctl/pkg/testutils/mockprovider"
 	vpcfakes "github.com/weaveworks/eksctl/pkg/vpc/fakes"
 )
@@ -198,6 +205,282 @@ var _ = Describe("StackCollection Tasks", func() {
 					Expect(errs).To(HaveLen(1))
 					Expect(errs[0]).To(MatchError(ContainSubstring("not found")))
 				})
+			})
+		})
+	})
+
+	Describe("IAMServiceAccountTask", func() {
+		var (
+			clusterName        string
+			iamServiceAccounts []*api.ClusterIAMServiceAccount
+			name               string
+			namespace          string
+			oidc               *iamoidc.OpenIDConnectManager
+			stackArn           string
+			stackArnPrefix     string
+			stackName          string
+		)
+		// service account creation should succeed
+		When("service account doesn't exist yet", func() {
+			// setup
+			BeforeEach(func() {
+				clusterName = "cluster1"
+				namespace = "default"
+				name = "new"
+				cfg = newClusterConfig(clusterName)
+				iamServiceAccounts = []*api.ClusterIAMServiceAccount{
+					{
+						ClusterIAMMeta: api.ClusterIAMMeta{
+							Name:      name,
+							Namespace: namespace,
+						},
+						AttachPolicyARNs: []string{"arn-123"},
+					},
+				}
+				cfg.IAM.ServiceAccounts = iamServiceAccounts
+				var err error
+				oidc, err = iamoidc.NewOpenIDConnectManager(nil, "456123987123", "https://oidc.eks.us-west-2.amazonaws.com/id/A39A2842863C47208955D753DE205E6E", "aws", nil)
+				Expect(err).NotTo(HaveOccurred())
+				oidc.ProviderARN = "arn:aws:iam::456123987123:oidc-provider/oidc.eks.us-west-2.amazonaws.com/id/A39A2842863C47208955D753DE205E6E"
+				p = mockprovider.NewMockProvider()
+				stackManager = NewStackCollection(p, cfg)
+				stackName = fmt.Sprintf("eksctl-%s-addon-iamserviceaccount-%s-%s", clusterName, namespace, name)
+				stackArn = fmt.Sprintf("arn:aws:cloudformation:eu-west-1:456123987123:stack/%s/01", stackName)
+				p.MockCloudFormation().On("DescribeStacks", &cloudformation.DescribeStacksInput{StackName: aws.String(stackName)}).Return(&cloudformation.DescribeStacksOutput{}, nil)
+				p.MockCloudFormation().On("CreateStack", mock.Anything).Return(&cloudformation.CreateStackOutput{StackId: aws.String(stackArn)}, nil)
+				describeStacksOutput := &cloudformation.DescribeStacksOutput{
+					Stacks: []*cloudformation.Stack{
+						{
+							StackName:   aws.String(stackName),
+							StackId:     aws.String(stackArn),
+							StackStatus: aws.String(cloudformation.StackStatusCreateComplete),
+							Tags: []*cloudformation.Tag{
+								{
+									Key:   aws.String("alpha.eksctl.io/cluster-name"),
+									Value: aws.String(clusterName),
+								},
+							},
+							Outputs: []*cloudformation.Output{
+								{
+									Description: aws.String("IAM Role"),
+									OutputKey:   aws.String("Role1"),
+									OutputValue: aws.String("role-arn-123"),
+									ExportName:  aws.String("role-arn-123"),
+								},
+							},
+						},
+					},
+				}
+				p.MockCloudFormation().On("DescribeStacks", &cloudformation.DescribeStacksInput{StackName: aws.String(stackArn)}).Return(describeStacksOutput, nil)
+				req := awstesting.NewClient(aws.NewConfig().WithRegion("us-west-2")).NewRequest(&request.Operation{Name: "Operation"}, nil, describeStacksOutput)
+				p.MockCloudFormation().On("DescribeStacksRequest", &cloudformation.DescribeStacksInput{StackName: aws.String(stackArn)}).Return(req, describeStacksOutput)
+			})
+			It("creates a new stack without errors", func() {
+				tasks := stackManager.NewTasksToCreateIAMServiceAccounts(iamServiceAccounts, oidc, kubernetes.NewCachedClientSet(fake.NewSimpleClientset()))
+				Expect(tasks.Len()).To(Equal(1))
+				Expect(tasks.Describe()).To(Equal(`1 task: { 
+    2 sequential sub-tasks: { 
+        create IAM role for serviceaccount "default/new",
+        create serviceaccount "default/new",
+    } }`))
+				errs := tasks.DoAllSync()
+				Expect(errs).To(HaveLen(0))
+				// all calls should happen
+				Expect(p.MockCloudFormation().AssertExpectations(GinkgoT()))
+			})
+		})
+		// existing stack in CREATE_COMPLETE status, shouldn't fail
+		When("service account already exists", func() {
+			BeforeEach(func() {
+				clusterName = "cluster1"
+				namespace = "default"
+				name = "existing"
+				cfg = newClusterConfig(clusterName)
+				iamServiceAccounts = []*api.ClusterIAMServiceAccount{
+					{
+						ClusterIAMMeta: api.ClusterIAMMeta{
+							Name:      name,
+							Namespace: namespace,
+						},
+						AttachPolicyARNs: []string{"arn-123"},
+					},
+				}
+				cfg.IAM.ServiceAccounts = iamServiceAccounts
+				var err error
+				oidc, err = iamoidc.NewOpenIDConnectManager(nil, "456123987123", "https://oidc.eks.us-west-2.amazonaws.com/id/A39A2842863C47208955D753DE205E6E", "aws", nil)
+				Expect(err).NotTo(HaveOccurred())
+				oidc.ProviderARN = "arn:aws:iam::456123987123:oidc-provider/oidc.eks.us-west-2.amazonaws.com/id/A39A2842863C47208955D753DE205E6E"
+				p = mockprovider.NewMockProvider()
+				stackManager = NewStackCollection(p, cfg)
+				stackName = fmt.Sprintf("eksctl-%s-addon-iamserviceaccount-%s-%s", clusterName, namespace, name)
+				stackArnPrefix = fmt.Sprintf("arn:aws:cloudformation:eu-west-1:456123987123:stack/%s", stackName)
+				describeStacksOutput01 := &cloudformation.DescribeStacksOutput{
+					Stacks: []*cloudformation.Stack{
+						{
+							StackName:   aws.String(stackName),
+							StackId:     aws.String(fmt.Sprintf("%s/01", stackArnPrefix)),
+							StackStatus: aws.String(cloudformation.StackStatusCreateComplete),
+							Tags: []*cloudformation.Tag{
+								{
+									Key:   aws.String("alpha.eksctl.io/cluster-name"),
+									Value: aws.String(clusterName),
+								},
+							},
+							Outputs: []*cloudformation.Output{
+								{
+									Description: aws.String("IAM Role"),
+									OutputKey:   aws.String("Role1"),
+									OutputValue: aws.String("role-arn-123"),
+									ExportName:  aws.String("role-arn-123"),
+								},
+							},
+						},
+					},
+				}
+				describeStacksOutput02 := &cloudformation.DescribeStacksOutput{
+					Stacks: []*cloudformation.Stack{
+						{
+							StackName:   aws.String(stackName),
+							StackId:     aws.String(fmt.Sprintf("%s/02", stackArnPrefix)),
+							StackStatus: aws.String(cloudformation.StackStatusCreateComplete),
+							Tags: []*cloudformation.Tag{
+								{
+									Key:   aws.String("alpha.eksctl.io/cluster-name"),
+									Value: aws.String(clusterName),
+								},
+							},
+							Outputs: []*cloudformation.Output{
+								{
+									Description: aws.String("IAM Role"),
+									OutputKey:   aws.String("Role1"),
+									OutputValue: aws.String("role-arn-123"),
+									ExportName:  aws.String("role-arn-123"),
+								},
+							},
+						},
+					},
+				}
+				// filter on stack name when looking for a unknown stack
+				p.MockCloudFormation().On("DescribeStacks", &cloudformation.DescribeStacksInput{StackName: aws.String(stackName)}).Return(describeStacksOutput01, nil)
+				// filter on stack id when looking for a known stack
+				p.MockCloudFormation().On("DescribeStacks", &cloudformation.DescribeStacksInput{StackName: aws.String(fmt.Sprintf("%s/02", stackArnPrefix))}).Return(describeStacksOutput02, nil)
+				p.MockCloudFormation().On("CreateStack", mock.Anything).Return(&cloudformation.CreateStackOutput{StackId: aws.String(fmt.Sprintf("%s/02", stackArnPrefix))}, nil)
+				req := awstesting.NewClient(aws.NewConfig().WithRegion("us-west-2")).NewRequest(&request.Operation{Name: "Operation"}, nil, describeStacksOutput02)
+				p.MockCloudFormation().On("DescribeStacksRequest", &cloudformation.DescribeStacksInput{StackName: aws.String(fmt.Sprintf("%s/02", stackArnPrefix))}).Return(req, describeStacksOutput02)
+			})
+			It("doesn't fail", func() {
+				tasks := stackManager.NewTasksToCreateIAMServiceAccounts(iamServiceAccounts, oidc, kubernetes.NewCachedClientSet(fake.NewSimpleClientset()))
+				Expect(tasks.Len()).To(Equal(1))
+				errs := tasks.DoAllSync()
+				Expect(errs).To(HaveLen(0))
+				// all calls should happen
+				Expect(p.MockCloudFormation().AssertExpectations(GinkgoT()))
+			})
+		})
+		// should delete and attempt to recreate the stack
+		When("service account exists and is in ROLLBACK_COMPLETE status", func() {
+			BeforeEach(func() {
+				clusterName = "cluster1"
+				namespace = "default"
+				name = "rollback"
+				cfg = newClusterConfig(clusterName)
+				iamServiceAccounts = []*api.ClusterIAMServiceAccount{
+					{
+						ClusterIAMMeta: api.ClusterIAMMeta{
+							Name:      name,
+							Namespace: namespace,
+						},
+						AttachPolicyARNs: []string{"arn-123"},
+					},
+				}
+				cfg.IAM.ServiceAccounts = iamServiceAccounts
+				var err error
+				oidc, err = iamoidc.NewOpenIDConnectManager(nil, "456123987123", "https://oidc.eks.us-west-2.amazonaws.com/id/A39A2842863C47208955D753DE205E6E", "aws", nil)
+				Expect(err).NotTo(HaveOccurred())
+				oidc.ProviderARN = "arn:aws:iam::456123987123:oidc-provider/oidc.eks.us-west-2.amazonaws.com/id/A39A2842863C47208955D753DE205E6E"
+				p = mockprovider.NewMockProvider()
+				p.SetRegion("us-west-2")
+				stackManager = NewStackCollection(p, cfg)
+				stackName = fmt.Sprintf("eksctl-%s-addon-iamserviceaccount-%s-%s", clusterName, namespace, name)
+				stackArnPrefix = fmt.Sprintf("arn:aws:cloudformation:eu-west-1:456123987123:stack/%s", stackName)
+				describeStacksOutput01 := &cloudformation.DescribeStacksOutput{
+					Stacks: []*cloudformation.Stack{
+						{
+							StackName:   aws.String(stackName),
+							StackId:     aws.String(fmt.Sprintf("%s/01", stackArnPrefix)),
+							StackStatus: aws.String(cloudformation.StackStatusRollbackComplete),
+							Tags: []*cloudformation.Tag{
+								{
+									Key:   aws.String("alpha.eksctl.io/cluster-name"),
+									Value: aws.String(clusterName),
+								},
+							},
+						},
+					},
+				}
+				describeStacksOutput01Deleted := &cloudformation.DescribeStacksOutput{
+					Stacks: []*cloudformation.Stack{
+						{
+							StackName:   aws.String(stackName),
+							StackId:     aws.String(fmt.Sprintf("%s/01", stackArnPrefix)),
+							StackStatus: aws.String(cloudformation.StackStatusDeleteComplete),
+							Tags: []*cloudformation.Tag{
+								{
+									Key:   aws.String("alpha.eksctl.io/cluster-name"),
+									Value: aws.String(clusterName),
+								},
+							},
+						},
+					},
+				}
+				describeStacksOutput02 := &cloudformation.DescribeStacksOutput{
+					Stacks: []*cloudformation.Stack{
+						{
+							StackName:   aws.String(stackName),
+							StackId:     aws.String(fmt.Sprintf("%s/02", stackArnPrefix)),
+							StackStatus: aws.String(cloudformation.StackStatusCreateComplete),
+							Tags: []*cloudformation.Tag{
+								{
+									Key:   aws.String("alpha.eksctl.io/cluster-name"),
+									Value: aws.String(clusterName),
+								},
+							},
+							Outputs: []*cloudformation.Output{
+								{
+									Description: aws.String("IAM Role"),
+									OutputKey:   aws.String("Role1"),
+									OutputValue: aws.String("role-arn-123"),
+									ExportName:  aws.String("role-arn-123"),
+								},
+							},
+						},
+					},
+				}
+				p.MockCloudFormation().On("DescribeStacks", &cloudformation.DescribeStacksInput{StackName: aws.String(stackName)}).Return(describeStacksOutput01, nil)
+				p.MockCloudFormation().On("DescribeStacks", &cloudformation.DescribeStacksInput{StackName: aws.String(fmt.Sprintf("%s/02", stackArnPrefix))}).Return(describeStacksOutput02, nil)
+				p.MockCloudFormation().On("CreateStack", mock.Anything).Return(&cloudformation.CreateStackOutput{StackId: aws.String(fmt.Sprintf("%s/02", stackArnPrefix))}, nil)
+				p.MockCloudFormation().On("DeleteStack", mock.Anything).Return(&cloudformation.DeleteStackOutput{}, nil)
+				// for deletion request
+				req01 := awstesting.NewClient(aws.NewConfig().WithRegion("us-west-2")).NewRequest(&request.Operation{Name: "Operation"}, nil, describeStacksOutput01Deleted)
+				p.MockCloudFormation().On("DescribeStacksRequest", &cloudformation.DescribeStacksInput{StackName: aws.String(fmt.Sprintf("%s/01", stackArnPrefix))}).Return(req01, describeStacksOutput01Deleted)
+				// for creation request
+				req02 := awstesting.NewClient(aws.NewConfig().WithRegion("us-west-2")).NewRequest(&request.Operation{Name: "Operation"}, nil, describeStacksOutput02)
+				p.MockCloudFormation().On("DescribeStacksRequest", &cloudformation.DescribeStacksInput{StackName: aws.String(fmt.Sprintf("%s/02", stackArnPrefix))}).Return(req02, describeStacksOutput02)
+			})
+			It("doesn't fail", func() {
+				tasks := stackManager.NewTasksToCreateIAMServiceAccounts(iamServiceAccounts, oidc, kubernetes.NewCachedClientSet(fake.NewSimpleClientset()))
+				Expect(tasks.Describe()).To(Equal(`1 task: { 
+    2 sequential sub-tasks: { 
+        create IAM role for serviceaccount "default/rollback",
+        create serviceaccount "default/rollback",
+    } }`))
+				Expect(tasks.Len()).To(Equal(1))
+				errs := tasks.DoAllSync()
+				Expect(errs).To(HaveLen(0))
+				// all calls should happen
+				Expect(p.MockCloudFormation().AssertExpectations(GinkgoT()))
+				// all calls should happen
+				Expect(p.MockCloudFormation().AssertExpectations(GinkgoT()))
 			})
 		})
 	})
