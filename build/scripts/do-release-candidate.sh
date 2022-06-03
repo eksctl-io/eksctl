ef3505c69fe2b8104697b764dfcf19adb4450430
Chetan Patwal <cPu1@users.noreply.github.com>
2022-06-03 16:44:23 +0530

    Automate release process (#5369)

--- build/scripts/do-release-candidate.sh
+++ build/scripts/do-release-candidate.sh
@@ -13,6 +13,9 @@ if [ ! -f "${RELEASE_NOTES_FILE}" ]; then
     exit 1
 fi
 
+# Update eksctl version to release-candidate
+pre_release_id="${tag#*-}"
+
 export RELEASE_DESCRIPTION="${tag}"
 
-GORELEASER_CURRENT_TAG=v${tag} goreleaser release --rm-dist --timeout 60m --skip-validate --config=./.goreleaser.yml --release-notes="${RELEASE_NOTES_FILE}"
+GORELEASER_CURRENT_TAG="v${tag}" PRE_RELEASE_ID="${pre_release_id}" goreleaser release --rm-dist --timeout 60m --skip-validate --config=./.goreleaser.yml --release-notes="${RELEASE_NOTES_FILE}"
