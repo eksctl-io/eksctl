111b65195dd947c9446d743004f0ffbe23389aaa
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2022-06-10 18:00:37 +0200

    Added new testcases

--- pkg/cfn/waiter/update_nodegroup_test.go
+++ pkg/cfn/waiter/update_nodegroup_test.go
@@ -1,6 +1,7 @@
 package waiter
 
 import (
+	"errors"
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
@@ -24,9 +25,43 @@ var _ = Describe("WaitForNodegroupUpdate", func() {
 			},
 		}, nil)
 		status, err := WaitForNodegroupUpdate("update-1", eksAPI, 35*time.Second, func(attempts int) time.Duration {
-			return 1 * time.Second
+			return 1 * time.Nanosecond
 		})
 		Expect(status).To(Equal(string(ekstypes.UpdateStatusSuccessful)))
 		Expect(err).NotTo(HaveOccurred())
+	})
+	When("describe fails", func() {
+		It("errors", func() {
+			eksAPI := &mocksv2.EKS{}
+			eksAPI.On("DescribeUpdate", mock.Anything, &eks.DescribeUpdateInput{
+				UpdateId: aws.String("update-1"),
+			}).Return(&eks.DescribeUpdateOutput{
+				Update: &ekstypes.Update{
+					Status: ekstypes.UpdateStatusFailed,
+				},
+			}, errors.New("nope"))
+			status, err := WaitForNodegroupUpdate("update-1", eksAPI, 35*time.Second, func(attempts int) time.Duration {
+				return 1 * time.Nanosecond
+			})
+			Expect(status).To(BeEmpty())
+			Expect(err).To(MatchError(ContainSubstring("failed to describe update for update id update-1: nope")))
+		})
+	})
+	When("status is failed", func() {
+		It("errors", func() {
+			eksAPI := &mocksv2.EKS{}
+			eksAPI.On("DescribeUpdate", mock.Anything, &eks.DescribeUpdateInput{
+				UpdateId: aws.String("update-1"),
+			}).Return(&eks.DescribeUpdateOutput{
+				Update: &ekstypes.Update{
+					Status: ekstypes.UpdateStatusFailed,
+				},
+			}, nil)
+			status, err := WaitForNodegroupUpdate("update-1", eksAPI, 35*time.Second, func(attempts int) time.Duration {
+				return 1 * time.Nanosecond
+			})
+			Expect(status).To(Equal(string(ekstypes.UpdateStatusFailed)))
+			Expect(err).To(MatchError(ContainSubstring("update failed or was cancelled")))
+		})
 	})
 })
