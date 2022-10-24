6116bb10eef05f661a2860b5e46dc5d4570aee6d
Himangini <himangini@weave.works>
2022-10-24 20:41:52 +0100

    updated the supported version of karpenter to 0.17.0

--- pkg/apis/eksctl.io/v1alpha5/types.go
+++ pkg/apis/eksctl.io/v1alpha5/types.go
@@ -372,7 +372,7 @@ const (
 
 // supported version of Karpenter
 const (
-	supportedKarpenterVersion = "0.15.0"
+	supportedKarpenterVersion = "0.17.0"
 )
 
 // Values for Capacity Reservation Preference
