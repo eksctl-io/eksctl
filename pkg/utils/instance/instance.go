a6767ed9a016dfc882b5dac79b6abeea31c4ada3
Tibi <110664232+TiberiuGC@users.noreply.github.com>
2023-03-17 09:06:57 +0200

    Add m7g and r7g as ARM instance types (#6436)

--- pkg/utils/instance/instance.go
+++ pkg/utils/instance/instance.go
@@ -12,9 +12,11 @@ func IsARMInstanceType(instanceType string) bool {
 	return strings.HasPrefix(instanceType, "a1") ||
 		strings.HasPrefix(instanceType, "t4g") ||
 		strings.HasPrefix(instanceType, "m6g") ||
+		strings.HasPrefix(instanceType, "m7g") ||
 		strings.HasPrefix(instanceType, "c6g") ||
 		strings.HasPrefix(instanceType, "c7g") ||
 		strings.HasPrefix(instanceType, "r6g") ||
+		strings.HasPrefix(instanceType, "r7g") ||
 		strings.HasPrefix(instanceType, "im4g") ||
 		strings.HasPrefix(instanceType, "is4g") ||
 		strings.HasPrefix(instanceType, "g5g") ||
