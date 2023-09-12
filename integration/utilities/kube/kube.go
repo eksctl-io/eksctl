77ea2f28d47a7b93a9821681a6c7c33444be48ff
Tibi <110664232+TiberiuGC@users.noreply.github.com>
2023-09-12 18:30:33 +0300

    Allow managed suite specs to run in parallel

--- integration/utilities/kube/kube.go
+++ integration/utilities/kube/kube.go
@@ -1,15 +1,14 @@
-//go:build integration
-// +build integration
-
 package kube
 
 import (
+	"fmt"
+
 	harness "github.com/dlespiau/kube-test-harness"
 	"github.com/dlespiau/kube-test-harness/logger"
 	"github.com/onsi/ginkgo/v2"
 )
 
-type tHelper struct{ ginkgo.GinkgoTInterface }
+type tHelper struct{ ginkgo.FullGinkgoTInterface }
 
 func (t *tHelper) Helper()      {}
 func (t *tHelper) Name() string { return "eksctl-test" }
@@ -26,6 +25,7 @@ func NewTest(kubeconfigPath string) (*harness.Test, error) {
 		return nil, err
 	}
 	test := h.NewTest(t)
+	test.Namespace += fmt.Sprintf("-%d", t.ParallelProcess())
 	test.Setup()
 	return test, nil
 }
