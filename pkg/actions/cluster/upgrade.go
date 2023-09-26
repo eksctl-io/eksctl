aa3898bf62b6139950b4cd5a698315c8ca7d8a41
cpu1 <chetan@weave.works>
2023-09-26 13:18:45 +0530

    Add support for EKS 1.28

--- pkg/actions/cluster/upgrade.go
+++ pkg/actions/cluster/upgrade.go
@@ -126,6 +126,8 @@ func getNextVersion(currentVersion string) (string, error) {
 		return api.Version1_27, nil
 	case api.Version1_27:
 		return api.Version1_28, nil
+	case api.Version1_28:
+		return api.Version1_29, nil
 	default:
 		// version of control plane is not known to us, maybe we are just too old...
 		return "", fmt.Errorf("control plane version %q is not known to this version of eksctl, try to upgrade eksctl first", currentVersion)
