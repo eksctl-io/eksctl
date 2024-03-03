3b82eca0be77ad26545538f78d5be8e39293b8d7
Pankaj Walke <advaitt@amazon.com>
2024-03-03 01:07:06 +0000

    Added UpdateAuthentication mode and aeEntries filter logic

--- pkg/actions/accessentry/migrator.go
+++ pkg/actions/accessentry/migrator.go
@@ -2,18 +2,275 @@ package accessentry
 
 import (
 	"context"
+	"fmt"
+	"strings"
 	"time"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	awseks "github.com/aws/aws-sdk-go-v2/service/eks"
+	ekstypes "github.com/aws/aws-sdk-go-v2/service/eks/types"
+	"github.com/kris-nova/logger"
+
 	api "github.com/weaveworks/eksctl/pkg/apis/eksctl.io/v1alpha5"
+	"github.com/weaveworks/eksctl/pkg/authconfigmap"
+	"github.com/weaveworks/eksctl/pkg/awsapi"
+	"github.com/weaveworks/eksctl/pkg/eks/waiter"
+	"github.com/weaveworks/eksctl/pkg/iam"
+	"github.com/weaveworks/eksctl/pkg/kubernetes"
 )
 
-type AddonCreator interface {
-	Create(ctx context.Context, addon *api.Addon, waitTimeout time.Duration) error
-}
+// Venkat TODO
+// Compare accessEntries and accessEntriesFromCm
+// Next steps:
+// 1. Instead of storing all into toDoEntries, Make a struct to contain multiple types of toDoEntries
+// type ToDoEntries struct {
+// 	NodeLinux   []api.AccessEntry
+// 	NodeWindows []api.AccessEntry
+// 	System      []api.AccessEntry
+// 	NonSystem   []api.AccessEntry
+// }
+// 2. Modify below code to store entries into one of above slices based on accessEntriesFromCm[item].KubernetesGroups
+// 3. Before storing, add Type property to each entry i.e. STANDARD, EC2_LINUX, EC2_WINDOWS etc based on condition
+
+// Pankaj TODO
+// Add loggic to remove aws-auth
 
 type AccessEntryMigrationOptions struct {
 	RemoveOIDCProviderTrustRelationship bool
 	TargetAuthMode                      string
 	Approve                             bool
 	Timeout                             time.Duration
+}
+
+type Migrator struct {
+	clusterName string
+	eksAPI      awsapi.EKS
+	iamAPI      awsapi.IAM
+	clientSet   kubernetes.Interface
+	aeCreator   Creator
+	curAuthMode ekstypes.AuthenticationMode
+	tgAuthMode  ekstypes.AuthenticationMode
+}
+
+func NewMigrator(
+	clusterName string,
+
+	eksAPI awsapi.EKS,
+	iamAPI awsapi.IAM,
+	clientSet kubernetes.Interface,
+	aeCreator Creator,
+	curAuthMode ekstypes.AuthenticationMode,
+	tgAuthMode ekstypes.AuthenticationMode,
+) *Migrator {
+	return &Migrator{
+		clusterName: clusterName,
+		eksAPI:      eksAPI,
+		iamAPI:      iamAPI,
+		clientSet:   clientSet,
+		aeCreator:   aeCreator,
+		curAuthMode: curAuthMode,
+		tgAuthMode:  tgAuthMode,
+	}
+}
+
+func (m *Migrator) MigrateToAccessEntry(ctx context.Context, options AccessEntryMigrationOptions) error {
+	if m.curAuthMode != m.tgAuthMode {
+		logger.Info("target authentication mode %v is different than the current authentication mode %v, Updating the Cluster authentication mode", m.tgAuthMode, m.curAuthMode)
+		if m.curAuthMode != ekstypes.AuthenticationModeApiAndConfigMap {
+			err := m.doUpdateAuhenticationMode(ctx, ekstypes.AuthenticationModeApiAndConfigMap)
+			if err != nil {
+				return err
+			}
+		}
+		m.curAuthMode = ekstypes.AuthenticationModeApiAndConfigMap
+	}
+
+	cmEntries, err := m.doGetIAMIdentityMappings()
+	if err != nil {
+		return err
+	}
+
+	curAccessEntries, err := m.doGetAccessEntries(ctx)
+	if err != nil {
+		return err
+	}
+
+	newAccessEntries, skipAPImode := m.doFilterAccessEntries(cmEntries, curAccessEntries)
+
+	newaelen := len(newAccessEntries)
+
+	logger.Info("%d new access entries will be created", newaelen)
+
+	if len(newAccessEntries) != 0 {
+		err = m.aeCreator.Create(ctx, newAccessEntries)
+		if err != nil {
+			return err
+		}
+	}
+
+	if !skipAPImode {
+		if m.curAuthMode != m.tgAuthMode {
+			return m.doUpdateAuhenticationMode(ctx, m.tgAuthMode)
+		}
+	}
+
+	return nil
+
+}
+
+func (m *Migrator) doUpdateAuhenticationMode(ctx context.Context, authMode ekstypes.AuthenticationMode) error {
+	output, err := m.eksAPI.UpdateClusterConfig(ctx, &awseks.UpdateClusterConfigInput{
+		Name: aws.String(m.clusterName),
+		AccessConfig: &ekstypes.UpdateAccessConfigRequest{
+			AuthenticationMode: authMode,
+		},
+	})
+	if err != nil {
+		return fmt.Errorf("failed to update cluster config: %v", err)
+	}
+
+	updateWaiter := waiter.NewUpdateWaiter(m.eksAPI, func(options *waiter.UpdateWaiterOptions) {
+		options.RetryAttemptLogMessage = fmt.Sprintf("waiting for update %q in cluster %q to complete", *output.Update.Id, m.clusterName)
+	})
+	err = updateWaiter.Wait(ctx, &awseks.DescribeUpdateInput{
+		Name:     &m.clusterName,
+		UpdateId: output.Update.Id,
+	}, api.DefaultWaitTimeout)
+
+	switch e := err.(type) {
+	case *waiter.UpdateFailedError:
+		if e.Status == string(ekstypes.UpdateStatusCancelled) {
+			return fmt.Errorf("request to update cluster authentication mode was cancelled: %s", e.UpdateError)
+		}
+		return fmt.Errorf("failed to update cluster authentication mode: %s", e.UpdateError)
+
+	case nil:
+		logger.Info("authentication mode was successfully updated to %s on cluster %s", authMode, m.clusterName)
+		m.curAuthMode = authMode
+		return nil
+
+	default:
+		return err
+	}
+}
+
+func (m *Migrator) doGetAccessEntries(ctx context.Context) ([]Summary, error) {
+
+	aegetter := NewGetter(m.clusterName, m.eksAPI)
+	accessEntries, err := aegetter.Get(ctx, api.ARN{})
+	if err != nil {
+		return nil, err
+	}
+
+	return accessEntries, nil
+}
+
+func (m *Migrator) doGetIAMIdentityMappings() ([]iam.Identity, error) {
+
+	acm, err := authconfigmap.NewFromClientSet(m.clientSet)
+	if err != nil {
+		return nil, err
+	}
+
+	cmEntries, err := acm.GetIdentities()
+	if err != nil {
+		return nil, err
+	}
+
+	return cmEntries, nil
+}
+
+func (m *Migrator) doFilterAccessEntries(cmEntries []iam.Identity, accessEntries []Summary) ([]api.AccessEntry, bool) {
+
+	skipAPImode := false
+	toDoEntries := []api.AccessEntry{}
+	uniqueCmEntries := map[string]bool{}
+
+	aeArns := make(map[string]bool)
+
+	// Create ARN Map for current access entries
+	for _, ae := range accessEntries {
+		aeArns[ae.PrincipalARN] = true
+	}
+
+	for _, cme := range cmEntries {
+		if !uniqueCmEntries[cme.ARN()] { // Check if cmEntry is not duplicate
+			if !aeArns[cme.ARN()] { // Check if the ARN is not in existing access entries
+				switch cme.Type() {
+				case iam.ResourceTypeRole:
+					if aeEntry := doBuildNodeRoleAccessEntry(cme); aeEntry != nil {
+						toDoEntries = append(toDoEntries, *aeEntry)
+					} else if aeEntry := doBuildAccessEntry(cme); aeEntry != nil {
+						toDoEntries = append(toDoEntries, *aeEntry)
+					}
+				case iam.ResourceTypeUser:
+					if aeEntry := doBuildAccessEntry(cme); aeEntry != nil {
+						toDoEntries = append(toDoEntries, *aeEntry)
+					}
+				case iam.ResourceTypeAccount:
+					logger.Warning("found account mapping %s, can not create access entry for account mapping, skipping", cme.Account())
+					skipAPImode = true
+				}
+			} else {
+				logger.Warning("%s already exists in access entry, skipping", cme.ARN())
+			}
+		}
+	}
+
+	return toDoEntries, skipAPImode
+}
+
+func doBuildNodeRoleAccessEntry(cme iam.Identity) *api.AccessEntry {
+
+	groupsStr := strings.Join(cme.Groups(), ",")
+
+	if strings.Contains(groupsStr, "system:nodes") && !strings.Contains(groupsStr, "eks:kube-proxy-windows") { // For Windows Nodes
+		return &api.AccessEntry{
+			PrincipalARN: api.MustParseARN(cme.ARN()),
+			Type:         "EC2_LINUX",
+		}
+	}
+
+	if strings.Contains(groupsStr, "system:nodes") && strings.Contains(groupsStr, "eks:kube-proxy-windows") { // For Linux Nodes
+		return &api.AccessEntry{
+			PrincipalARN: api.MustParseARN(cme.ARN()),
+			Type:         "EC2_WINDOWS",
+		}
+	}
+
+	return nil
+}
+
+func doBuildAccessEntry(cme iam.Identity) *api.AccessEntry {
+
+	groupsStr := strings.Join(cme.Groups(), ",")
+
+	if strings.Contains(groupsStr, "system:masters") { // Admin Role
+		return &api.AccessEntry{
+			PrincipalARN: api.MustParseARN(cme.ARN()),
+			Type:         "STANDARD",
+			AccessPolicies: []api.AccessPolicy{
+				api.AccessPolicy{
+					PolicyARN: api.MustParseARN("arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"),
+					AccessScope: api.AccessScope{
+						Type: ekstypes.AccessScopeTypeCluster,
+					},
+				},
+			},
+			KubernetesUsername: cme.Username(),
+		}
+	}
+
+	if strings.Contains(groupsStr, "system") { // Admin Role
+		logger.Warning("at least one group name associated with %s starts with \"system\", can not create access entry with such group name, skipping", cme.ARN())
+		return nil
+	}
+
+	return &api.AccessEntry{
+		PrincipalARN:       api.MustParseARN(cme.ARN()),
+		Type:               "STANDARD",
+		KubernetesGroups:   cme.Groups(),
+		KubernetesUsername: cme.Username(),
+	}
+
 }
--- pkg/ctl/utils/migrate_to_access_entry.go
+++ pkg/ctl/utils/migrate_to_access_entry.go
@@ -5,12 +5,10 @@ import (
 	"fmt"
 
 	ekstypes "github.com/aws/aws-sdk-go-v2/service/eks/types"
-	"github.com/kris-nova/logger"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 	accessentryactions "github.com/weaveworks/eksctl/pkg/actions/accessentry"
 	api "github.com/weaveworks/eksctl/pkg/apis/eksctl.io/v1alpha5"
-	"github.com/weaveworks/eksctl/pkg/authconfigmap"
 	"github.com/weaveworks/eksctl/pkg/ctl/cmdutils"
 )
 
@@ -22,12 +20,9 @@ func migrateAccessEntryCmd(cmd *cmdutils.Cmd) {
 
 	var options accessentryactions.AccessEntryMigrationOptions
 	cmd.FlagSetGroup.InFlagSet("Migrate to Access Entry", func(fs *pflag.FlagSet) {
+		fs.StringVar(&options.TargetAuthMode, "target-authentication-mode", "API_AND_CONFIG_MAP", "Target Authentication mode of migration")
 		fs.BoolVar(&options.RemoveOIDCProviderTrustRelationship, "remove-aws-auth", false, "Remove aws-auth from cluster")
-		fs.StringVar(&options.TargetAuthMode, "authentication-mode", "API_AND_CONFIG_MAP", "Target Authentication mode of migration")
 		fs.BoolVar(&options.Approve, "approve", false, "Apply the changes")
-
-		// fs.BoolVar(&options.SkipAgentInstallation, "skip-agent-installation", false, "Skip installing pod-identity-agent addon")
-		// cmdutils.AddIAMServiceAccountFilterFlags(fs, &cmd.Include, &cmd.Exclude)
 	})
 
 	cmd.FlagSetGroup.InFlagSet("General", func(fs *pflag.FlagSet) {
@@ -67,72 +62,24 @@ func doMigrateToAccessEntry(cmd *cmdutils.Cmd, options accessentryactions.Access
 
 	curAuthMode := ctl.GetClusterState().AccessConfig.AuthenticationMode
 
-	if curAuthMode != tgAuthMode {
-		logger.Info("target authentication mode %v is different than the current authentication mode %v, Updating the Cluster authentication mode", tgAuthMode, curAuthMode)
-		// Add UpdateAuthentication Mode Method call here
-	}
-
 	clientSet, err := ctl.NewStdClientSet(cfg)
 	if err != nil {
 		return err
 	}
-	acm, err := authconfigmap.NewFromClientSet(clientSet)
-	if err != nil {
-		return err
-	}
-	cmEntries, err := acm.GetIdentities()
-	if err != nil {
-		return err
-	}
-	accessEntriesFromCm := []api.AccessEntry{}
-	for _, cme := range cmEntries {
-		idArn := api.MustParseARN(cme.ARN())
-		accessEntriesFromCm = append(accessEntriesFromCm, api.AccessEntry{
-			PrincipalARN: idArn,
-			// Type: "STANDARD",
-			KubernetesGroups:   cme.Groups(),
-			KubernetesUsername: cme.Username(),
-		})
-	}
 
-	asgetter := accessentryactions.NewGetter(cfg.Metadata.Name, ctl.AWSProvider.EKS())
-	accessEntries, err := asgetter.Get(ctx, api.ARN{})
-	if err != nil {
-		return err
-	}
-
-	//// Compare accessEntries and accessEntriesFromCm
-	// Next steps:
-	// 1. Instead of storing all into toDoEntries, Make a struct to contain multiple types of toDoEntries
-	// type ToDoEntries struct {
-	// 	NodeLinux   []api.AccessEntry
-	// 	NodeWindows []api.AccessEntry
-	// 	System      []api.AccessEntry
-	// 	NonSystem   []api.AccessEntry
-	// }
-	// 2. Modify below code to store entries into one of above slices based on accessEntriesFromCm[item].KubernetesGroups
-	// 3. Before storing, add Type property to each entry i.e. STANDARD, EC2_LINUX, EC2_WINDOWS etc based on condition
-	toDoEntries := []api.AccessEntry{}
-	aeArns := make(map[string]bool)
-	for _, ae := range accessEntries {
-		aeArns[ae.PrincipalARN] = true
-	}
-	for _, cme := range accessEntriesFromCm {
-		if !aeArns[cme.PrincipalARN.String()] {
-			toDoEntries = append(toDoEntries, cme)
-		} else {
-			logger.Warning("%v already exists in Access Entry, ignoring.", cme.PrincipalARN.String())
-		}
-	}
-
-	// Create Access Entries
 	stackManager := ctl.NewStackManager(cfg)
-	fmt.Printf("%+v\n%+v\n", accessEntries, accessEntriesFromCm)
-	accessEntryCreator := &accessentryactions.Creator{
+	aeCreator := accessentryactions.Creator{
 		ClusterName:  cmd.ClusterConfig.Metadata.Name,
 		StackCreator: stackManager,
 	}
-	accessEntryCreator.Create(ctx, toDoEntries)
 
-	return nil
+	return accessentryactions.NewMigrator(
+		cfg.Metadata.Name,
+		ctl.AWSProvider.EKS(),
+		ctl.AWSProvider.IAM(),
+		clientSet,
+		aeCreator,
+		curAuthMode,
+		tgAuthMode,
+	).MigrateToAccessEntry(ctx, options)
 }
