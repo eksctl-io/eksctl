78e671104ddce7433c4fcc1cc4ba60c788079a60
Tibi <110664232+TiberiuGC@users.noreply.github.com>
2024-03-22 20:42:52 +0200

    Aim for namespace uniqueness across parallel specs (#7680)
    ensure namespace uniqueness across parallel specs

--- integration/utilities/kube/kube.go
+++ integration/utilities/kube/kube.go
@@ -4,17 +4,16 @@
 package kube
 
 import (
-	"fmt"
-
 	harness "github.com/dlespiau/kube-test-harness"
 	"github.com/dlespiau/kube-test-harness/logger"
+	"github.com/google/uuid"
 	"github.com/onsi/ginkgo/v2"
 )
 
 type tHelper struct{ ginkgo.FullGinkgoTInterface }
 
 func (t *tHelper) Helper()      {}
-func (t *tHelper) Name() string { return "eksctl-test" }
+func (t *tHelper) Name() string { return "eksctl" }
 
 // NewTest creates a new test harness to more easily run integration tests against the provided Kubernetes cluster.
 func NewTest(kubeconfigPath string) (*harness.Test, error) {
@@ -28,7 +27,9 @@ func NewTest(kubeconfigPath string) (*harness.Test, error) {
 		return nil, err
 	}
 	test := h.NewTest(t)
-	test.Namespace += fmt.Sprintf("-%d", t.ParallelProcess())
+	// several parallel test specs may initialize a new harness and close it after completion,
+	// thus, we aim to minimize the chance of conflicting actions against same K8s namespace
+	test.Namespace += uuid.NewString()
 	test.Setup()
 	return test, nil
 }
