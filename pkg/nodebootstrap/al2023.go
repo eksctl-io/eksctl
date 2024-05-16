ec1ff7b57f781a8b829a52e9366cc546f93954fa
cPu1 <patwal.chetan@gmail.com>
2024-05-16 19:35:42 +0530

    Fix kubeletExtraConfig support for AL2023

--- pkg/nodebootstrap/al2023.go
+++ pkg/nodebootstrap/al2023.go
@@ -3,6 +3,14 @@ package nodebootstrap
 import (
 	"bytes"
 	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"strconv"
+
+	nodeadmapi "github.com/awslabs/amazon-eks-ami/nodeadm/api"
+	nodeadm "github.com/awslabs/amazon-eks-ami/nodeadm/api/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 
 	api "github.com/weaveworks/eksctl/pkg/apis/eksctl.io/v1alpha5"
 	"github.com/weaveworks/eksctl/pkg/nodebootstrap/assets"
@@ -11,13 +19,11 @@ import (
 
 type AL2023 struct {
 	cfg        *api.ClusterConfig
-	ng         *api.NodeGroupBase
-	taints     []api.NodeGroupTaint
+	nodePool   api.NodePool
 	clusterDNS string
 
-	scripts    []string
-	cloudboot  []string
-	nodeConfig *NodeConfig
+	scripts   []string
+	cloudboot []string
 
 	UserDataMimeBoundary string
 }
@@ -27,9 +33,6 @@ func NewManagedAL2023Bootstrapper(cfg *api.ClusterConfig, mng *api.ManagedNodeGr
 	if api.IsEnabled(mng.EFAEnabled) {
 		al2023.cloudboot = append(al2023.cloudboot, assets.EfaManagedAL2023Boothook)
 	}
-	if api.IsAMI(mng.AMI) {
-		al2023.setNodeConfig()
-	}
 	return al2023
 }
 
@@ -38,84 +41,91 @@ func NewAL2023Bootstrapper(cfg *api.ClusterConfig, ng *api.NodeGroup, clusterDNS
 	if api.IsEnabled(ng.EFAEnabled) {
 		al2023.scripts = append(al2023.scripts, assets.EfaAl2023Sh)
 	}
-	al2023.setNodeConfig()
 	return al2023
 }
 
 func newAL2023Bootstrapper(cfg *api.ClusterConfig, np api.NodePool, clusterDNS string) *AL2023 {
 	return &AL2023{
 		cfg:        cfg,
-		ng:         np.BaseNodeGroup(),
-		taints:     np.NGTaints(),
+		nodePool:   np,
 		clusterDNS: clusterDNS,
 	}
 }
 
 func (m *AL2023) UserData() (string, error) {
-	var buf bytes.Buffer
-
-	if len(m.scripts) == 0 && len(m.cloudboot) == 0 && m.nodeConfig == nil {
+	nodeConfig, err := m.createNodeConfig()
+	if err != nil {
+		return "", fmt.Errorf("generating node config: %w", err)
+	}
+	if len(m.scripts) == 0 && len(m.cloudboot) == 0 && nodeConfig == nil {
 		return "", nil
 	}
 
-	if err := createMimeMessage(&buf, m.scripts, m.cloudboot, m.nodeConfig, m.UserDataMimeBoundary); err != nil {
+	var buf bytes.Buffer
+	if err := createMimeMessage(&buf, m.scripts, m.cloudboot, nodeConfig, m.UserDataMimeBoundary); err != nil {
 		return "", err
 	}
-
 	return base64.StdEncoding.EncodeToString(buf.Bytes()), nil
 }
 
-func (m *AL2023) setNodeConfig() {
-	m.nodeConfig = &NodeConfig{
-		APIVersion: "node.eks.aws/v1alpha1",
-		Kind:       "NodeConfig",
-		Spec: NodeSpec{
-			Cluster: ClusterSpec{
-				Name:                 m.cfg.Metadata.Name,
-				APIServerEndpoint:    m.cfg.Status.Endpoint,
-				CertificateAuthority: base64.StdEncoding.EncodeToString(m.cfg.Status.CertificateAuthorityData),
-				CIDR:                 m.cfg.Status.KubernetesNetworkConfig.ServiceIPv4CIDR,
-			},
-			Kubelet: KubeletSpec{
-				Config: KubeletConfig{
-					ClusterDNS: []string{m.clusterDNS},
-				},
-				Flags: []string{"--node-labels=" + formatLabels(m.ng.Labels)},
-			},
+func (m *AL2023) createNodeConfig() (*nodeadm.NodeConfig, error) {
+	kubeletConfig := api.InlineDocument{}
+	switch nodeGroup := m.nodePool.(type) {
+	case *api.ManagedNodeGroup:
+		if !api.IsAMI(nodeGroup.AMI) {
+			return nil, nil
+		}
+	case *api.NodeGroup:
+		if nodeGroup.KubeletExtraConfig != nil {
+			kubeletConfig = *nodeGroup.KubeletExtraConfig.DeepCopy()
+		}
+	}
+
+	kubeletConfig["clusterDNS"] = []string{m.clusterDNS}
+	ng := m.nodePool.BaseNodeGroup()
+	if ng.MaxPodsPerNode > 0 {
+		kubeletConfig["maxPods"] = strconv.Itoa(ng.MaxPodsPerNode)
+	}
+	nodeKubeletConfig, err := ToKubeletConfig(kubeletConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	kubeletOptions := nodeadm.KubeletOptions{
+		Flags:  []string{"--node-labels=" + formatLabels(ng.Labels)},
+		Config: nodeKubeletConfig,
+	}
+	if taints := m.nodePool.NGTaints(); len(taints) > 0 {
+		kubeletOptions.Flags = append(kubeletOptions.Flags, "--register-with-taints="+utils.FormatTaints(taints))
+	}
+
+	clusterStatus := m.cfg.Status
+	return &nodeadm.NodeConfig{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       nodeadmapi.KindNodeConfig,
+			APIVersion: nodeadm.GroupVersion.String(),
 		},
+		Spec: nodeadm.NodeConfigSpec{
+			Cluster: nodeadm.ClusterDetails{
+				Name:                 m.cfg.Metadata.Name,
+				APIServerEndpoint:    clusterStatus.Endpoint,
+				CertificateAuthority: clusterStatus.CertificateAuthorityData,
+				CIDR:                 clusterStatus.KubernetesNetworkConfig.ServiceIPv4CIDR,
+			},
+			Kubelet: kubeletOptions,
+		},
+	}, nil
+}
+
+// ToKubeletConfig generates a kubelet config that can be used with nodeadm.NodeConfig.
+func ToKubeletConfig(kubeletExtraConfig api.InlineDocument) (map[string]runtime.RawExtension, error) {
+	kubeletConfig := map[string]runtime.RawExtension{}
+	for k, v := range kubeletExtraConfig {
+		raw, err := json.Marshal(v)
+		if err != nil {
+			return nil, err
+		}
+		kubeletConfig[k] = runtime.RawExtension{Raw: raw}
 	}
-	if m.ng.MaxPodsPerNode > 0 {
-		m.nodeConfig.Spec.Kubelet.Config.MaxPods = &m.ng.MaxPodsPerNode
-	}
-	if len(m.taints) > 0 {
-		m.nodeConfig.Spec.Kubelet.Flags = append(m.nodeConfig.Spec.Kubelet.Flags, "--register-with-taints="+utils.FormatTaints(m.taints))
-	}
-}
-
-type NodeConfig struct {
-	APIVersion string   `yaml:"apiVersion"`
-	Kind       string   `yaml:"kind"`
-	Spec       NodeSpec `yaml:"spec"`
-}
-
-type NodeSpec struct {
-	Cluster ClusterSpec `yaml:"cluster"`
-	Kubelet KubeletSpec `yaml:"kubelet"`
-}
-
-type ClusterSpec struct {
-	APIServerEndpoint    string `yaml:"apiServerEndpoint"`
-	CertificateAuthority string `yaml:"certificateAuthority"`
-	CIDR                 string `yaml:"cidr"`
-	Name                 string `yaml:"name"`
-}
-
-type KubeletSpec struct {
-	Config KubeletConfig `yaml:"config"`
-	Flags  []string      `yaml:"flags"`
-}
-
-type KubeletConfig struct {
-	MaxPods    *int     `yaml:"maxPods,omitempty"`
-	ClusterDNS []string `yaml:"clusterDNS"`
+	return kubeletConfig, nil
 }
