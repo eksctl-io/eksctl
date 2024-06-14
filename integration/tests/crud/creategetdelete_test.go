4b62d1a3fab80e4c1dfe761e4b7b786b4c976290
Tibi <110664232+TiberiuGC@users.noreply.github.com>
2024-06-14 14:23:00 +0300

    Schedule pods on a nodegroup on which no concurrent actions are executed (#7834)
    * Schedule pods on a nodegroup on which no concurrent actions are executed
    
    * patch test assertions

--- integration/tests/crud/creategetdelete_test.go
+++ integration/tests/crud/creategetdelete_test.go
@@ -66,6 +66,7 @@ func TestCRUD(t *testing.T) {
 }
 
 const (
+	deployNg        = "ng-deploy"
 	deleteNg        = "ng-delete"
 	taintsNg1       = "ng-taints-1"
 	taintsNg2       = "ng-taints-2"
@@ -131,6 +132,17 @@ var _ = SynchronizedBeforeSuite(func() []byte {
 		},
 	}
 	cfg.ManagedNodeGroups = []*api.ManagedNodeGroup{
+		{
+			NodeGroupBase: &api.NodeGroupBase{
+				Name: deployNg,
+				ScalingConfig: &api.ScalingConfig{
+					DesiredCapacity: aws.Int(5),
+				},
+				Labels: map[string]string{
+					"used-for": "test-pods",
+				},
+			},
+		},
 		{
 			NodeGroupBase: &api.NodeGroupBase{
 				Name: drainMng,
@@ -214,7 +226,7 @@ var _ = Describe("(Integration) Create, Get, Scale & Delete", func() {
 			Expect(session.ExitCode()).To(BeZero())
 			var stacks []*cfntypes.Stack
 			Expect(yaml.Unmarshal(session.Out.Contents(), &stacks)).To(Succeed())
-			Expect(stacks).To(HaveLen(6))
+			Expect(stacks).To(HaveLen(7))
 
 			var (
 				names, descriptions []string
@@ -227,6 +239,7 @@ var _ = Describe("(Integration) Create, Get, Scale & Delete", func() {
 
 			Expect(names).To(ContainElements(
 				ContainSubstring(params.ClusterName+"-cluster"),
+				ContainSubstring(ngPrefix+deployNg),
 				ContainSubstring(ngPrefix+deleteNg),
 				ContainSubstring(ngPrefix+scaleSingleNg),
 				ContainSubstring(ngPrefix+scaleMultipleNg),
@@ -1196,6 +1209,7 @@ var _ = Describe("(Integration) Create, Get, Scale & Delete", func() {
 				"-o", "json",
 				"--cluster", params.ClusterName,
 			)).To(RunSuccessfullyWithOutputString(BeNodeGroupsWithNamesWhich(
+				ContainElement(deployNg),
 				ContainElement(taintsNg1),
 				ContainElement(taintsNg2),
 				ContainElement(scaleSingleNg),
