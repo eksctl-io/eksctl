4960fee1507babec212ac74254a1d94f22af46b3
Gustavo Diaz <38817851+gustavodiaz7722@users.noreply.github.com>
2025-01-15 10:09:19 -0800

    Update pkg/actions/addon/tasks.go
    Co-authored-by: Amine <hilalyamine@gmail.com>

--- pkg/actions/addon/tasks.go
+++ pkg/actions/addon/tasks.go
@@ -29,10 +29,7 @@ func CreateAddonTasks(ctx context.Context, cfg *api.ClusterConfig, clusterProvid
 			if addonInfo.IsDefault && !slices.ContainsFunc(cfg.Addons, func(a *api.Addon) bool {
 				return strings.EqualFold(a.Name, addonName)
 			}) {
-				if !cfg.IsAutoModeEnabled() {
-					addons = append(addons, &api.Addon{Name: addonName})
-					autoDefaultAddonNames = append(autoDefaultAddonNames, addonName)
-				} else if addonInfo.IsDefaultAutoMode {
+				if !cfg.IsAutoModeEnabled() || addonInfo.IsDefaultAutoMode {
 					addons = append(addons, &api.Addon{Name: addonName})
 					autoDefaultAddonNames = append(autoDefaultAddonNames, addonName)
 				}
