8be8c93b77cddd4e606a31f5205e18904ca45f2f
Bryant Biggs <bryantbiggs@gmail.com>
2025-01-21 10:38:14 -0600

    Migrate `goformation` under `pkg/` as a local package and remove location re-write

---
+++ pkg/goformation/cloudformation/eks/aws-eks-nodegroup_noderepairconfig.go
@@ -0,0 +1,32 @@
+package eks
+
+import (
+	"github.com/weaveworks/eksctl/pkg/goformation/cloudformation/types"
+
+	"github.com/weaveworks/eksctl/pkg/goformation/cloudformation/policies"
+)
+
+// Nodegroup_NodeRepairConfig AWS CloudFormation Resource (AWS::EKS::Nodegroup.NodeRepairConfig)
+type Nodegroup_NodeRepairConfig struct {
+	Enabled *types.Value `json:"Enabled,omitempty"`
+
+	// AWSCloudFormationDeletionPolicy represents a CloudFormation DeletionPolicy
+	AWSCloudFormationDeletionPolicy policies.DeletionPolicy `json:"-"`
+
+	// AWSCloudFormationUpdateReplacePolicy represents a CloudFormation UpdateReplacePolicy
+	AWSCloudFormationUpdateReplacePolicy policies.UpdateReplacePolicy `json:"-"`
+
+	// AWSCloudFormationDependsOn stores the logical ID of the resources to be created before this resource
+	AWSCloudFormationDependsOn []string `json:"-"`
+
+	// AWSCloudFormationMetadata stores structured data associated with this resource
+	AWSCloudFormationMetadata map[string]interface{} `json:"-"`
+
+	// AWSCloudFormationCondition stores the logical ID of the condition that must be satisfied for this resource to be created
+	AWSCloudFormationCondition string `json:"-"`
+}
+
+// AWSCloudFormationType returns the AWS CloudFormation resource type
+func (r *Nodegroup_NodeRepairConfig) AWSCloudFormationType() string {
+	return "AWS::EKS::Nodegroup.NodeRepairConfig"
+}
