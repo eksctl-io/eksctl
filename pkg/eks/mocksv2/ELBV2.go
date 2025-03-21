e553c48b60bdd06c6b987f52063acf4f59f06210
Adam <adacrowd@amazon.com>
2025-03-21 12:48:28 -0700

    Update aws-sdk-go-v2 dependencies (#8308)

--- pkg/eks/mocksv2/ELBV2.go
+++ pkg/eks/mocksv2/ELBV2.go
@@ -2612,6 +2612,80 @@ func (_c *ELBV2_ModifyCapacityReservation_Call) RunAndReturn(run func(context.Co
 	return _c
 }
 
+// ModifyIpPools provides a mock function with given fields: ctx, params, optFns
+func (_m *ELBV2) ModifyIpPools(ctx context.Context, params *elasticloadbalancingv2.ModifyIpPoolsInput, optFns ...func(*elasticloadbalancingv2.Options)) (*elasticloadbalancingv2.ModifyIpPoolsOutput, error) {
+	_va := make([]interface{}, len(optFns))
+	for _i := range optFns {
+		_va[_i] = optFns[_i]
+	}
+	var _ca []interface{}
+	_ca = append(_ca, ctx, params)
+	_ca = append(_ca, _va...)
+	ret := _m.Called(_ca...)
+
+	if len(ret) == 0 {
+		panic("no return value specified for ModifyIpPools")
+	}
+
+	var r0 *elasticloadbalancingv2.ModifyIpPoolsOutput
+	var r1 error
+	if rf, ok := ret.Get(0).(func(context.Context, *elasticloadbalancingv2.ModifyIpPoolsInput, ...func(*elasticloadbalancingv2.Options)) (*elasticloadbalancingv2.ModifyIpPoolsOutput, error)); ok {
+		return rf(ctx, params, optFns...)
+	}
+	if rf, ok := ret.Get(0).(func(context.Context, *elasticloadbalancingv2.ModifyIpPoolsInput, ...func(*elasticloadbalancingv2.Options)) *elasticloadbalancingv2.ModifyIpPoolsOutput); ok {
+		r0 = rf(ctx, params, optFns...)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*elasticloadbalancingv2.ModifyIpPoolsOutput)
+		}
+	}
+
+	if rf, ok := ret.Get(1).(func(context.Context, *elasticloadbalancingv2.ModifyIpPoolsInput, ...func(*elasticloadbalancingv2.Options)) error); ok {
+		r1 = rf(ctx, params, optFns...)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// ELBV2_ModifyIpPools_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'ModifyIpPools'
+type ELBV2_ModifyIpPools_Call struct {
+	*mock.Call
+}
+
+// ModifyIpPools is a helper method to define mock.On call
+//   - ctx context.Context
+//   - params *elasticloadbalancingv2.ModifyIpPoolsInput
+//   - optFns ...func(*elasticloadbalancingv2.Options)
+func (_e *ELBV2_Expecter) ModifyIpPools(ctx interface{}, params interface{}, optFns ...interface{}) *ELBV2_ModifyIpPools_Call {
+	return &ELBV2_ModifyIpPools_Call{Call: _e.mock.On("ModifyIpPools",
+		append([]interface{}{ctx, params}, optFns...)...)}
+}
+
+func (_c *ELBV2_ModifyIpPools_Call) Run(run func(ctx context.Context, params *elasticloadbalancingv2.ModifyIpPoolsInput, optFns ...func(*elasticloadbalancingv2.Options))) *ELBV2_ModifyIpPools_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		variadicArgs := make([]func(*elasticloadbalancingv2.Options), len(args)-2)
+		for i, a := range args[2:] {
+			if a != nil {
+				variadicArgs[i] = a.(func(*elasticloadbalancingv2.Options))
+			}
+		}
+		run(args[0].(context.Context), args[1].(*elasticloadbalancingv2.ModifyIpPoolsInput), variadicArgs...)
+	})
+	return _c
+}
+
+func (_c *ELBV2_ModifyIpPools_Call) Return(_a0 *elasticloadbalancingv2.ModifyIpPoolsOutput, _a1 error) *ELBV2_ModifyIpPools_Call {
+	_c.Call.Return(_a0, _a1)
+	return _c
+}
+
+func (_c *ELBV2_ModifyIpPools_Call) RunAndReturn(run func(context.Context, *elasticloadbalancingv2.ModifyIpPoolsInput, ...func(*elasticloadbalancingv2.Options)) (*elasticloadbalancingv2.ModifyIpPoolsOutput, error)) *ELBV2_ModifyIpPools_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
 // ModifyListener provides a mock function with given fields: ctx, params, optFns
 func (_m *ELBV2) ModifyListener(ctx context.Context, params *elasticloadbalancingv2.ModifyListenerInput, optFns ...func(*elasticloadbalancingv2.Options)) (*elasticloadbalancingv2.ModifyListenerOutput, error) {
 	_va := make([]interface{}, len(optFns))
