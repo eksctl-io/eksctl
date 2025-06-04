15628213366a83a8f5ad4555275865713c7287ee
Nathan Clonts <nclonts@amazon.com>
2025-06-04 06:20:13 +0000

    Add eksctl support for new AWS region TPE

--- pkg/apis/eksctl.io/v1alpha5/types.go
+++ pkg/apis/eksctl.io/v1alpha5/types.go
@@ -138,6 +138,9 @@ const (
 	// RegionAPEast1 represents the Asia Pacific Region Hong Kong
 	RegionAPEast1 = "ap-east-1"
 
+	// RegionAPEast2 represents the Asia Pacific Region Taipei
+	RegionAPEast2 = "ap-east-2"
+
 	// RegionMECentral1 represents the Middle East Region Dubai
 	RegionMECentral1 = "me-central-1"
 
@@ -312,6 +315,9 @@ const (
 
 	// eksResourceAccountAPEast1 defines the AWS EKS account ID that provides node resources in ap-east-1 region
 	eksResourceAccountAPEast1 = "800184023465"
+
+	// eksResourceAccountAPEast2 defines the AWS EKS account ID that provides node resources in ap-east-2 region
+	eksResourceAccountAPEast2 = "533267051163"
 
 	// eksResourceAccountCAWest1 defines the AWS EKS account ID that provides node resources in ca-west-1 region
 	eksResourceAccountCAWest1 = "761377655185"
@@ -516,6 +522,7 @@ func SupportedRegions() []string {
 		RegionAPSouth1,
 		RegionAPSouth2,
 		RegionAPEast1,
+		RegionAPEast2,
 		RegionMECentral1,
 		RegionMESouth1,
 		RegionSAEast1,
@@ -583,6 +590,8 @@ func EKSResourceAccountID(region string) string {
 	switch region {
 	case RegionAPEast1:
 		return eksResourceAccountAPEast1
+	case RegionAPEast2:
+		return eksResourceAccountAPEast2
 	case RegionCAWest1:
 		return eksResourceAccountCAWest1
 	case RegionMECentral1:
