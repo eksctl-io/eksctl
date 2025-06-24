46170a33d0575de1989b9848489826014cceef2a
Nathan Clonts <nclonts@amazon.com>
2025-06-24 17:41:47 +0000

    rm duplicate validation file

--- pkg/actions/podidentityassociation/podidentityassociation_suite_test.go
+++ pkg/actions/podidentityassociation/podidentityassociation_suite_test.go
@@ -9,5 +9,5 @@ import (
 
 func TestPodIdentityAssociation(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "PodIdentityAssociation Suite")
+	RunSpecs(t, "Pod Identity Association Suite")
 }
