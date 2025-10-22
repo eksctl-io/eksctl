57d0cf35439519b1fac0c1f51e74dc21aa075275
Nick Blaskey <nblaskey@amazon.com>
2025-10-22 17:12:53 +0000

    Unit tests passing

--- pkg/actions/nodegroup/export_test.go
+++ pkg/actions/nodegroup/export_test.go
@@ -5,10 +5,6 @@ import (
 	"github.com/weaveworks/eksctl/pkg/eks"
 )
 
-func (m *Manager) SetWaiter(wait WaitFunc) {
-	m.wait = wait
-}
-
 func (m *Manager) SetStackManager(stackManager manager.StackManager) {
 	m.stackManager = stackManager
 }
