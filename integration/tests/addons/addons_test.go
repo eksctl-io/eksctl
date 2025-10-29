58cad8de0a5bd46f1c4d2bbe7914e973b4c56f7d
Nick Blaskey <nblaskey@amazon.com>
2025-10-29 19:23:17 +0000

    Misspell

--- integration/tests/addons/addons_test.go
+++ integration/tests/addons/addons_test.go
@@ -808,7 +808,7 @@ var _ = Describe("(Integration) [EKS Addons test]", func() {
 
 			By("removing all pod identity associations owned by the addon")
 			clusterConfig.Addons[1].PodIdentityAssociations = &[]api.PodIdentityAssociation{}
-			// Don't wait for aws-ebs-csi-driver add-on becuase it won't become healthy without iam perms now.
+			// Don't wait for aws-ebs-csi-driver add-on because it won't become healthy without iam perms now.
 			Expect(makeUpdateAddonCMD("--timeout", "0")).To(RunSuccessfully())
 			assertAddonHasPodIDs(api.AWSEBSCSIDriverAddon, 0)
 
